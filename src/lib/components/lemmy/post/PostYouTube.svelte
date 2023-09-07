81b806056c8bb4d35ab33738836fe3ea6e287688
ptz <ptz@app03.ptz.net>
2023-09-07 14:19:33 -0400

    Updated to support YouTube Shorts videos :sigh:

--- src/lib/components/lemmy/post/PostYouTube.svelte
+++ src/lib/components/lemmy/post/PostYouTube.svelte
@@ -19,8 +19,9 @@
     
     if (post.post && post.post.url) {
         // Parse URLs to pick out video IDs to create embed URLs
-        videoID = new URL(post.post.url).pathname.replace('/','')
-        if (videoID == "watch") {
+        videoID = new URL(post.post.url).pathname.replace('/watch','').replace('/shorts/','')
+        
+        if (!videoID) {
             videoID = new URL(post.post.url).searchParams.get('v');
         }
         
