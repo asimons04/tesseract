12f906ec840983dd275a32c2099ae82f1e93ebd8
ptz <ptz@app03.ptz.net>
2023-09-11 10:13:03 -0400

    Refactor routes/+page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -6,34 +6,31 @@ import { error } from '@sveltejs/kit'
 import { profile } from '$lib/auth.js'
 
 export async function load({ url, fetch }) {
-  const page = Number(url.searchParams.get('page') || 1) || 1
+    const page = Number(url.searchParams.get('page') || 1) || 1
 
-  const sort: SortType =
-    (url.searchParams.get('sort') as SortType) ||
-    get(userSettings).defaultSort.sort
-  const listingType: ListingType =
-    (url.searchParams.get('type') as ListingType) ||
-    get(userSettings).defaultSort.feed
+    const sort: SortType = (url.searchParams.get('sort') as SortType) || get(userSettings).defaultSort.sort
 
-  try {
-    return {
-      sort: sort,
-      listingType: listingType,
-      page: page,
-      posts: await getClient(undefined, fetch).getPosts({
-        limit: 20,
-        page: page,
-        sort: sort,
-        type_: listingType,
-        auth: get(profile)?.jwt,
-      }),
-      streamed: {
-        site: getClient(undefined, fetch).getSite({}),
-      },
+    const listingType: ListingType = (url.searchParams.get('type') as ListingType) || get(userSettings).defaultSort.feed
+
+    try {
+        return {
+            sort: sort,
+            listingType: listingType,
+            page: page,
+            posts: await getClient(undefined, fetch).getPosts({
+                limit: 20,
+                page: page,
+                sort: sort,
+                type_: listingType,
+                auth: get(profile)?.jwt,
+            }),
+            streamed: {
+                site: getClient(undefined, fetch).getSite({}),
+            },
+        }
+    } catch (err) {
+        throw error(500, {
+            message: 'Failed to fetch homepage.',
+        })
     }
-  } catch (err) {
-    throw error(500, {
-      message: 'Failed to fetch homepage.',
-    })
-  }
 }
