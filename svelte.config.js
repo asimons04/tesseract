7c66534229ab6f5804c3190c8cbc2a977b00f053
asimons04 <asimons@ptznetwork.org>
2023-09-13 21:26:10 -0400

    Fix formatting

--- svelte.config.js
+++ svelte.config.js
@@ -13,12 +13,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: process.env.ADAPTER == 'node' ? node() : auto(),
-		csrf: {
-                  checkOrigin: true,
+        csrf: {
+            checkOrigin: true,
         },
         csp: {
-			directives: {
-				'default-src':  ['self'],
+            directives: {
+                'default-src':  ['self'],
                 'connect-src':  ['*'],
                 'manifest-src': ['self'],
                 'img-src':      ['*', 'data:', 'blob:'],
@@ -28,9 +28,9 @@ const config = {
                 'base-uri':     ['self'],
                 'frame-src':    ['*'],
                 'media-src':    ['*', 'data:'],
-		 		'object-src':   ['none'],
-			},
-			mode: 'nonce'
+                'object-src':   ['none'],
+            },
+            mode: 'nonce'
 		}
 		
 	},
