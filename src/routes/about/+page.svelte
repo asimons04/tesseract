8fbab708c7213eebe68e9a304833d5302e20a5f3
asimons04 <asimons@ptznetwork.org>
2023-09-16 08:32:05 -0400

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,69 +1,75 @@
 <script>
-  import Link from '$lib/components/input/Link.svelte'
-  import Logo from '$lib/components/ui/Logo.svelte'
+    import Link from '$lib/components/input/Link.svelte'
+    import Logo from '$lib/components/ui/Logo.svelte'
     import Application from '../admin/applications/Application.svelte';
 </script>
 
 <title>Tesseract | About</title>
-<div class="max-w-xl mx-auto">
+<div class="max-w-3xl mx-auto">
     <div class="flex items-center gap-1 mx-auto w-max">
     <Logo width={48} />
         <span class="text-2xl font-bold">Tesseract</span>
     </div>
     <h1 class="text-3xl font-bold leading-8">About</h1>
+
     <p class="my-3">
-        <strong>Tesseract</strong> is a fork of Xylight's <a href="https://github.com/Xyphyn/photon" target="_blank" title="Photon">Photon</a> 
-        and is designed for media-rich feeds and content.
+        <strong>Tesseract</strong> is a heavily modified fork of Photon that is designed to support media-rich feeds and content.  It was originally 
+        my unnamed custom build of Photon before I decided to make it into an official fork.
     </p>
     
-    <h2 class="text-2xl font-bold leading-8">Why Fork Photon</h2>
-    <p class="my-3">
-        Photon is great, and I've contributed code and donations to it (and will continue to do so), but the dev and I have very different 
-        visions as far as what we want in a Lemmy client.  I'm going for a full, fat experience with all the bells and whistles as far
-        as media and presentation are concerned.
-    </p>
     
-    <h2 class="text-2xl font-bold leading-8">How does Tesseract differ from Photon?</h2>
-    <p>Right now, only minimally, but also substantially in terms of qualify of life features that I want in a Lemmy frontend.</p>
     
-    <p>
-        I fully admit that I'm standing on the shoulders of giants here, but as time progresses, I hope to make this client my own
-        with unique features and enhancements tailored to the evolving landscape of the Lemmy ecosystem.
-    </p>
-    
-    <h3 class="text-xl font-bold leading-8">Features Unique to Tesseract</h3>
+    <h2 class="text-2xl font-bold leading-8 ">Features</h2>
     <ul class="list-disc pl-4 my-3">
-        <li>Inline media players for Youtube and Spotify (more coming eventually)</li>
-        <li>More focus on images in feed and post view.</li>
+        <li>
+            Full media support in feed and posts:
+            <ul class="list-disc pl-4">
+                <li>YouTube/Invidious/Piped</li>
+                <li>Soundcloud</li>
+                <li>Bandcamp</li>
+                <li>More as I think of them or as requested</li>
+            </ul>
+            <p class="my-2">Users can enable/disable media embeds from their settings.</p>
+        </li>
+        <li>Optimized for desktop and mobile. All desktop features are available in mobile.</li>
         <li>A more "new" Reddit-like look and feel</li>
-        <li>For a full list, please see the <Link href="https://github.com/asimons04/Tesseract/blob/main/ChangeLog.md" newtab={true} title="ChangeLog.md" highlight>change log</Link>.</li>
+        <li>Code syntax highlighting.</li>
+        <li>Full admin tools to manage your Lemmy instance</li>
+        <li>Extensive configuration options</li>
+        <li>Modlog and Moderation Tools</li>
     </ul>
 
-    <h2 class="text-2xl font-bold leading-8">Roadmap</h2>
-    <p>
-        There are a few things I have in mind to start with, but only a few are fully fleshed out as far as the implementation goes:
-    </p>
+    <h2 class="text-2xl font-bold leading-8">Get Tesseract</h2>
+    <!--
+    <div class="flex flex-row my-3">
+        <div class="flex flex-row flex-wrap">
+            <div class="flex-initial">
+                <img src="https://y.yarn.co/1ecfce66-a4ec-48de-8516-a7d52478cc2e_text.gif" alt="Verna from '30 Rock' saying 'Where did you get that?'"/>
+            </div>
 
-    <ul class="list-disc pl-4 my-3">
-        <li>Adding a search field to the top navbar for quick searching.</li>
-        <li>
-            For the rest, see the <Link href="https://github.com/asimons04/Tesseract/blob/main/README.md" newtab={true} title="README.md" highlight>README.md</Link>
-            on the GitHub page.
-        </li>
-    </ul>
+            <div class="flex-initial">
+                <img src="https://y.yarn.co/01fce8e0-fbf0-4109-a649-de693986fc88_text.gif" alt="Verna from '30 Rock' saying 'You made it?'"/>
+            </div>
+        </div>
 
-    <h2 class="text-2xl font-bold leading-8">Support/Contributions</h2>
-    <p>
-        If you happened to stumble upon this project and feel like you want to make a contribution,
-        please consider donating to the original dev, Xylight, as I do not wish to receive any funding
-        for my work on this fork.  
-    </p>
+        <div class="flex flex-row flex-wrap">
+            <div class="flex-initial">
+                <img src="https://y.yarn.co/eedeeba5-675e-458e-b354-d09bd8ef6336_text.gif" alt="Verna from '30 Rock' saying 'Holy crap!'"/>
+            </div>
+
+            <div class="flex-initial">
+                <img src="https://y.yarn.co/24a95368-e9b8-40f1-bc97-4863be5f4358_text.gif" alt="Verna from '30 Rock' asking 'Where do I commission one?'"/>
+            </div>
+        </div>
+    </div>
+    -->
     
-    <ul class="list-disc pl-4 my-3">
-        <li>
-            <Link href="https://buymeacoffee.com/xylight" newtab={true} title="Donate to Xylight" highlight>Donate to Xylight</Link>
-        </li>
-    </ul>
+    <p class="my-3">
+        The project has a <Link href="https://github.com/asimons04/Tesseract" newtab={true} title="ChangeLog.md" highlight>GitHub page.</Link>
+        with more details, a detailed change log, and instructions for deployment..
+    </p>
+   
+
 </div>
 
 
