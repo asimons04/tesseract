3615a2e75e054e9c2aaff65b547862109cc52b11
asimons04 <asimons@ptznetwork.org>
2023-10-10 12:32:42 -0400

    Removed 'muted' flag on html5 video tag

--- src/lib/components/lemmy/post/PostVideo.svelte
+++ src/lib/components/lemmy/post/PostVideo.svelte
@@ -9,7 +9,7 @@
 
 
 <div class="ml-auto mr-auto mt-1 mb-1 max-w-4xl">
-    <video controls muted playsinline class="rounded-md max-w-full max-h-[80svh] mx-auto" >
+    <video controls playsinline class="rounded-md max-w-full max-h-[80svh] mx-auto" >
         <source src="{imageProxyURL(post.post.url)}" />
     </video>
 </div>
