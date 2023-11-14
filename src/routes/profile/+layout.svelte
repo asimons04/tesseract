9ed4eead7943c38277fcf1a831e636f16d89a7a1
asimons04 <asimons@ptznetwork.org>
2023-11-14 11:53:08 -0500

    Add new buttons to profile layout

--- src/routes/profile/+layout.svelte
+++ src/routes/profile/+layout.svelte
@@ -1,9 +1,18 @@
 <script>
-    import { goto } from '$app/navigation'
     import { page } from '$app/stores'
 
-    import MultiSelect from '$lib/components/input/MultiSelect.svelte'
+    import Button from '$lib/components/input/Button.svelte'
     import UserCard from '$lib/components/lemmy/user/UserCard.svelte'
+
+    import {
+        Icon,
+        Cog6Tooth,
+        UserCircle,
+        Inbox,
+        NoSymbol,
+        Bookmark
+    } from 'svelte-hero-icons'
+
 
     export let data;
 </script>
@@ -13,17 +22,41 @@
 </svelte:head>
 
 <div class="flex flex-col gap-4 h-full">
-    <div class="p-2">
-        <MultiSelect
-            headless
-            options={['/profile/user', '/profile/inbox', '/profile/settings', '/profile/blocks', '/profile/saved']}
-            optionNames={['User', 'Inbox', 'Settings', 'Blocks', 'Saved']}
-            selected={$page.url.pathname}
-            on:select={(e) => {
-                goto(e.detail)
-            }}
-            items={5}
-        />
+    <div class="flex flex-row gap-1 p-2">
+        <Button color="tertiary" alignment="left" title="Profile" class="hover:bg-slate-200" href="/profile/user">
+            <span class="flex flex-col items-center {$page.url.pathname =='/profile/user' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                <Icon src={UserCircle} mini size="18" title="Profile" />
+                <span class="text-xs">Profile</span>
+            </span>            
+        </Button>
+
+        <Button color="tertiary" alignment="left" title="Inbox" class="hover:bg-slate-200" href="/profile/inbox">
+            <span class="flex flex-col items-center {$page.url.pathname =='/profile/inbox' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                <Icon src={Inbox} mini size="18" title="Inbox" />
+                <span class="text-xs">Inbox</span>
+            </span>            
+        </Button>
+
+        <Button color="tertiary" alignment="left" title="Settings" class="hover:bg-slate-200" href="/profile/settings">
+            <span class="flex flex-col items-center {$page.url.pathname =='/profile/settings' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                <Icon src={Cog6Tooth} mini size="18" title="Settings" />
+                <span class="text-xs">Settings</span>
+            </span>            
+        </Button>
+
+        <Button color="tertiary" alignment="left" title="Blocks" class="hover:bg-slate-200" href="/profile/blocks">
+            <span class="flex flex-col items-center {$page.url.pathname =='/profile/blocks' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                <Icon src={NoSymbol} mini size="18" title="Blocks" />
+                <span class="text-xs">Blocks</span>
+            </span>            
+        </Button>
+
+        <Button color="tertiary" alignment="left" title="Saved" class="hover:bg-slate-200" href="/profile/saved">
+            <span class="flex flex-col items-center {$page.url.pathname =='/profile/saved' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                <Icon src={Bookmark} mini size="18" title="Saved" />
+                <span class="text-xs">Saved</span>
+            </span>            
+        </Button>
     </div>
     
     <div class="flex flex-col-reverse xl:flex-row gap-4 max-w-full w-full">
