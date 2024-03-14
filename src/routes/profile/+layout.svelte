aae485c1e9e60529f8384b2eecb6319396a6d6c0
Admiral Patrick <asimons@ptznetwork.org>
2024-03-14 19:54:42 -0400

    Fix page detectoin

--- src/routes/profile/+layout.svelte
+++ src/routes/profile/+layout.svelte
@@ -25,28 +25,28 @@
    
     <div class="flex flex-row gap-1 p-2">
         <Button color="tertiary" alignment="left" title="Profile" class="hover:bg-slate-200" href="/profile/user">
-            <span class="flex flex-col items-center {$page.url.pathname =='/profile/user' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+            <span class="flex flex-col items-center {$page.url.pathname.startsWith('/profile/user') ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
                 <Icon src={UserCircle} mini size="18" title="Profile" />
                 <span class="text-xs">Profile</span>
             </span>            
         </Button>
 
         <Button color="tertiary" alignment="left" title="Inbox" class="hover:bg-slate-200" href="/profile/inbox">
-            <span class="flex flex-col items-center {$page.url.pathname =='/profile/inbox' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+            <span class="flex flex-col items-center {$page.url.pathname.startsWith('/profile/inbox') ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
                 <Icon src={Inbox} mini size="18" title="Inbox" />
                 <span class="text-xs">Inbox</span>
             </span>            
         </Button>
 
         <Button color="tertiary" alignment="left" title="Settings" class="hover:bg-slate-200" href="/profile/settings">
-            <span class="flex flex-col items-center {$page.url.pathname =='/profile/settings' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+            <span class="flex flex-col items-center {$page.url.pathname.startsWith('/profile/settings') ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
                 <Icon src={Cog6Tooth} mini size="18" title="Settings" />
                 <span class="text-xs">Settings</span>
             </span>            
         </Button>
 
         <Button color="tertiary" alignment="left" title="Blocks" class="hover:bg-slate-200" href="/profile/blocks">
-            <span class="flex flex-col items-center {$page.url.pathname =='/profile/blocks' ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+            <span class="flex flex-col items-center {$page.url.pathname.startsWith('/profile/blocks') ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
                 <Icon src={NoSymbol} mini size="18" title="Blocks" />
                 <span class="text-xs">Blocks</span>
             </span>            
