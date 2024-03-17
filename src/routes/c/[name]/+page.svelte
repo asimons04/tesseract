40325d8baebcfb2608eda27a97f510a0ea27a2db
Admiral Patrick <asimons@ptznetwork.org>
2024-03-17 13:09:01 -0400

    TS fixes

--- src/routes/c/[name]/+page.svelte
+++ src/routes/c/[name]/+page.svelte
@@ -1,57 +1,45 @@
 <script lang="ts">
     
-    import { afterNavigate, disableScrollHandling, goto } from '$app/navigation';
+    //import { afterNavigate, disableScrollHandling, goto } from '$app/navigation';
     import { arrayRange, fullCommunityName, searchParam } from '$lib/util.js'
     import { getSessionStorage, setSessionStorage } from '$lib/session.js'
     import { onDestroy, onMount } from 'svelte'
     import { page } from '$app/stores'
-    import { scrollToTop } from '$lib/components/lemmy/post/helpers'
-    import { sortOptions, sortOptionNames } from '$lib/lemmy'
-    import { userSettings } from '$lib/settings.js'
+    //import { scrollToTop } from '$lib/components/lemmy/post/helpers'
+    //import { sortOptions, sortOptionNames } from '$lib/lemmy'
+    //import { userSettings } from '$lib/settings.js'
     
     
     import CommunityCard from '$lib/components/lemmy/community/CommunityCard.svelte'
-    import MultiSelect from '$lib/components/input/MultiSelect.svelte'
     import Pageination from '$lib/components/ui/Pageination.svelte'
     import PostFeed from '$lib/components/lemmy/post/PostFeed.svelte'
     import SubNavbar from '$lib/components/ui/subnavbar/SubNavbar.svelte'
-    import SelectMenu from '$lib/components/input/SelectMenu.svelte'
-    import Sort from '$lib/components/lemmy/Sort.svelte'
 
-    import {
-        ArrowSmallRight,
-        Bars3,
-        ChartBar,
-        DocumentDuplicate,
-        Home,
-        Icon,
-        QueueList,
-        UserGroup,
-        Window
-    } from 'svelte-hero-icons'
 
     export let data
     
     
     onMount(() => {
-        setSessionStorage('lastSeenCommunity', {
-            id: data.community.community_view.community.id,
-            name: fullCommunityName(
-                data.community.community_view.community.name,
-                data.community.community_view.community.actor_id
-            ),
-        })
+        if (data?.community) {
+            setSessionStorage('lastSeenCommunity', {
+                id: data.community.community_view.community.id,
+                name: fullCommunityName(
+                    data.community.community_view.community.name,
+                    data.community.community_view.community.actor_id
+                ),
+            })
+        }
     })
 </script>
 
 <svelte:head>
-    <title>{data.community.community_view.community.title}</title>
+    <title>{data?.community?.community_view.community.title ?? "Community Not Found"}</title>
 
     <meta
         name="og:title"
-        content={data.community.community_view.community.title}
+        content={data?.community?.community_view.community.title ?? "Community Not Found"}
     />
-    {#if data.community.community_view.community.description}
+    {#if data?.community?.community_view.community.description}
         <meta
             name="og:description"
             content={data.community.community_view.community.description}
@@ -63,7 +51,7 @@
     sortMenu={true} bind:selectedSortOption={data.sort}
     pageSelection={true} bind:currentPage={data.page}
 />
-
+{#if data?.posts && data?.community}
 <div class="flex flex-col-reverse  xl:flex-row gap-4 max-w-full w-full py-2">
     <div class="flex flex-col gap-4 max-w-full w-full min-w-0">
         
@@ -76,3 +64,4 @@
         <CommunityCard community_view={data.community.community_view} moderators={data.community.moderators}/>
     </div>
 </div>
+{/if}
