ea5e03b10cfdcf1f2304fe850888a78951323c67
Admiral Patrick <asimons@ptznetwork.org>
2024-03-26 08:02:07 -0400

    Expand community info in sidebar by default

--- src/lib/components/lemmy/community/CommunityCard.svelte
+++ src/lib/components/lemmy/community/CommunityCard.svelte
@@ -397,7 +397,7 @@
         {/if}
         
         {#if community_view?.community?.description}
-            <CollapseButton icon={InformationCircle} title="Community Details">
+            <CollapseButton icon={InformationCircle} title="Community Details" expanded={true}>
                 <Markdown source={community_view.community.description} />
             </CollapseButton>
         {/if}
