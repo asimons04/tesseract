d132d2ac6a2cf6c7d11926090ecb029ed70a9440
Admiral Patrick <asimons@ptznetwork.org>
2024-03-29 20:06:50 -0400

    Increase max height of sticky card by 2 rem

--- src/lib/components/ui/StickyCard.svelte
+++ src/lib/components/ui/StickyCard.svelte
@@ -2,7 +2,7 @@
     class="sticky top-[8rem] break-words flex flex-col gap-1 z-10 pr-2
     w-full
     xl:w-[25rem] xl:overflow-auto
-    max-h-[calc(100svh-11rem)]
+    max-h-[calc(100svh-9rem)]
     text-sm border-slate-200 dark:border-zinc-800 {$$props.class}"
 >
   <slot />
