3ce00c2a0a10262e0391b6773689ee5a89b3bef3
Admiral Patrick <asimons@ptznetwork.org>
2024-04-09 18:10:34 -0400

    Increase delay before scroll to post

--- src/routes/c/[name]/+page.svelte
+++ src/routes/c/[name]/+page.svelte
@@ -72,7 +72,7 @@
             }
             
             // Scroll to last stored position if found in snapshot data (delay by number of posts + 100 ms)
-            if (pageState.scrollY) await scrollTo(pageState.scrollY, infiniteScroll.maxPosts + 300)
+            if (pageState.scrollY) await scrollTo(pageState.scrollY, infiniteScroll.maxPosts + 400)
             else window.scrollTo(0,0)
         }
     }
