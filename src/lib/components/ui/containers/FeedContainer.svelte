ca4484ccee43be0fa82bf25a7dbcb004f797eb83
Admiral Patrick <asimons@ptznetwork.org>
2024-04-13 21:39:24 -0400

    Refine margins

--- src/lib/components/ui/containers/FeedContainer.svelte
+++ src/lib/components/ui/containers/FeedContainer.svelte
@@ -2,6 +2,6 @@
     import { userSettings} from '$lib/settings'
 </script>
 
-<div class="w-full flex flex-col gap-5 ml-auto mr-auto {$userSettings.uiState.feedMargins ? 'sm:w-full md:w-[85%] lg:w-[90%] xl:w-[75%]' : ''}">
+<div class="w-full flex flex-col gap-5 ml-auto mr-auto mt-2 {$userSettings.uiState.feedMargins ? 'sm:w-full md:w-[85%] lg:w-[90%] xl:w-[75%]' : ''}">
     <slot/>
 </div>
--- src/lib/components/ui/containers/MainContentArea.svelte
+++ src/lib/components/ui/containers/MainContentArea.svelte
@@ -3,21 +3,8 @@
         <slot/>
     </div>
 
-    <div class="lg:mb-[-24px]">
+    <div class="xl:mb-[-24px]">
         <slot name="right-panel"/>
     </div>
 
 </div>
-
-<!---
-<div class="flex flex-col-reverse  xl:flex-row gap-4 max-w-full w-full h-full py-4">
-    <div class="flex flex-col gap-4 max-w-full w-full h-full min-w-0">
-        <slot/>
-    </div>
-
-    <div class="lg:mb-[-24px]">
-        <slot name="right-panel"/>
-    </div>
-
-</div>
---->
