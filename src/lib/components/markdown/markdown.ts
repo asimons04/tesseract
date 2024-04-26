ed74bdcd9f2c8ea778e023350f905cd370fa829a
Admiral Patrick <asimons@ptznetwork.org>
2024-04-26 07:47:12 -0400

    Remove no longer used markdown helper functions

--- src/lib/components/markdown/markdown.ts
+++ src/lib/components/markdown/markdown.ts
@@ -3,56 +3,6 @@ import { userSettings } from '$lib/settings'
 
 const $userSettings = get(userSettings)
 
-const communityLinks = {
-    validate: function (text: any, pos: any, self: any) {
-        var tail = text.slice(pos)
-
-        if (!self.re.community) {
-            self.re.community = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z]{2,6})/)
-        }
-
-        if (self.re.community.test(tail)) {
-            // Linkifier allows punctuation chars before prefix,
-            // but we additionally disable `@` ("@@mention" is invalid)
-            if (pos >= 2 && tail[pos - 2] === '!') {
-                return false
-            }
-            return tail.match(self.re.community)![0].length
-        }
-        return 0
-    },
-  
-    normalize: function (match: any) {
-        let prefix = match.url
-        prefix = prefix.startsWith('c/') ? prefix.slice(2) : prefix.slice(1)
-        match.url = `/c/${prefix}`
-    },
-}
-
-const userLinks = {
-    validate: function (text: any, pos: any, self: any) {
-        var tail = text.slice(pos)
-
-        if (!self.re.user) {
-            self.re.user = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z]{2,6})/)
-        }
-        if (self.re.user.test(tail)) {
-            // Linkifier allows punctuation chars before prefix,
-            // but we additionally disable `@` ("@@mention" is invalid)
-        if (pos >= 2 && tail[pos - 2] === '!') {
-            return false
-        }
-        return tail.match(self.re.user)![0].length
-        }
-        return 0
-    },
-    normalize: function (match: any) {
-        let prefix = match.url
-        prefix = prefix.startsWith('u/') ? prefix.slice(2) : prefix.slice(1)
-        match.url = `/u/${prefix}`
-    },
-}
-
 
 export function filterAnnoyingCCLicenseOnComments(source:string) {
     return $userSettings.uiState.filterAnnoyingCCLicense
