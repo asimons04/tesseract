947792481b263d9ece98d6a89bb98fe4bbe5053c
Admiral Patrick <asimons@ptznetwork.org>
2024-04-28 19:30:04 -0400

    Fix indentation

--- src/lib/components/markdown/renderers/MarkdownImage.svelte
+++ src/lib/components/markdown/renderers/MarkdownImage.svelte
@@ -24,63 +24,63 @@
 
 {#if $userSettings.inlineImages}
 
-<div bind:this={container} class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 m-1 rounded-2xl w-fit lg:max-w-[60%] p-2">
-    <div class="ml-auto mr-auto max-w-full">
-        
-        <!---Show Text as a Title--->
-        {#if token.title && $userSettings.uiState.showAltText}
-            <p class="font-bold text-center text-sm">{fixLemmyEncodings(token.title)}</p>
-        {/if}
-        
-               
-        <!--- Audio--->
-        {#if isAudio(token.href) && inViewport}
-            <audio controls preload="auto">
-                <source src={imageProxyURL(token.href)} type="{
-                    new URL(token.href).pathname.endsWith('mp3')
-                        ? 'audio/mpeg'
-                        : new URL(token.href).pathname.endsWith('aac')
-                            ? 'audio/aac'
-                            : new URL(token.href).pathname.endsWith('oga')
-                                ? 'audio/ogg'
-                                : new URL(token.href).pathname.endsWith('opus')
-                                    ? 'audio/opus'
-                                    : undefined
+    <div bind:this={container} class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 m-1 rounded-2xl w-fit lg:max-w-[60%] p-2">
+        <div class="ml-auto mr-auto max-w-full">
+            
+            <!---Show Text as a Title--->
+            {#if token.title && $userSettings.uiState.showAltText}
+                <p class="font-bold text-center text-sm">{fixLemmyEncodings(token.title)}</p>
+            {/if}
+            
+                
+            <!--- Audio--->
+            {#if isAudio(token.href) && inViewport}
+                <audio controls preload="auto">
+                    <source src={imageProxyURL(token.href)} type="{
+                        new URL(token.href).pathname.endsWith('mp3')
+                            ? 'audio/mpeg'
+                            : new URL(token.href).pathname.endsWith('aac')
+                                ? 'audio/aac'
+                                : new URL(token.href).pathname.endsWith('oga')
+                                    ? 'audio/ogg'
+                                    : new URL(token.href).pathname.endsWith('opus')
+                                        ? 'audio/opus'
+                                        : undefined
 
-                    }
-                " 
-            />
-            </audio>
-        
-        <!---Direct Video--->
-        {:else if isVideo(token.href) && inViewport}
-            <!-- svelte-ignore a11y-media-has-caption -->
-            <video class="rounded-xl max-w-full max-h-[65vh] max-w-[88vw] mx-auto" controls playsinline {loop}>
-                <source src="{imageProxyURL(token.href)}" 
-                    type="{
-                        new URL(token.href).pathname.endsWith('mp4') || new URL(token.href).pathname.endsWith('m4v')
-                            ? 'video/mp4' 
-                            : new URL(token.href).pathname.endsWith('webm') 
-                                ? "video/webm" 
-                                : new URL(token.href).pathname.endsWith('mov') 
-                                    ? "video/mp4"
-                                    : ''
-                    }"
+                        }
+                    " 
                 />
-            </video>    
-        
-        <!---Image--->
-        {:else}
-            <img src={imageProxyURL(token.href)} title={token.title} alt={token.text} loading="lazy" class="mx-auto rounded-xl" />
-        {/if}
+                </audio>
+            
+            <!---Direct Video--->
+            {:else if isVideo(token.href) && inViewport}
+                <!-- svelte-ignore a11y-media-has-caption -->
+                <video class="rounded-xl max-w-full max-h-[65vh] max-w-[88vw] mx-auto" controls playsinline {loop}>
+                    <source src="{imageProxyURL(token.href)}" 
+                        type="{
+                            new URL(token.href).pathname.endsWith('mp4') || new URL(token.href).pathname.endsWith('m4v')
+                                ? 'video/mp4' 
+                                : new URL(token.href).pathname.endsWith('webm') 
+                                    ? "video/webm" 
+                                    : new URL(token.href).pathname.endsWith('mov') 
+                                        ? "video/mp4"
+                                        : ''
+                        }"
+                    />
+                </video>    
+            
+            <!---Image--->
+            {:else}
+                <img src={imageProxyURL(token.href)} title={token.title} alt={token.text} loading="lazy" class="mx-auto rounded-xl" />
+            {/if}
 
-        <!---Show Alt Text as Caption--->
-        {#if token.text && $userSettings.uiState.showAltText}
-            <!---<p class="pt-2 text-xs">{fixLemmyEncodings(token.text)}</p>--->
-            <p class="background-blur-3xl bg-white/50 dark:bg-black/50 rounded-xl p-2 mt-2 text-xs text-center">{fixLemmyEncodings(token.text)}</p>
-        {/if}
+            <!---Show Alt Text as Caption--->
+            {#if token.text && $userSettings.uiState.showAltText}
+                <!---<p class="pt-2 text-xs">{fixLemmyEncodings(token.text)}</p>--->
+                <p class="background-blur-3xl bg-white/50 dark:bg-black/50 rounded-xl p-2 mt-2 text-xs text-center">{fixLemmyEncodings(token.text)}</p>
+            {/if}
+        </div>
     </div>
-</div>
 
 {:else}
     <Link highlight href={token.href} title={token.title} text={token.text} newtab={$userSettings.openInNewTab.links}/>
