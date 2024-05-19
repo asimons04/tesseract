59b963c72139758054994cc2f9c08d45e4422793
Admiral Patrick <asimons@ptznetwork.org>
2024-05-19 17:32:12 -0400

    Make avatar images/placeholders shrink as needed

--- src/lib/components/ui/Avatar.svelte
+++ src/lib/components/ui/Avatar.svelte
@@ -20,56 +20,58 @@
     export let community:boolean = false
 </script>
 
-{#if url}
-    <img
-        src="{imageProxyURL(url, (fullRes ? undefined : findClosestNumber(sizes,res||width)), 'webp')}"
-        {alt}
-        {width}
-        {title}
-        loading="lazy"
-        class="aspect-square object-cover overflow-hidden {ring ? 'ring-2 ring-sky-700' : ''} {$$props.class} "
-        class:rounded-full={circle}
-    />
-{:else}
-    <div
-        style="width: {width}px; height: {width}px;"
-        class="aspect-square object-cover overflow-hidden ring-2 ring-sky-700 {$$props.class}"
-        class:rounded-full={circle}
-    >
-        {#if community}
-            {@html createAvatar(initials, {
-                seed: alt,
-                }).toString()
-            }
-        {:else}
-            {@html createAvatar(adventurer, {
+<div class="shrink" style="width: {width}px; height: {width}px;">
+    {#if url}
+        <img
+            src="{imageProxyURL(url, (fullRes ? undefined : findClosestNumber(sizes,res||width)), 'webp')}"
+            {alt}
+            {width}
+            {title}
+            loading="lazy"
+            class="aspect-square object-cover overflow-hidden {ring ? 'ring-2 ring-sky-700' : ''} {$$props.class} "
+            class:rounded-full={circle}
+        />
+    {:else}
+        <!--style="width: {width}px; height: {width}px;"-->
+        <div
+            class="w-full h-full aspect-square object-cover overflow-hidden ring-2 ring-sky-700 {$$props.class}"
+            class:rounded-full={circle}
+        >
+            {#if community}
+                {@html createAvatar(initials, {
                     seed: alt,
-                    //scale: 125,
-                    backgroundColor: ["ffffff"],
-                    flip: true,
-                    eyebrows: [
-                        "variant01","variant02","variant03", "variant04","variant05",
-                        "variant06", "variant07","variant08","variant09","variant10",
-                        "variant11", "variant12","variant13","variant14","variant15"
-                    ],
-                    eyes: [
-                        "variant01","variant02","variant03", "variant04","variant05",
-                        "variant06", "variant07","variant08","variant09","variant10",
-                        "variant11", "variant12","variant13","variant14","variant15",
-                        "variant16", "variant17","variant18","variant19","variant20",
-                        "variant21", "variant22","variant23","variant24","variant25",
-                        "variant26"
-                    ],
-                    features: ["birthmark", "freckles"],
-                    featuresProbability: 45,
-                    glasses: ["variant01","variant02","variant03", "variant04","variant05"],
-                    glassesProbability: 35,
-                    randomizeIds: true
-                }).toString()
-            }
-        {/if}
-    </div>
-{/if}
+                    }).toString()
+                }
+            {:else}
+                {@html createAvatar(adventurer, {
+                        seed: alt,
+                        //scale: 125,
+                        backgroundColor: ["ffffff"],
+                        flip: true,
+                        eyebrows: [
+                            "variant01","variant02","variant03", "variant04","variant05",
+                            "variant06", "variant07","variant08","variant09","variant10",
+                            "variant11", "variant12","variant13","variant14","variant15"
+                        ],
+                        eyes: [
+                            "variant01","variant02","variant03", "variant04","variant05",
+                            "variant06", "variant07","variant08","variant09","variant10",
+                            "variant11", "variant12","variant13","variant14","variant15",
+                            "variant16", "variant17","variant18","variant19","variant20",
+                            "variant21", "variant22","variant23","variant24","variant25",
+                            "variant26"
+                        ],
+                        features: ["birthmark", "freckles"],
+                        featuresProbability: 45,
+                        glasses: ["variant01","variant02","variant03", "variant04","variant05"],
+                        glassesProbability: 35,
+                        randomizeIds: true
+                    }).toString()
+                }
+            {/if}
+        </div>
+    {/if}
+</div>
 
 <!---
     {@html createAvatar(initials, {
