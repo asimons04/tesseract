f3e63bb7eae1e0897b2b492b2069b2804523c7bd
Admiral Patrick <asimons@ptznetwork.org>
2024-05-24 21:09:15 -0400

    Use zoomable images

--- src/lib/components/lemmy/post/PostLink.svelte
+++ src/lib/components/lemmy/post/PostLink.svelte
@@ -4,17 +4,16 @@
     
     import { getInstance } from '$lib/lemmy.js'
     import { imageSize } from './helpers.js'
-    import { imageProxyURL } from '$lib/image-proxy'
     import { userSettings } from '$lib/settings.js'
 
     import Link from '$lib/components/input/Link.svelte'
     import NSFWOverlay from './utils/NSFWOverlay.svelte'
     import ArchiveLinkSelector from './utils/ArchiveLinkSelector.svelte';
+    import ZoomableImage from '$lib/components/ui/ZoomableImage.svelte';
 
     export let post:PostView
     export let displayType: PostDisplayType
 
-    let loaded          = false;
     let size: string    = imageSize(displayType);
     
 
@@ -31,28 +30,32 @@
 
 
 {#if post.post?.thumbnail_url}
-    <a 
-        href={displayType == 'feed' ? `/post/${getInstance()}/${post.post.id}` : `${post.post.url}`}
-        target={
-            (displayType== 'feed' && $userSettings.openInNewTab.posts) || (displayType == 'post' && $userSettings.openInNewTab.links)
-                ? '_blank'
-                : undefined
-        }
-    >
-        <div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 rounded-md max-w-full p-1">
+    {#if displayType == 'feed'}
+        <a href="/post/{getInstance()}/{post.post.id}" target={ (displayType== 'feed' && $userSettings.openInNewTab.posts) ? '_blank' : undefined } >
+            <div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 rounded-md max-w-full p-1">
                 <div class="ml-auto mr-auto {size ?? 'max-w-3xl'}">
                     <NSFWOverlay bind:nsfw={post.post.nsfw} displayType={displayType} />
-                    <img
-                        src="{imageProxyURL(post.post?.thumbnail_url, (displayType=='feed' ? 1024 : undefined), 'webp')}"
-                        class="max-w-full ml-auto mr-auto object-cover rounded-md opacity-0 transition-opacity duration-300"
-                        class:opacity-100={loaded}
-                        on:load={() => (loaded = true)}
-                        class:blur-3xl={post.post.nsfw && $userSettings.nsfwBlur && displayType==='feed'}
-                        alt="{post.post.name}"
+
+                    <ZoomableImage url={post.post.thumbnail_url ?? post.post.url} 
+                        limitHeight={false} bind:nsfw={post.post.nsfw} altText={post.post.name} 
+                        zoomable={false}
+                        class="max-w-full ml-auto mr-auto object-cover rounded-md"
                     />
                 </div>
-            
+                
+            </div>
+        </a>
+    {:else if displayType == 'post'}
+        <div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 rounded-md max-w-full p-1">
+            <div class="ml-auto mr-auto {size ?? 'max-w-3xl'}">
+                <NSFWOverlay bind:nsfw={post.post.nsfw} displayType={displayType} />
+
+                <ZoomableImage url={post.post.thumbnail_url ?? post.post.url} 
+                    limitHeight={false} bind:nsfw={post.post.nsfw} altText={post.post.name} 
+                    class="max-w-full ml-auto mr-auto object-cover rounded-md"
+                />
+            </div>
         </div>
-    </a>
+    {/if}
 {/if}
 
