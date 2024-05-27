deb68bc2dfe740b4d92cfa22d51a7d4816575644
Admiral Patrick <asimons@ptznetwork.org>
2024-05-27 14:07:47 -0400

    Allow passing class from the component to the zoomable image

--- src/lib/components/lemmy/post/PostImage.svelte
+++ src/lib/components/lemmy/post/PostImage.svelte
@@ -40,7 +40,7 @@
         <ClickToPlayOverlay bind:show={clickToPlay} displayType={displayType} on:click={(e)=> dispatcher('click', e)}/>
         
         <ZoomableImage url={thumbnail_url ?? url} bind:nsfw={post.post.nsfw} altText={name} zoomable={zoomable}
-            class="ml-auto mr-auto object-cover rounded-md max-h-[min(80vh,800px)] z-20"
+            class="ml-auto mr-auto object-cover rounded-md max-h-[min(80vh,800px)] z-20 {$$props.class}"
             on:click={(e)=> dispatcher('click', e)}
         />
     
