27cd07ce84687a7b94456dec9563b6083e8d744c
Admiral Patrick <asimons@ptznetwork.org>
2024-05-27 12:46:02 -0400

    Add 'click to play' ability for when media embeds are disabled in feed

--- src/lib/components/lemmy/post/PostYouTube.svelte
+++ src/lib/components/lemmy/post/PostYouTube.svelte
@@ -8,7 +8,6 @@
     import Link from '$lib/components/input/Link.svelte'
     import IFrame from './utils/IFrame.svelte'
     import PostIsInViewport from './utils/PostIsInViewport.svelte'
-    import PostLink from '$lib/components/lemmy/post/PostLink.svelte'
     import PostImage from '$lib/components/lemmy/post/PostImage.svelte'
     import ArchiveLinkSelector from './utils/ArchiveLinkSelector.svelte';
 
@@ -21,12 +20,17 @@
     let embedURL:   URL | null | undefined
     let inViewport = false
     let size: string = imageSize(displayType);
+    let clickToPlayClicked = false
 
     $: if (post?.post?.url) embedURL = buildYouTubeEmbedLink(post.post.url, displayType, autoplay)
 
-    $: showAsEmbed = embedURL &&
-        (displayType == 'feed' && inViewport && $userSettings.embeddedMedia.feed && (!post.post.nsfw || !$userSettings.nsfwBlur)) ||
-        (displayType == 'post' && $userSettings.embeddedMedia.post)
+    $:  showAsEmbed = embedURL && (
+            (clickToPlayClicked && inViewport) ||
+            (displayType == 'feed' && inViewport && $userSettings.embeddedMedia.feed && (!post.post.nsfw || !$userSettings.nsfwBlur)) ||
+            (displayType == 'post' && $userSettings.embeddedMedia.post)
+        )
+
+    $: if (!inViewport) clickToPlayClicked = false
 </script>
 
 
@@ -34,18 +38,18 @@
 
 {#if showAsEmbed && embedURL}
     <span class="flex flex-row flex-wrap w-full gap-2 px-1">
-        <ArchiveLinkSelector url={post.post?.url} postType='youtube'/>    
-        
+        <ArchiveLinkSelector url={post.post?.url} postType='youtube' />    
+
         <Link domainOnly={!$userSettings.uiState.showFullURL} newtab={$userSettings.openInNewTab.links} highlight nowrap 
-            href={ embedURL
-                    ? embedURL.href.replace('embed','watch').replace('www.youtube-nocookie','youtube')
-                    : post.post.url
-            }
+            bind:href={post.post.url}
         />
+        
     </span>
-
+    
     <IFrame bind:embedURL bind:size bind:title={post.post.name} />
+    
 
+<!---IF embeds disabled, show as an image with link + alternate source selector menu--->
 {:else if post.post.thumbnail_url}
     <span class="flex flex-row flex-wrap w-full gap-2 px-1">
         <ArchiveLinkSelector url={post.post?.url} postType='youtube'/>    
@@ -53,20 +57,20 @@
         <Link highlight nowrap 
             domainOnly={!$userSettings.uiState.showFullURL} 
             newtab={$userSettings.openInNewTab.links} 
-            href={ embedURL
-                    ? embedURL.href.replace('embed','watch').replace('www.youtube-nocookie','youtube')
-                    : post.post.url
-            }
+            bind:href={post.post.url}
         />
     </span>
-
-    <PostImage bind:post displayType={displayType} zoomable={false}/>
-
-{:else if !post.post.thumbnail_url}
-    <Link title={post.post.name} highlight nowrap href={
-            embedURL
-            ? embedURL.href.replace('embed','watch').replace('www.youtube-nocookie','youtube')
-            : post.post.url
-        }
+    <PostImage bind:post displayType={displayType} clickToPlay={true} zoomable={false} on:click={(e)=> {
+            e.preventDefault()
+            e.stopPropagation()
+            clickToPlayClicked = true
+        }}
     />
+
+<!---If embeds disabled and no thumbnail image is available, show as a bare link--->
+{:else if !post.post.thumbnail_url}
+    <span class="flex flex-row flex-wrap w-full gap-2 px-1">
+        <ArchiveLinkSelector url={post.post?.url} postType='youtube'/>    
+        <Link title={post.post.name} highlight nowrap bind:href={post.post.url} />
+    </span>
 {/if}
