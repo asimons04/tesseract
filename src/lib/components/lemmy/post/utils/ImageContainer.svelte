cb7c37d62092da91cccf8e3e669d5fe7c7ab8847
Admiral Patrick <asimons@ptznetwork.org>
2024-05-27 12:42:42 -0400

    Create image container component instead of classing things the same repeatedly

---
+++ src/lib/components/lemmy/post/utils/ImageContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { userSettings } from "$lib/settings";
+    export let limitHeight = false
+</script>
+
+<div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 rounded-md max-w-full p-1">
+    <div class="ml-auto mr-auto {$userSettings.imageSize.feed ?? 'max-w-3xl'}"> 
+        <picture class="rounded-md overflow-hidden w-full {limitHeight ? 'max-h-[min(50vh,500px)]' : ''}  max-w-full"> 
+            <slot/>
+        </picture>
+    </div>
+</div>
