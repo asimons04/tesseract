72b4fc62f9e383ee01e93d999e8c2ed81bbf160e
Admiral Patrick <asimons@ptznetwork.org>
2024-05-27 12:44:39 -0400

    Refactor to use standard image container, add click to play overlay support

--- src/lib/components/lemmy/post/PostImage.svelte
+++ src/lib/components/lemmy/post/PostImage.svelte
@@ -2,26 +2,26 @@
     import type { PostDisplayType } from './helpers.js'
     import type { PostView } from 'lemmy-js-client'
     
-    import { getInstance } from '$lib/lemmy.js'
-    import { imageProxyURL } from '$lib/image-proxy'
-    import { userSettings } from '$lib/settings.js'
+    import { createEventDispatcher } from 'svelte'
 
+    import ImageContainer from './utils/ImageContainer.svelte'
     import NSFWOverlay from './utils/NSFWOverlay.svelte'
     import ZoomableImage from '$lib/components/ui/ZoomableImage.svelte'
+    import ClickToPlayOverlay from './utils/ClickToPlayOverlay.svelte';
 
     export let post:PostView 
     export let displayType: PostDisplayType
     export let zoomable:boolean = true
+    export let clickToPlay:boolean = false
+
+    let dispatcher = createEventDispatcher()
 
     $: url           = post.post.url as string
     $: thumbnail_url = post.post.thumbnail_url as string ?? post.post.url as string
     $: name          = post.post.name
-    
-    let instance     = getInstance()
-    let id           = post.post.id ?? undefined
 
     // Hack to get Imgur gifs to render without having to click through to the site.
-    $: if (!url?.endsWith('.gif') && post.post.embed_video_url && post.post.embed_video_url.endsWith('.gif')) {
+    $: if (!url?.endsWith('.gif')  && post?.post?.embed_video_url?.endsWith('.gif')) {
         url = post.post.embed_video_url
     }
 
@@ -33,53 +33,26 @@
     
 </script>
 
+<ImageContainer>
+    {#if displayType == 'feed'}
+        <NSFWOverlay bind:nsfw={post.post.nsfw} displayType={displayType} />
+        
+        <ClickToPlayOverlay bind:show={clickToPlay} displayType={displayType} on:click={(e)=> dispatcher('click', e)}/>
+        
+        <ZoomableImage url={thumbnail_url ?? url} bind:nsfw={post.post.nsfw} altText={name} zoomable={zoomable}
+            class="ml-auto mr-auto object-cover rounded-md max-h-[min(80vh,800px)] z-20"
+            on:click={(e)=> dispatcher('click', e)}
+        />
+    
+    <!---Rendering in post page--->
+    {:else}
+        <ZoomableImage url={url} altText={name} {zoomable}
+            class="ml-auto mr-auto object-contain rounded-md h-auto xl:min-h-[500px] z-30"
+        />
+
+    {/if}
+</ImageContainer>
 
 
-{#if displayType == 'feed'}
-<a href="/post/{instance}/{id}"
-    data-sveltekit-preload-data="off"
-    aria-label={name}
-    title={name}
->
-    <div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 rounded-md max-w-full p-1">
-        <div class="ml-auto mr-auto {$userSettings.imageSize.feed ?? 'max-w-3xl'}"> 
-            <picture class="rounded-md overflow-hidden w-full max-h-[min(50vh,500px)]  max-w-full"> 
-                <source srcset="{imageProxyURL(thumbnail_url, 768, 'webp') ?? imageProxyURL(url, 768, 'webp')}"
-                    media="(max-width: 768px)"
-                />
 
-                <source
-                    srcset="{imageProxyURL(thumbnail_url, undefined, 'webp') ?? imageProxyURL(url, undefined, 'webp')}"
-                    media="(max-width: 1024px)"
-                />
-                
-                <NSFWOverlay bind:nsfw={post.post.nsfw} displayType={displayType} />
-                
-                <ZoomableImage url={thumbnail_url ?? url} limitHeight={true} bind:nsfw={post.post.nsfw} altText={name} zoomable={false}
-                    class="ml-auto mr-auto object-cover rounded-md max-h-[min(80vh,800px)] z-20"
-                />
-
-            </picture>
-        </div>
-    </div>
-</a>
-
-{/if}
-
-{#if displayType =='post'}
-<div
-    class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 rounded-md max-w-full p-1"
-    data-sveltekit-preload-data="off"
-    aria-label={name}
->
-    <div class="ml-auto mr-auto {$userSettings.imageSize.post ?? 'max-w-3xl'}">
-        <picture class="rounded-md overflow-hidden  max-w-full"> 
-            <ZoomableImage url={url} limitHeight={false} altText={name} {zoomable}
-                class="ml-auto mr-auto object-contain rounded-md h-auto xl:min-h-[500px] z-30"
-            />
-        </picture>
-    </div>
-</div>
-
-{/if}
 
