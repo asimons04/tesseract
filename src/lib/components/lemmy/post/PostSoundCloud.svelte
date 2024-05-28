3d0a54c4fe6201aca12d54e6382a86b9ba6016b9
Admiral Patrick <asimons@ptznetwork.org>
2024-05-28 08:50:11 -0400

    Add click-to-play support to SoundCloud component

--- src/lib/components/lemmy/post/PostSoundCloud.svelte
+++ src/lib/components/lemmy/post/PostSoundCloud.svelte
@@ -3,37 +3,38 @@
     import type { PostView } from 'lemmy-js-client'
     
     import { userSettings,  } from '$lib/settings.js'
-    import { getInstance } from '$lib/lemmy.js'
     import { imageSize} from './helpers.js'
 
     import Link from '$lib/components/input/Link.svelte'
     import IFrame from './utils/IFrame.svelte'
     import PostIsInViewport from './utils/PostIsInViewport.svelte'
-    import PostLink from '$lib/components/lemmy/post/PostLink.svelte'
     import PostImage from '$lib/components/lemmy/post/PostImage.svelte'
     
-
     export let post: PostView
     export let displayType: PostDisplayType
     export let postContainer: HTMLDivElement
 
     let inViewport = false
     let embedURL:   URL
-    
+    let clickToPlayClicked = false
     let size: string = imageSize(displayType);
     
     // Check for a defined embed URL and use that if available
-    $: if (post.post && post.post.embed_video_url) {
-        embedURL = new URL(post.post.embed_video_url);
-    }
-    // If embed URL isn't provided, make our own!
-    else if (post.post && post.post.url && !post.post.url.includes("/discover/")) {
-        embedURL = new URL(`https://w.soundcloud.com/player/?visual=true&url=${post.post.url.replace('m.soundcloud.com', 'soundcloud.com')}`)
-    }
+    $:  if (post.post && post.post.embed_video_url) {
+            embedURL = new URL(post.post.embed_video_url);
+        }
+        // If embed URL isn't provided, make our own!
+        else if (post.post && post.post.url && !post.post.url.includes("/discover/")) {
+            embedURL = new URL(`https://w.soundcloud.com/player/?visual=true&url=${post.post.url.replace('m.soundcloud.com', 'soundcloud.com')}`)
+        }
 
-   $: showAsEmbed = embedURL &&
-        (displayType == 'feed' && inViewport && $userSettings.embeddedMedia.feed && (!post.post.nsfw || !$userSettings.nsfwBlur)) ||
-        (displayType == 'post' && $userSettings.embeddedMedia.post)
+    $:  showAsEmbed = embedURL && (clickToPlayClicked && inViewport) || (
+            (displayType == 'feed' && inViewport && $userSettings.embeddedMedia.feed && (!post.post.nsfw || !$userSettings.nsfwBlur)) ||
+            (displayType == 'post' && $userSettings.embeddedMedia.post)
+        )
+    
+        // Unset click to play when out of viewport (revert to thumbnail)
+    $:  if (!inViewport) clickToPlayClicked = false
 
 </script>
 
@@ -45,15 +46,8 @@
     <IFrame bind:embedURL bind:size bind:title={post.post.name} />
 
 {:else if post.post.thumbnail_url}
-    <!---Create image post if user has media embeds enabled for posts--->    
-    {#if $userSettings.embeddedMedia.post}
-        <Link href={post.post.url} title={post.post.name} newtab={$userSettings.openInNewTab.links} highlight nowrap />
-        <PostImage bind:post={post} displayType={displayType}/>
-    {:else}
-        <!---Create PostLink to external link if user does not have embeds enaled for posts--->
-        <PostLink post={post} displayType={displayType}/>
-
-    {/if}
+    <Link href={post.post.url} title={post.post.name} newtab={$userSettings.openInNewTab.links} highlight nowrap />
+    <PostImage bind:post={post} displayType={displayType} clickToPlay={true} zoomable={false} class="min-h-[300px]" on:click={(e)=> clickToPlayClicked = true }/>
 
 {:else if !post.post.thumbnail_url}
     <Link href={post.post.url} title={post.post.name} highlight nowrap />
