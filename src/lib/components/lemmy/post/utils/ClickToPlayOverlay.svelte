11614ab7cffc18b5c5a8346d85072d5490b581fe
Admiral Patrick <asimons@ptznetwork.org>
2024-05-29 15:47:30 -0400

    Decrease opacity in overlay

--- src/lib/components/lemmy/post/utils/ClickToPlayOverlay.svelte
+++ src/lib/components/lemmy/post/utils/ClickToPlayOverlay.svelte
@@ -15,7 +15,7 @@
 {#if show && displayType =='feed'}
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <div class="absolute z-[5] left-0 top-0 w-full h-full bg-black/50" 
+    <div class="absolute z-[5] left-0 top-0 w-full h-full bg-black/35" 
         on:click={(e)=> {
             if (show) {
                 e.preventDefault();
