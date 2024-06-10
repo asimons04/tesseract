c33d5cb397ee6c14998eca87dbabc6f201dbbb46
Admiral Patrick <asimons@ptznetwork.org>
2024-06-10 12:58:53 -0400

    Don't show placeholder in navbar for site icon if icon not defined; use Tesseract logo

--- src/lib/components/ui/Navbar.svelte
+++ src/lib/components/ui/Navbar.svelte
@@ -81,7 +81,7 @@
     <div data-sveltekit-preload-data="hover" class="flex flex-row gap-2 items-center mr-auto">
         <a href="/" class="flex flex-row items-center gap-2">
             
-            {#if $site && new URL($site.site_view.site.actor_id).hostname == $instance}
+            {#if $site && $site.site_view.site.icon && new URL($site.site_view.site.actor_id).hostname == $instance}
                 <Avatar url={$site.site_view.site.icon} alt={$site.site_view.site.name} width={32} res={64} circle={false}/>
                 <div class="flex flex-row items-center gap-2 max-[500px]:hidden">
                     
