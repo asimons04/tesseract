da9b4177c6dccb8343497b6c888231cc1d167502
Admiral Patrick <asimons@ptznetwork.org>
2024-07-01 15:31:39 -0400

    Fix vitePreprocess import since it is no longer re-exported from svelte/kit/vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import auto from '@sveltejs/adapter-auto';
 import node from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: process.env.ADAPTER == 'node' ? node() : auto(),
+		adapter: node(),
         alias: {
             '$routes': './src/routes',
             '$routes/*': './src/routes/*'
