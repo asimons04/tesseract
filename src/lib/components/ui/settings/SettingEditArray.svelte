f8b7de8765bed968d0c2a83126079ed84399103a
Admiral Patrick <asimons@ptznetwork.org>
2024-07-02 16:17:38 -0400

    Add filtering, placeholder options, and ability to add/remove multiple comma-delimited items

--- src/lib/components/ui/settings/SettingEditArray.svelte
+++ src/lib/components/ui/settings/SettingEditArray.svelte
@@ -20,11 +20,16 @@
     export let icon:IconSource = PlusCircle         // Icon used for the setting entry
     export let condition:boolean = true             // Boolean condition to show/hide the component
     export let textInputPlaceholder:string = ''     // Placeholder for text input field
-    export let showPlaceholder:boolean = true       // Show empty list placeholder
-    
+    export let filterable:boolean = false           // Show the filter input to allow searching of the list
+
+    export let showPlaceholder:boolean = true               // Show empty list placeholder
+    export let placeholderText:string = ''                  // Text to show in the empty placeholder
+    export let placeholderIcon:IconSource = ArchiveBoxXMark // Icon to use for the empty placeholder
+    export let placeholderTitle:string = 'No Items'         // Title for the empty placeholder
     
     let input_text:string
-    
+    let filter_input_text:string
+
     let dispatcher = createEventDispatcher<{
         add: string,
         delete: string,
@@ -43,20 +48,38 @@
         // Don't add a duplicate or empty item
         if (input == '') return arr
 
+        let items = input.split(',')
+
         if (add) {
-            if (arr.includes(input)) return arr
-            arr.push(input)
+            items.forEach((item) => {
+                if (item && !arr.includes(item)) {
+                    arr.push(item.trim())
+                }
+            })
             arr.sort()
         }
+        
         else {
-            let index = arr.findIndex((item) => item == input)
-            if (index >=0) {
-                arr.splice(index,1)
-            }
+            items.forEach((item) => {
+                let index = arr.findIndex((i) => i == item.trim())
+                if (index >=0) {
+                    arr.splice(index,1)
+                }
+            })
             arr.sort()
         }
-        // Reactivity hack
         return arr
+    }
+
+    // Debounce function
+    let debounceTimer: ReturnType<typeof setTimeout>;
+    function debounce(value:string,  timeout=300) {
+        clearTimeout(debounceTimer);
+            debounceTimer = setTimeout( () => {
+                filter_input_text = value.toLowerCase();
+                clearTimeout(debounceTimer);
+            }, timeout
+        )
     }
 
 </script>
@@ -74,7 +97,7 @@
 
         <slot />
 
-        <div class="flex flex-row flex-wrap lg:flex-nowrap gap-2 w-full mt-4">
+        <div class="flex flex-row flex-wrap lg:flex-nowrap gap-4 w-full mt-4">
                                             
             <div class="flex flex-col w-full gap-2 lg:w-1/2">
                 <form class="flex flex-row gap-2 mt-2 w-full" on:submit|preventDefault={(e) => {
@@ -93,31 +116,57 @@
                 </form>
             </div>
 
-            <div class="flex flex-col mt-2 gap-2 items-center max-h-[250px] w-full lg:w-1/2 overflow-y-scroll px-4">
+            <div class="flex flex-col w-full gap-2 lg:w-1/2">
                 {#if list.length > 0}
-                    {#each list as item}
-                        
-                        <div class="w-full rounded-md bg-slate-200 dark:bg-zinc-700 flex flex-row gap-2 items-center">
-                            <p class="pl-4 py-2 text-sm font-bold">{item}</p>
+                    
+                    {#if filterable}
+                        <!---Filter input--->
+                        <div class="flex flex-row gap-2 px-4 w-full items-center">
+                            <TextInput type="text" class="w-full" placeholder="Filter entries"
+                                bind:value={filter_input_text} 
+                                on:keyup={(e) => { 
+                                    debounce(e.detail.srcElement.value);
+                                }}
+                            />
 
-                            <div class="mx-auto"/>
-                            
-                            <Button color="ghost" class="mr-4 border-none" on:click={() => { 
-                                    list = updateArray(item, list, false); 
-                                    dispatcher('delete', item)
-                                }} 
+                            <!---Clear the filter text box--->
+                            <Button 
+                                color="primary"
+                                class="mr-4"
+                                title="Clear filter"
+                                on:click={() => { filter_input_text = '' }}
                             >
                                 <Icon src={XCircle} mini width={22}/>
                             </Button>
                         </div>
-                        
-                    {/each}
+                    {/if}
+
+                    <div class="flex flex-col mt-2 gap-2 items-center max-h-[250px] w-full overflow-y-scroll px-4">
+                        {#each list as item}
+                            <div class="w-full rounded-md bg-slate-200 dark:bg-zinc-700 flex flex-row gap-2 items-center"
+                                class:hidden={ filter_input_text && !item.toLowerCase().trim().includes(filter_input_text.toLowerCase().trim()) }
+                            >
+                                <p class="pl-2 py-2 text-sm font-bold">{item}</p>
+
+                                <div class="mx-auto"/>
+                                
+                                <Button color="ghost" class="mr-4 border-none" on:click={() => { 
+                                        list = updateArray(item, list, false); 
+                                        dispatcher('delete', item)
+                                    }} 
+                                >
+                                    <Icon src={XCircle} mini width={22}/>
+                                </Button>
+                            </div>
+                        {/each}
+                    </div>
                 {:else if showPlaceholder}
                     <span class="flex mx-auto my-auto">
-                        <Placeholder icon={ArchiveBoxXMark} title="No Items" description="" />
+                        <Placeholder icon={placeholderIcon} title="{placeholderTitle}" description="{placeholderText}" />
                     </span>
                 {/if}
             </div>
+            
         </div>
     </div>
 {/if}
