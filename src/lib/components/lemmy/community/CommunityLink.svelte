198d36fdbea90132e34ffc4381a7c052495beafe
Admiral Patrick <asimons@ptznetwork.org>
2024-07-05 16:36:39 -0400

    Add extra params to control link behavior

--- src/lib/components/lemmy/community/CommunityLink.svelte
+++ src/lib/components/lemmy/community/CommunityLink.svelte
@@ -2,14 +2,14 @@
     import type { Community } from 'lemmy-js-client'
     
     import { communityProfileModal } from '../moderation/moderation'
+    import { createEventDispatcher } from 'svelte'
     import { fixLemmyEncodings } from '$lib/components/lemmy/post/helpers'
     import { goto } from '$app/navigation'
     import { shortenCommunityName } from '$lib/components/lemmy/community/helpers'
     import { userSettings } from '$lib/settings.js'
     
     import Avatar from '$lib/components/ui/Avatar.svelte'
-    import CommunityProfileModal from '../modal/CommunityProfileModal.svelte'
-    
+  
 
     export let community: Community
     export let avatar: boolean = false
@@ -20,17 +20,20 @@
     export let heading:boolean = false
     export let boldCommunityName:boolean = true;
     export let useDisplayNames:boolean|undefined = undefined 
+    export let noClick:boolean = false
+
+    const dispatcher = createEventDispatcher()
 
     function linkFromCommunity(community: Community) {
         const domain = new URL(community.actor_id).hostname
         return `/c/${community.name}@${domain}`
     }
 </script>
-<!---href={href ?? linkFromCommunity(community)} --->
 
-<button  class="items-center flex flex-row gap-2 hover:underline {heading ? 'font-bold text-2xl' : ''}" 
+<button  class="items-center flex flex-row gap-2 {noClick ? 'pointer-events-none' : ''} hover:underline {heading ? 'font-bold text-2xl' : ''}" 
     title={fixLemmyEncodings(community.title)}
     on:click={() => {
+        dispatcher('click')
         if (href) goto(linkFromCommunity(community))
         else  communityProfileModal(community)
     }}
