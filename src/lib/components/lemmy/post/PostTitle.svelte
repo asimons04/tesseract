0a44ebdfa3b487cc62070bce2253ea9c0203b40c
Admiral Patrick <asimons@ptznetwork.org>
2024-07-07 07:25:48 -0400

    Add flairs to post title component

--- src/lib/components/lemmy/post/PostTitle.svelte
+++ src/lib/components/lemmy/post/PostTitle.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
     import type { CommentReplyView, PersonMentionView, PostView } from 'lemmy-js-client'
+    import { 
+        extractFlairsFromTitle,
+        fixLemmyEncodings, 
+        isPostView 
+    } from '$lib/components/lemmy/post/helpers'
     import { getInstance } from '$lib/lemmy.js'
     import { goto } from '$app/navigation'
-    import { fixLemmyEncodings, isPostView } from '$lib/components/lemmy/post/helpers'
     import { userSettings } from '$lib/settings.js'
     
+    import Badge from '$lib/components/ui/Badge.svelte';
+    
     export let post:PostView | CommentReplyView | PersonMentionView
+
+    // Extract any [flairs] from the post title and update the title to remove them.
+    let postName: string = post.post.name
+    let postFlairs:string[] = []
+    $:  {
+            let {name, flairs} = extractFlairsFromTitle(post.post.name)
+            postName = postName = name
+            postFlairs = postFlairs = flairs
+        }
 
 </script>
 
@@ -19,7 +34,23 @@
         class:dark:text-zinc-400={isPostView(post) && post.read && $userSettings.markReadPosts}
         title="{fixLemmyEncodings(post.post.name)}"
     >
-        <h1 class="text-base md:text-lg {(isPostView(post) && !post.read) || !$userSettings.markReadPosts ? 'font-bold' : ''}">{fixLemmyEncodings(post.post.name)}</h1>    
+        <h1 class="text-base md:text-lg flex flex-row gap-2 {(isPostView(post) && !post.read) || !$userSettings.markReadPosts ? 'font-bold' : ''}">
+            {postName}
+            <span class="flex flex-row gap-2 ml-auto text-xs items-center capitalize">
+                {#if postFlairs}
+                    {#each postFlairs as flair}
+                        <Badge color="orange" on:click={(e) => { 
+                                e.preventDefault()
+                                e.stopPropagation()
+                                goto(`/search?q=${encodeURIComponent(`[${flair}]`)}`)
+                            }}
+                        >
+                            {flair}
+                        </Badge>
+                    {/each}
+                {/if}
+            </span>
+        </h1> 
     </a>
 {:else}
     <button
@@ -30,7 +61,21 @@
         class:dark:text-zinc-400={isPostView(post) &&post.read && $userSettings.markReadPosts}
         title="{fixLemmyEncodings(post.post.name)}"
     >
-        <h1 class="text-base md:text-lg {(isPostView(post) && !post.read) || !$userSettings.markReadPosts ? 'font-bold' : ''}">{fixLemmyEncodings(post.post.name)}</h1>    
+        <h1 class="text-base md:text-lg flex flex-row gap-2 {(isPostView(post) && !post.read) || !$userSettings.markReadPosts ? 'font-bold' : ''}">
+            {postName}
+            <span class="flex flex-row gap-2 ml-auto text-xs items-center capitalize">
+                {#each postFlairs as flair}
+                    <Badge color="orange" on:click={(e) => { 
+                            e.preventDefault()
+                            e.stopPropagation()
+                            goto(`/search?q=${encodeURIComponent(`[${flair}]`)}`)
+                        }}
+                    >
+                        {flair}
+                    </Badge>
+                {/each}
+            </span>
+        </h1>    
     </button>
 {/if}
 
