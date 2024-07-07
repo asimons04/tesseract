c66252f428107d13dd782854a87dca1b23ed58ac
Admiral Patrick <asimons@ptznetwork.org>
2024-07-07 09:03:48 -0400

    Add params to include icon, add extra colors, add param to randomize badge color

--- src/lib/components/ui/Badge.svelte
+++ src/lib/components/ui/Badge.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
+    import { type IconSource, Icon } from 'svelte-hero-icons'
+    
     export let label: string = ''
+    export let color: BadgeColor = 'gray'
+    export let icon: IconSource | undefined = undefined
+    export let iconSize:number = 16
+    export let randomColor = false
 
     const badgeColor = {
         red: 'bg-red-200 dark:bg-red-900 text-red-800 dark:text-red-300',
         green: 'bg-green-200 dark:bg-green-900 text-green-800 dark:text-green-300',
         yellow: 'bg-yellow-200 dark:bg-yellow-900 text-yellow-800 dark:text-yellow-300',
         gray: 'bg-slate-200 dark:bg-zinc-800 text-gray-800 dark:text-gray-200',
+        orange: 'bg-orange-200 dark:bg-orange-800 text-orange-800 dark:text-orange-200',
+        purple: 'bg-fuchsia-200 dark:bg-fuchsia-800 text-fuchsia-800 dark:text-fuchsia-200',
+        pink: 'bg-pink-200 dark:bg-pink-800 text-pink-800 dark:text-pink-200',
     }
-
     type BadgeColor = keyof typeof badgeColor
-
-  export let color: BadgeColor = 'gray'
+    
+    let colors = Object.keys(badgeColor)
+    let colorClass = randomColor
+        ? badgeColor[colors[Math.floor(Math.random()*colors.length)] as BadgeColor]
+        : badgeColor[color]
 </script>
 
-<span class="px-2 py-0.5 rounded-full max-h-[20px] text-xs ml-auto font-bold flex items-center gap-1 whitespace-nowrap overflow-hidden {badgeColor[color]} {$$props.class}" title={label}>
+<button class="px-2 py-0.5 rounded-full max-h-[20px] text-xs ml-auto font-bold flex items-center gap-1 
+    whitespace-nowrap overflow-hidden 
+    {colorClass} 
+    {$$props.class}
+    " 
+    title={label} on:click
+>   
+    {#if icon}
+        <Icon src={icon} mini width={iconSize} />
+    {/if}
+
     <slot />
-</span>
+</button>
