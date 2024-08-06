0031ffafd25c290df024798441273fded73be042
Admiral Patrick <asimons@ptznetwork.org>
2024-08-06 10:14:22 -0400

    Remove unused components/old code, update invocation of Link to account for previews

--- src/lib/components/markdown/renderers/MarkdownLink.svelte
+++ src/lib/components/markdown/renderers/MarkdownLink.svelte
@@ -1,36 +1,13 @@
 <script lang="ts">
-    /*
-        Look for user setting to preview links
-        if off, just show a regular link
-
-        if on:
-            - Check if "internal" link.  Just make that a regular link
-            - If YouTube or something we can build an embed URL from, open a modal with the generated embed URL. Use getSiteMetaData() to async fill in the title/description
-            - Otherwise, open a modal and call the getSiteMetadata() endpiont for the URL
-            - Add a toggle switch to change the link preview behavior
-            - Add a link to continue to the resource
-
-    */
-    
+   
     import type { Tokens } from 'marked'
     
-    import IFrame from '$lib/components/lemmy/post/utils/IFrame.svelte';
     import Link from '$lib/components/input/Link.svelte';
-    
-    import { 
-        type PostType,
-        isYoutubeLikeVideo, 
-        buildYouTubeEmbedLink 
-    } from '$lib/components/lemmy/post/helpers'
     
     import { photonify } from '../markdown';
     import { userSettings } from '$lib/settings';
 
     export let token: Tokens.Link
-    //export let linkPreviews:boolean = true
-
-    let postType:PostType
-    let embedURL: URL | undefined
     
     let internalLink = photonify(token.href)
 
@@ -38,21 +15,16 @@
     if (internalLink) {
         token.href = photonify(token.href)!
     }
-
-    if (token.href && isYoutubeLikeVideo(token.href)) {
-        postType = 'youtube';
-        embedURL = buildYouTubeEmbedLink(token.href)
-    }
 </script>
 
-<Link highlight href={token.href} title={token.title ?? token.href} newtab={$userSettings.openInNewTab.links}>
+<Link highlight 
+    href={token.href} 
+    title={token.title ?? token.href} 
+    preview={
+        !(token.href.startsWith('/')) && 
+        $userSettings.uiState.linkPreviews
+    } 
+    newtab={$userSettings.openInNewTab.links}
+>
     <slot/>
 </Link>
-
-<!---
-{#if postType=='youtube' && embedURL}
-    <IFrame embedURL={embedURL} />
-{:else}
-
-{/if}
---->
