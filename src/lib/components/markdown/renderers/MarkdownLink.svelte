c73c790df8b53fa54f6a1b1273b24376596b7a64
Admiral Patrick <asimons@ptznetwork.org>
2024-08-06 19:33:04 -0400

    Move pre-processed community/user link logic into Link renderer

--- src/lib/components/markdown/renderers/MarkdownLink.svelte
+++ src/lib/components/markdown/renderers/MarkdownLink.svelte
@@ -1,30 +1,85 @@
 <script lang="ts">
-   
+    import type { Community, Person } from 'lemmy-js-client';
     import type { Tokens } from 'marked'
     
+    
+
+    import Badge from '$lib/components/ui/Badge.svelte';
     import Link from '$lib/components/input/Link.svelte';
     
-    import { photonify } from '../markdown';
+    import { createFakePerson, createFakeCommunity } from '$lib/components/lemmy/post/helpers'
+    import { 
+        type CustomMarkdownOptions,
+        photonify 
+    } from '../markdown';
+    import { userProfileModal, communityProfileModal } from '$lib/components/lemmy/moderation/moderation'
     import { userSettings } from '$lib/settings';
+    
 
     export let token: Tokens.Link
-    
-    let internalLink = photonify(token.href)
+    export let options: CustomMarkdownOptions
 
-    // Make the links relative to this instance
-    if (internalLink) {
-        token.href = photonify(token.href)!
+    let person: Person | undefined = undefined
+    let community: Community | undefined = undefined
+    
+    $: token, token.href = photonify(token.href) ?? token.href
+    $: token, person = generatePerson(token.href)
+    $: token, community = generateCommunity(token.href)
+
+    function generatePerson(text:string) {
+        if (!text.startsWith('/u/')) return
+
+        let username = text.replaceAll('/u/', '').trim()
+        const [user, domain] = username.split('@')
+        const actor_id =  `https://${domain.trim()}/u/${user}`
+        const person = createFakePerson()
+        person.actor_id = actor_id
+        person.name = user
+        return person
+    }
+
+    function generateCommunity(text:string) {
+        if (!text.startsWith('/c/')) return
+        let username = text.replaceAll('/c/', '').trim()
+        const [name, domain] = username.split('@')
+        const actor_id =  `https://${domain.trim()}/c/${name}`
+        const community = createFakeCommunity()
+        community.actor_id = actor_id
+        community.name = name
+        return community
     }
 </script>
 
-<Link highlight 
-    href={token.href} 
-    title={token.title ?? token.href} 
-    preview={
-        !(token.href.startsWith('/')) && 
-        $userSettings.uiState.linkPreviews
-    } 
-    newtab={$userSettings.openInNewTab.links}
->
-    <slot/>
-</Link>
+<!--- Turn user links into badges that load a user profile modal--->
+{#if person}
+    <Badge color="blue" rightJustify={false} inline={true} on:click={() => {
+        if (person) userProfileModal(person)
+    }}>
+        @{person.name}@{new URL(person.actor_id).hostname}
+    </Badge>
+
+<!--- Turn community links into badges that load a community profile modal--->
+{:else if community}
+    <Badge color="orange" rightJustify={false} inline={true} on:click={() => {
+        if (community) communityProfileModal(community)
+    }}>
+        !{community.name}@{new URL(community.actor_id).hostname}
+    </Badge>
+
+<!---Display a regular link--->
+{:else}
+    <Link highlight 
+        href={token.href} 
+        title={token.title ?? token.href} 
+        preview={
+            !(token.href.startsWith('/')) && 
+            $userSettings.uiState.linkPreviews &&
+            !(options?.custom?.noPreview ?? false)
+        } 
+        newtab={$userSettings.openInNewTab.links}
+    >
+        <slot/>
+    </Link>
+
+    
+{/if}
