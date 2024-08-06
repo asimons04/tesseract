cf48c288e653b86858f875ca82ac372176b92020
Admiral Patrick <asimons@ptznetwork.org>
2024-08-06 10:10:10 -0400

    Add preview param option for Link component

--- src/lib/components/input/Link.svelte
+++ src/lib/components/input/Link.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { linkPreviewModal } from "$lib/components/lemmy/moderation/moderation";
 
     export let href: string|undefined
     export let highlight: boolean = false
@@ -8,25 +9,37 @@
     export let id: string = ""
     export let domainOnly:boolean = false;
     export let text:string | undefined = undefined
+    export let preview:boolean = false
 
 </script>
 {#if href}
-<a {href}
-    id={id}
-    class="{highlight ? 'text-sky-700 dark:text-sky-500' : ''} hover:underline max-w-full"
-    rel="nofollow"
-    target="{newtab
-        ? '_blank'
-        : '_self'
-    }"
-    title = "{title ?? href}"
->
-    <slot name="icon" />
-    <slot>
-        <span class="flex gap-0  {nowrap ? 'w-full whitespace-nowrap overflow-hidden text-ellipsis text-xs' : ''}  {$$props.class}" >
-            {text ?? (domainOnly ? new URL(href).hostname : href)}
-        </span>
-    </slot>
-  
-</a>
+    <a {href}
+        id={id}
+        class="{highlight ? 'text-sky-700 dark:text-sky-500' : ''} text-left hover:underline max-w-full"
+        rel="nofollow"
+        target="{newtab
+            ? '_blank'
+            : '_self'
+        }"
+        on:click={
+            (
+                //@ts-ignore
+                e
+            ) => {
+                if (preview) {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    linkPreviewModal(href)
+                }
+            }}
+        title = "{title ?? href}"
+    >
+        <slot name="icon" />
+        <slot>
+            <span class="flex gap-0  {nowrap ? 'w-full whitespace-nowrap overflow-hidden text-ellipsis text-xs' : ''}  {$$props.class}" >
+                {text ?? (domainOnly ? new URL(href).hostname : href)}
+            </span>
+        </slot>
+    
+    </a>
 {/if}
