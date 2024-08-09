cbb5fae1af24a1e82d9e53d1c3e947aa10e177d6
Admiral Patrick <asimons@ptznetwork.org>
2024-08-09 09:21:35 -0400

    Rewrite mod team list, add transfer community ability, indicate top mod/owner

--- src/routes/c/[name]/settings/team/+page.svelte
+++ src/routes/c/[name]/settings/team/+page.svelte
@@ -2,19 +2,19 @@
     import type { PageData } from './$types.js'
     
     import { flip } from 'svelte/animate'
+    import { isAdmin } from '$lib/components/lemmy/moderation/moderation.js'
     import { getClient } from '$lib/lemmy.js'
     import { profile } from '$lib/auth.js'
     import { toast } from '$lib/components/ui/toasts/toasts.js'
 
     import Avatar from '$lib/components/ui/Avatar.svelte'
     import Button from '$lib/components/input/Button.svelte'
-    import EditableList from '$lib/components/ui/list/EditableList.svelte'
-    import FeedContainer from '$lib/components/ui/containers/FeedContainer.svelte'
+    import Card from '$lib/components/ui/Card.svelte'
     import MainContentArea from '$lib/components/ui/containers/MainContentArea.svelte'
     import TextInput from '$lib/components/input/TextInput.svelte'
     import UserLink from '$lib/components/lemmy/user/UserLink.svelte'
     
-    import { Icon, Plus, Trash } from 'svelte-hero-icons'
+    import { Icon, Plus, ShieldCheck, Trash } from 'svelte-hero-icons'
     
     
     export let data: PageData
@@ -105,6 +105,23 @@
             })
         }
     }
+
+
+    async function transferCommunity(personID: number) {
+        if (!$profile?.jwt) return
+        try {
+            const res = await getClient().transferCommunity({
+                community_id: data.community.community_view.community.id,
+                person_id: personID
+            })
+
+            data.community.moderators = res.moderators
+        }
+        catch {
+
+        }
+
+    }
 </script>
 
 <svelte:head>
@@ -113,38 +130,66 @@
 
 <MainContentArea>
     <h1 class="font-bold text-2xl">Moderator Team</h1>
-    <EditableList let:action
-        on:action={(e) => {
-            toast({
-                content: `Are you sure you want to remove ${e.detail.name} as a moderator?`,
-                action: () => removeMod(e.detail.id),
-                title: 'Confirm'
-            })
-        }}
-    >
-        {#each data.community.moderators as moderator (moderator.moderator.id)}
-            <div class="py-4 flex items-center gap-2 justify-between" animate:flip={{ duration: 300 }} >
-                <div class="flex gap-2 items-center">
-                    <Avatar width={28} url={moderator.moderator.avatar} alt={moderator.moderator.name} />
-                    <div class="flex flex-col gap-0">
-                        <UserLink user={moderator.moderator} showInstance={false} />
-                        <span class="text-xs text-slate-600 dark:text-zinc-400">
-                            {new URL(moderator.moderator.actor_id).hostname}
-                        </span>
+    
+    <p class="font-normal text-sm">
+        Add or remove members of the moderation team.  The "top mod" is indicated with a green shield and is considered the owner of the community.
+        {#if isAdmin($profile?.user)}
+            To assign someone else as the top mod, use the transfer community button on their entry.
+        {/if}
+    </p>
+
+
+    <Card class="p-2">
+        <div class="divide-y divide-slate-200 dark:divide-zinc-500 flex flex-col">
+            {#each data.community.moderators as moderator, i (moderator.moderator.id)}
+                <div class="py-4 flex items-center gap-2 justify-between" animate:flip={{ duration: 300 }} >
+                    <div class="flex gap-2 items-center">
+                        <Avatar width={28} url={moderator.moderator.avatar} alt={moderator.moderator.name} />
+                        <div class="flex flex-col gap-0">
+
+                            <div class="flex flex-row items-center">
+                                <UserLink user={moderator.moderator} showInstance={false} />
+                                <!--- Badge "top mod" which I am just now realizing is a thing--->
+                                {#if i == 0 }
+                                    <span class="text-green-500">
+                                        <Icon src={ShieldCheck} size="16" mini />
+                                    </span>
+                                {/if}
+                            </div>
+
+                            <span class="text-xs text-slate-600 dark:text-zinc-400">
+                                {new URL(moderator.moderator.actor_id).hostname}
+                            </span>
+                        </div>
+                    </div>
+            
+                    <div class="flex flex-row gap-2">
+                        {#if isAdmin($profile?.user)}
+                            <Button size="square-md" title="Transfer Community" on:click={() => transferCommunity(moderator.moderator.id)} icon={ShieldCheck} disabled={i == 0}/>
+                        {/if}
+                        <Button size="square-md" title="Remove as Moderator" on:click={() => removeMod(moderator.moderator.id)} icon={Trash} />
                     </div>
                 </div>
-        
-                <Button size="square-md" on:click={() => action(moderator.moderator)} icon={Trash} />
+            {/each}
+            
+            <!---Input Form to Add New Moderator--->
+            <div class="flex flex-row gap-2 pt-4 w-full items-end">
+                <TextInput bind:value={formData.newModerator} class="w-full" placeholder="@user@example.com or https://example.com/u/user" label="Add a New Moderator to the Team"
+                    on:keydown={(e) => {
+                        if (e.detail.key == 'Enter') {
+                            addModerator()
+                        }
+                    }}
+                />
+                
+                <Button loading={formData.addingModerator} disabled={formData.addingModerator} color="primary" size="md" class="w-max h-8 flex-shrink-0 h-" on:click={addModerator}>
+                    <Icon slot="icon" src={Plus} mini size="16" />Add moderator
+                </Button>
             </div>
-        {/each}
 
-        <div class="flex flex-row gap-2 pt-4 w-full items-end">
-            <TextInput bind:value={formData.newModerator} class="w-full" placeholder="@user@example.com or https://example.com/u/user" label="Add a New Moderator to the Team"/>
-            <Button loading={formData.addingModerator} disabled={formData.addingModerator} color="primary" size="md" class="w-max h-8 flex-shrink-0 h-" on:click={addModerator}>
-                <Icon slot="icon" src={Plus} mini size="16" />Add moderator
-            </Button>
+            
         </div>
-    </EditableList>
+    </Card>
 </MainContentArea>
 
 
