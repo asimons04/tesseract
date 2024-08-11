3f905240d2cee93fa99fef0eb1f99a9b5cfce94b
Admiral Patrick <asimons@ptznetwork.org>
2024-08-11 09:53:10 -0400

    Put quick settings into wider/row-column layout on desktop

--- src/lib/components/lemmy/modal/QuickSettingsModal.svelte
+++ src/lib/components/lemmy/modal/QuickSettingsModal.svelte
@@ -41,7 +41,7 @@
 </script>
 
 <!-- svelte-ignore missing-declaration -->
-<Modal bind:open icon={Cog6Tooth} card  width="max-w-lg" title="Quick Settings" >
+<Modal bind:open icon={Cog6Tooth} card  width="max-w-4xl" title="Quick Settings" >
     
     
     <div class="flex flex-col w-full p-2 gap-2 cursor-default">
@@ -78,60 +78,69 @@
         {/if}
 
         <!---User Settings--->
-        <div class="flex flex-col gap-2 items-center pr-2 max-h-[70vh] divide-y overflow-y-scroll">
-            
-            <!---Post Style--->
-            <SettingToggle title="Show Compact Posts" icon={$userSettings.showCompactPosts ? QueueList : Photo}  small={true}
-                bind:value={$userSettings.showCompactPosts} 
-                on:change={async (e) => {
-                    $userSettings.uiState.feedMargins = !e.detail
-                    await scrollToLastSeenPost()
-                }}
-            />
-            
-            <!---Open in New Tab--->
-            <SettingToggle icon={ArrowTopRightOnSquare} title="Open Links in New Tab" bind:value={$userSettings.openInNewTab.links} small={true} />
+        <div class="flex flex-col divide-y lg:flex-row lg:divide-y-0 lg:gap-4 pr-2 max-h-[70vh] items-start w-full overflow-y-scroll">
+        
+        
+            <div class="flex flex-col gap-2 items-center divide-y w-full lg:w-1/2">
+                
+                <!---Post Style--->
+                <SettingToggle title="Show Compact Posts" icon={$userSettings.showCompactPosts ? QueueList : Photo}  small={true}
+                    bind:value={$userSettings.showCompactPosts} 
+                    on:change={async (e) => {
+                        $userSettings.uiState.feedMargins = !e.detail
+                        await scrollToLastSeenPost()
+                    }}
+                />
+                
+                <!---Open in New Tab--->
+                <SettingToggle icon={ArrowTopRightOnSquare} title="Open Links in New Tab" bind:value={$userSettings.openInNewTab.links} small={true} />
 
-            <!---Open Posts in New Tab--->
-            <SettingToggle title="Open Posts in New Tab" icon={ArrowTopRightOnSquare} bind:value={$userSettings.openInNewTab.posts} small={true} />
+                <!---Open Posts in New Tab--->
+                <SettingToggle title="Open Posts in New Tab" icon={ArrowTopRightOnSquare} bind:value={$userSettings.openInNewTab.posts} small={true} />
 
-            <!---Use Link Preview Modals--->
-            <SettingToggle icon={LinkIcon} title="Preview Links in Modal" bind:value={$userSettings.uiState.linkPreviews} small={true}/>
+                <!---Use Link Preview Modals--->
+                <SettingToggle icon={LinkIcon} title="Preview Links in Modal" bind:value={$userSettings.uiState.linkPreviews} small={true}/>
 
 
-            <!---Use Display Names--->
-            <SettingToggle icon={Identification} title="Use Display Names" bind:value={$userSettings.displayNames} small={true}/>
+                <!---Use Display Names--->
+                <SettingToggle icon={Identification} title="Use Display Names" bind:value={$userSettings.displayNames} small={true}/>
 
-            <!---Show Instances--->
-            <SettingToggle icon={Server} title="Show Instance Names" bind:value={$userSettings.uiState.showInstances} small={true}/>
-            
-            <!---Show Scores--->
-            <SettingToggle title="Show Scores" icon={Trophy} bind:value={$userSettings.uiState.showScores} small={true} />
-            
-            <!---Disable Downvotes--->
-            {#if $site?.site_view?.local_site?.enable_downvotes}
-                <SettingToggle icon={ArrowDown} title="Disable Downvotes"  bind:value={$userSettings.uiState.disableDownvotes} small={true} />
-            {/if}
-            <!---Reverse Action Bar--->
-            <SettingToggle title="Reverse Action Bar" icon={ArrowsRightLeft} bind:value={$userSettings.uiState.reverseActionBar} small={true}/>
+                <!---Show Instances--->
+                <SettingToggle icon={Server} title="Show Instance Names" bind:value={$userSettings.uiState.showInstances} small={true}/>
+                
+                <!---Show Scores--->
+                <SettingToggle title="Show Scores" icon={Trophy} bind:value={$userSettings.uiState.showScores} small={true} />
+                
+                 <!---Disable Downvotes--->
+                 {#if $site?.site_view?.local_site?.enable_downvotes}
+                    <SettingToggle icon={ArrowDown} title="Disable Downvotes"  bind:value={$userSettings.uiState.disableDownvotes} small={true} />
+                {/if}
+               
+            </div>
 
-            <!---Expand Crosspost List--->
-            <SettingToggle title="Expand Crosspost List" icon={BarsArrowDown} bind:value={$userSettings.uiState.expandCrossPosts} small={true}/>
+            <div class="flex flex-col gap-2 items-center divide-y w-full lg:w-1/2">
 
-            <!---Match Crosspost on Title--->
-            <SettingToggle title="Match Crossposts on Title" icon={BarsArrowDown} bind:value={$userSettings.uiState.matchCrossPostOnTitle} small={true}/>
-            
-            <!---Enable Embeds in Feed--->
-            <SettingToggle title="Enable Embeds in Feed" icon={Film} bind:value={$userSettings.embeddedMedia.feed} small={true}/>
+                <!---Reverse Action Bar--->
+                <SettingToggle title="Reverse Action Bar" icon={ArrowsRightLeft} bind:value={$userSettings.uiState.reverseActionBar} small={true}/>
 
-            <!---Enable Hashtags--->
-            <SettingToggle title="Enable Hashtags" icon={Hashtag} bind:value={$userSettings.linkifyHashtags} small={true} />
+                <!---Expand Crosspost List--->
+                <SettingToggle title="Expand Crosspost List" icon={BarsArrowDown} bind:value={$userSettings.uiState.expandCrossPosts} small={true}/>
 
-            <!---Enable Flairs--->
-            <SettingToggle title="Enable Flairs" icon={Tag} bind:value={$userSettings.extractFlairsFromTitle} small={true} />
+                <!---Match Crosspost on Title--->
+                <SettingToggle title="Match Crossposts on Title" icon={BarsArrowDown} bind:value={$userSettings.uiState.matchCrossPostOnTitle} small={true}/>
 
-            <!---Hide Posts from Users of Blocked Instances--->
-            <SettingToggle title="Hide Users From Blocked Instances" icon={EyeSlash} bind:value={$userSettings.hidePosts.hideUsersFromBlockedInstances} small={true}/>
+                <!---Enable Embeds in Feed--->
+                <SettingToggle title="Enable Embeds in Feed" icon={Film} bind:value={$userSettings.embeddedMedia.feed} small={true}/>
+
+                <!---Enable Hashtags--->
+                <SettingToggle title="Enable Hashtags" icon={Hashtag} bind:value={$userSettings.linkifyHashtags} small={true} />
+
+                <!---Enable Flairs--->
+                <SettingToggle title="Enable Flairs" icon={Tag} bind:value={$userSettings.extractFlairsFromTitle} small={true} />
+
+                <!---Hide Posts from Users of Blocked Instances--->
+                <SettingToggle title="Hide Users From Blocked Instances" icon={EyeSlash} bind:value={$userSettings.hidePosts.hideUsersFromBlockedInstances} small={true}/>
+            </div>
         </div>
     </div>
 
