47c9454c132ac40dcacd2058431cf75c34f5de2e
Admiral Patrick <asimons@ptznetwork.org>
2024-08-12 15:48:46 -0400

    Combine ban instance and ban community modals again

--- src/lib/components/lemmy/moderation/BanModal.svelte
+++ src/lib/components/lemmy/moderation/BanModal.svelte
@@ -1,30 +1,34 @@
 <script lang="ts">
-    import type { Community, Person, PersonView } from 'lemmy-js-client'
-
+    import type { Community, Person } from 'lemmy-js-client'
+    
+    import { createEventDispatcher } from 'svelte';
+    import { dispatchWindowEvent } from '$lib/ui/events'
     import { getClient } from '$lib/lemmy.js'
     import { profile } from '$lib/auth.js'
     import { toast } from '$lib/components/ui/toasts/toasts.js'
 
     import Avatar from '$lib/components/ui/Avatar.svelte'
     import Button from '$lib/components/input/Button.svelte'
-    import Checkbox from '$lib/components/input/Checkbox.svelte'
-    import DateInput from '$lib/components/input/DateInput.svelte'
+    import MarkdownEditor from '$lib/components/markdown/MarkdownEditor.svelte'
     import Modal from '$lib/components/ui/modal/Modal.svelte'
-    import TextArea from '$lib/components/input/TextArea.svelte'
-    import UserItem from '$lib/components/lemmy/user/UserItem.svelte'
-    
-    import {
-        Icon,
-        Check,
-        NoSymbol
-    } from 'svelte-hero-icons'
+    import SettingDateInput from '$lib/components/ui/settings/SettingDateInput.svelte';
+    import SettingToggle from '$lib/components/ui/settings/SettingToggle.svelte'
+    import SettingToggleContainer from '$lib/components/ui/settings/SettingToggleContainer.svelte'
 
-    let item: Person | undefined
+    import {
+        CalendarDays,
+        Check,
+        NoSymbol,
+        Trash
+    } from 'svelte-hero-icons'
+    
 
     export let open = false
-    export { item as user }
-    export let community: Community | undefined = undefined
+    export let user: Person | undefined
+    export let community: Community | undefined
     export let banned: boolean
+    
+    const dispatcher = createEventDispatcher<{ban:boolean}>()
 
     let reason = ''
     let deleteData = false
@@ -37,7 +41,7 @@
         deleteData = false
         expires = ''
     }
-    $: if (item) resetReason()
+    $: if (user) resetReason()
 
     function invalidDateErrorToast() {
         toast({
@@ -48,7 +52,7 @@
     }
 
     async function submit() {
-        if (!item || !$profile?.user || !$profile?.jwt) return
+        if (!user || !$profile?.user || !$profile?.jwt) return
         
         loading = true
 
@@ -64,75 +68,106 @@
                 }
             }
 
+            // Ban from community if `community` is provided in the call
             if (community) {
                 await getClient().banFromCommunity({
                     ban: !banned,
                     community_id: community.id,
-                    person_id: item.id,
+                    person_id: user.id,
                     reason: reason || undefined,
                     remove_data: deleteData,
                     expires: date ? Math.floor(date / 1000) : undefined,
                 })
-            } else {
+                
+                // Dispatch global event so other components can react
+                dispatchWindowEvent('banCommunity', {
+                    person_id: user.id,
+                    community_id: community.id,
+                    banned: !banned,
+                    remove_content: deleteData
+                })
+
+            }
+            
+            // Ban from instance if no community provided
+            else {
                 await getClient().banPerson({
                     ban: !banned,
-                    person_id: item.id,
+                    person_id: user.id,
                     reason: reason || undefined,
                     remove_data: deleteData,
                     expires: date ? Math.floor(date / 1000) : undefined,
                 })
+
+                // Dispatch global event so other components can react
+                dispatchWindowEvent('banUser', {
+                    person_id: user.id,
+                    banned: !banned,
+                    remove_content: deleteData
+                })
             }
+            
             open = false
+            banned = !banned
+            
             toast({
-                content: `Successfully 
-                    ${ banned ? 'unbanned' : 'banned'} 
-                    that user. You may need to refresh to see changes.
-                `,
+                content: `Successfully ${ banned ? 'banned' : 'unbanned'}  ${user.name}@${new URL(user.actor_id).hostname} ${community ? 'from the community' : 'from the instance'}.`,
                 type: 'success',
                 title: 'Success'
             })
-            //item.banned = !banned
-            banned = !banned
+            
+            
         } catch (err) {
             toast({
                 content: err as any,
                 type: 'error',
+                title: 'Error'
             })
         }
         loading = false
+        //dispatcher('ban', banned)
     }
 </script>
 
-<Modal bind:open title="{banned ? 'Unbanning' : 'Banning'} User" icon={banned ? Check : NoSymbol}  width="max-w-2xl">
+<Modal bind:open title="{banned ? 'Unbanning' : 'Banning'} User From {community ? 'Community' : 'Instance'}" icon={banned ? Check : NoSymbol}  width="max-w-2xl">
     
-    {#if item}
+    {#if user}
         <form class="flex flex-col gap-4" on:submit|preventDefault={submit}>
-            <div class="flex items-center gap-1">
-                <Avatar url={item.avatar} alt={item.name} width={24} />
-                <span class="font-bold">{item.name}</span>
-            </div>
-            <p>
-                {banned ? 'Unbanning' : 'Banning'} from
-                <span class="font-bold">{community ? community.name : 'site'}</span>
-            </p>
+            
+            <div class="flex flex-col gap-1">
+                <div class="flex flex-row gap-1 items-center">
+                    <Avatar url={user.avatar} alt={user.name} width={24} />
+                    <span class="font-bold">{user.name}@{new URL(user.actor_id).hostname}</span>
+                </div>
+            
+            
+                {#if community}
+                    <span class="flex gap-1">
+                        {banned ? 'Unbanning' : 'Banning'} from
+                        <span class="font-bold">{community.name}@{new URL(community.actor_id).hostname}</span>
+                    </span>
+                {/if}
 
-            <TextArea required bind:value={reason} label="Reason"
-                placeholder="Why are you {banned
-                    ? 'unbanning'
-                    : 'banning'} {item.name}?"
-            />
+            </div>
+
+
+            <MarkdownEditor required previewButton images={false} rows={6} 
+                bind:value={reason} label="Reason"
+                placeholder="Why are you { banned ? 'unbanning' : 'banning'} {user.name}@{new URL(user.actor_id).hostname}?"
+            >
+                <Button submit color="primary" {loading} disabled={loading} size="lg" slot="actions">
+                    {banned ? 'Unban' : 'Ban'}
+                </Button>
+            </MarkdownEditor>
 
             {#if !banned}
-                <Checkbox bind:checked={deleteData}>Delete data</Checkbox>
-                <DateInput
-                    bind:value={expires}
-                    label="Ban Expires (Leave blank for permanent ban)"
-                />
+                <SettingToggleContainer>
+                    <SettingToggle bind:value={deleteData} icon={Trash} title="Remove Content" description="Remove all of this user's content when banning." />
+                    <SettingDateInput bind:value={expires} icon={CalendarDays} title="Ban Expires" description="To effect a temporary ban, enter a date for the ban to expire. Leave blank for a permanent ban." />
+                </SettingToggleContainer>
             {/if}
         
-            <Button submit color="primary" {loading} disabled={loading} size="lg">
-                Submit
-            </Button>
+            
         </form>
     {/if}
 </Modal>
