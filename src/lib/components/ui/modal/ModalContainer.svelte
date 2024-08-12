2189dd37f6204fdb163663933a3ae3061e6c7b76
Admiral Patrick <asimons@ptznetwork.org>
2024-08-12 19:57:33 -0400

    Async load lesser-used modals, move ban user modal below profile

--- src/lib/components/ui/modal/ModalContainer.svelte
+++ src/lib/components/ui/modal/ModalContainer.svelte
@@ -2,10 +2,10 @@
     import { modals } from '$lib/components/lemmy/moderation/moderation.js'
     
     import CommunityProfileModal from '$lib/components/lemmy/modal/CommunityProfileModal.svelte';
-    import FederationStateModal from '$lib/components/lemmy/modal/FederationStateModal.svelte'
-    import Fediseer from '$lib/fediseer/Fediseer.svelte';
+    //import FederationStateModal from '$lib/components/lemmy/modal/FederationStateModal.svelte'
+    //import Fediseer from '$lib/fediseer/Fediseer.svelte';
     import LinkPreviewModal from '$lib/components/lemmy/modal/LinkPreviewModal.svelte';
-    import PostCommentVoteShowModal from '$lib/components/lemmy/modal/PostCommentVoteShowModal.svelte'
+    //import PostCommentVoteShowModal from '$lib/components/lemmy/modal/PostCommentVoteShowModal.svelte'
     import QuickSettingsModal from '$lib/components/lemmy/modal/QuickSettingsModal.svelte'
     import UserProfileModal from '$lib/components/lemmy/modal/UserProfileModal.svelte'
     import ZoomImageModal from '$lib/components/lemmy/modal/ZoomImageModal.svelte';
@@ -36,32 +36,27 @@
   {/await}
 {/if}
 
-<!---Ban User Modal--->
-{#if $modals.banning.open}
-  {#await import('$lib/components/lemmy/moderation/BanModal.svelte') then { default: BanModal }}
-    <BanModal
-      bind:open={$modals.banning.open}
-      banned={$modals.banning.banned}
-      user={$modals.banning.user}
-      community={$modals.banning.community}
-    />
-  {/await}
-{/if}
 
 
-
+<!--- Post/Comment Vote Viewer Modal--->
 {#if $modals.votes.open}
-    <PostCommentVoteShowModal bind:open={$modals.votes.open} type={$modals.votes.type} submission_id={$modals.votes.submission_id} />
+    {#await import('$lib/components/lemmy/modal/PostCommentVoteShowModal.svelte') then { default: PostCommentVoteShowModal }}
+        <PostCommentVoteShowModal bind:open={$modals.votes.open} type={$modals.votes.type} submission_id={$modals.votes.submission_id} />
+    {/await}
 {/if}
 
 <!---Fediseer Modal--->
 {#if $modals.fediseer.open}
-    <Fediseer bind:open={$modals.fediseer.open} instance={$modals.fediseer.instance} />
+    {#await import('$lib/fediseer/Fediseer.svelte') then { default: Fediseer }}
+        <Fediseer bind:open={$modals.fediseer.open} instance={$modals.fediseer.instance} />
+    {/await}
 {/if}
 
 <!---Federation State Viewer--->
 {#if $modals.federationState.open}
-    <FederationStateModal bind:open={$modals.federationState.open} domain={$modals.federationState.domain} />
+    {#await import('$lib/components/lemmy/modal/FederationStateModal.svelte') then { default: FederationStateModal}}
+        <FederationStateModal bind:open={$modals.federationState.open} domain={$modals.federationState.domain} />
+    {/await}
 {/if}
 
 <!---Quick Settings Modal--->
@@ -78,6 +73,20 @@
 {#if $modals.user.open}
     <UserProfileModal  bind:open={$modals.user.open} user={$modals.user.user} mod={$modals.user.mod} />
 {/if}
+
+<!---Ban User Modal--->
+{#if $modals.banning.open}
+  {#await import('$lib/components/lemmy/moderation/BanModal.svelte') then { default: BanModal }}
+    <BanModal
+      bind:open={$modals.banning.open}
+      banned={$modals.banning.banned}
+      user={$modals.banning.user}
+      community={$modals.banning.community}
+    />
+  {/await}
+{/if}
+
+
 
 {#if $modals.linkPreview.open}
     <LinkPreviewModal bind:open={$modals.linkPreview.open} url={$modals.linkPreview.url} />
