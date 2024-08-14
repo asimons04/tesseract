6f0fb3c0a1abe3b6f00f16ebccab8bc22526b2ca
Admiral Patrick <asimons@ptznetwork.org>
2024-08-14 17:35:19 -0400

    Get preview length from user settings

--- src/lib/components/lemmy/post/PostBody.svelte
+++ src/lib/components/lemmy/post/PostBody.svelte
@@ -21,15 +21,26 @@
     export let postContainer: HTMLDivElement
     export let displayType:PostDisplayType  = 'post'
     export let expandPreviewText:boolean    = false
-    export let previewLength:number         = 300
     export let inline:boolean               = false
     
-    // Use the embed description from the metadata, if available, if no post body is provided.
-    $:  post, post.post.body = post.post.body ??  ( 
+    let source: string
+    
+    function generateSource() {
+        let text = post.post.body ??  ( 
             post.post.embed_description 
                 ? `**Summary**: ${post.post.embed_description}`
                 : ''
         )
+
+        text = !expandPreviewText && text.length > $userSettings.uiState.postBodyPreviewLength
+            ? text.slice(0, $userSettings.uiState.postBodyPreviewLength)
+            : text
+        
+        return text
+
+    }
+    // Use the embed description from the metadata, if available, if no post body is provided.
+    $:  (post, $userSettings.uiState.postBodyPreviewLength), source = generateSource()
 
 </script>
 
@@ -44,7 +55,7 @@
         <!--- Show expandable preview in feed--->
         {#if displayType=='feed'}
             <div class="
-                {!expandPreviewText && !post.post.nsfw && post.post.body.length > previewLength
+                {!expandPreviewText && !post.post.nsfw && post.post.body.length > $userSettings.uiState.postBodyPreviewLength
                     ? 'bg-gradient-to-b text-transparent from-slate-800 via-slate-800 dark:from-zinc-100 dark:via-zinc-100 bg-clip-text z-0'
                     : ''
                 }
@@ -52,17 +63,13 @@
                 <Markdown 
                     class="{post.post.nsfw && $userSettings.nsfwBlur ? 'blur-sm' : ''}"
                     {inline}
-                    source={
-                        !expandPreviewText && post.post.body.length > previewLength
-                            ? post.post.body.slice(0, previewLength)
-                            : post.post.body
-                    }
+                    bind:source
                     
                 />
             </div>
 
             <!---Expand/Collapse Button--->
-            {#if (post.post.body.length > previewLength) || post.post.nsfw}
+            {#if (post.post.body.length > $userSettings.uiState.postBodyPreviewLength) || post.post.nsfw}
                 <Button color="tertiary" class="mx-auto w-fit text-xs font-bold !py-0"
                     title="{expandPreviewText ? 'Collapse' : 'Expand'} {post.post.nsfw && $userSettings.nsfwBlur? 'NSFW Text' : ''}"
                     on:click={() => {
