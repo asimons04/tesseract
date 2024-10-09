ce75a9a1ee1db344a1f176cb79def97db0cecb8f
Admiral Patrick <asimons@ptznetwork.org>
2024-10-09 13:17:57 -0400

    Use thumbnail, if available, for direct video embeds. Add click to play

--- src/lib/components/lemmy/post/PostVideo.svelte
+++ src/lib/components/lemmy/post/PostVideo.svelte
@@ -7,8 +7,9 @@
     import { userSettings } from '$lib/settings'
 
     import Link from '$lib/components/input/Link.svelte'
-    import PostIsInViewport from './utils/PostIsInViewport.svelte'
     import NSFWOverlay from '$lib/components/lemmy/post/utils/NSFWOverlay.svelte'
+    import PostIsInViewport from './utils/PostIsInViewport.svelte'
+    import PostImage from './PostImage.svelte';
     
     export let post: PostView 
     export let autoplay:boolean = false;
@@ -16,44 +17,71 @@
     export let displayType:PostDisplayType = 'feed'
     export let postContainer: HTMLDivElement
 
+    let clickToPlayClicked = false
+    let muted = autoplay
+    let inViewport = false
+
+
     $: source = post.post.url && isVideo(post.post.url) 
                 ? imageProxyURL(post.post.url)
                 : post.post.embed_video_url && isVideo(post.post.embed_video_url)
                     ? imageProxyURL(post.post.embed_video_url)
                     : undefined
     
-    let muted = autoplay
-    let inViewport = false
+    $:  showAsEmbed = (
+        (clickToPlayClicked && inViewport) ||
+        (   displayType == 'feed' && 
+            inViewport && 
+            $userSettings.embeddedMedia.feed && 
+            (!post.post.nsfw || !$userSettings.nsfwBlur)
+        ) ||
+        (displayType == 'post' && $userSettings.embeddedMedia.post)
+    )
+
+    // If there is no thumbnail URL, use a transparent PNG that will have the play button overlaid onto it.
+    $:  post.post.thumbnail_url = post.post.thumbnail_url ?? '/img/transparent.png'
+    
+    // Unset click to play when out of viewport (revert to thumbnail)
+    $:  if (!inViewport) clickToPlayClicked = false
+    
+    function clickToPlay() {
+        if (source) { 
+            clickToPlayClicked = true
+            autoplay = true
+        }
+    }
 
 
 </script>
 
 <PostIsInViewport bind:postContainer bind:inViewport />
 
-{#if post && source}
 <Link  href={post.post.url} title={post.post.url} newtab={$userSettings.openInNewTab.links}   domainOnly={!$userSettings.uiState.showFullURL} highlight nowrap  />
 
-<div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 m-1 rounded-2xl max-w-full p-1">
-    <div class="ml-auto mr-auto mt-1 mb-1 max-w-full">
-        
-        <NSFWOverlay bind:nsfw={post.post.nsfw} displayType={displayType} />
-        
-        {#if inViewport}
-        <video class="rounded-2xl max-w-full max-h-[75vh] max-w-[88vw] mx-auto" 
-            class:blur-2xl={(post.post.nsfw && $userSettings.nsfwBlur && displayType=='feed')}    
-            controls playsinline {muted} {autoplay}  {loop}
-        >
-            <source src="{source}" type="{
-                new URL(source).pathname.endsWith('mp4') || new URL(source).pathname.endsWith('m4v')
-                    ? 'video/mp4' 
-                    : new URL(source).pathname.endsWith('webm') 
-                        ? "video/webm" 
-                        : new URL(source).pathname.endsWith('mov') 
-                            ? "video/mp4"
-                            : ''
-            }" />
-        </video>
-        {/if}
+{#if source && showAsEmbed}
+    <div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 m-1 rounded-2xl max-w-full p-1">
+        <div class="ml-auto mr-auto mt-1 mb-1 max-w-full">
+            
+            <NSFWOverlay bind:nsfw={post.post.nsfw} displayType={displayType} />
+            
+            <video class="rounded-2xl max-w-full max-h-[75vh] max-w-[88vw] mx-auto" 
+                class:blur-2xl={(post.post.nsfw && $userSettings.nsfwBlur && displayType=='feed')}    
+                controls playsinline {muted} {autoplay}  {loop}
+            >
+                <source src="{source}" type="{
+                    new URL(source).pathname.endsWith('mp4') || new URL(source).pathname.endsWith('m4v')
+                        ? 'video/mp4' 
+                        : new URL(source).pathname.endsWith('webm') 
+                            ? "video/webm" 
+                            : new URL(source).pathname.endsWith('mov') 
+                                ? "video/mp4"
+                                : ''
+                }" />
+            </video>
+        </div>
     </div>
-</div>
+
+{:else}
+    <PostImage bind:post displayType={displayType} clickToPlay={true} zoomable={false} class="min-h-[300px]" on:click={(e)=> {clickToPlay() }}/>
 {/if}
+
