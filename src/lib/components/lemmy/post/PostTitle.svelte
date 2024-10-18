73fb3ceb30f17d423d80c33ae7e91a6f5f5a0301
Admiral Patrick <asimons@ptznetwork.org>
2024-10-18 17:08:37 -0400

    Lighten faded post title when marked as read

--- src/lib/components/lemmy/post/PostTitle.svelte
+++ src/lib/components/lemmy/post/PostTitle.svelte
@@ -48,7 +48,7 @@
     class="flex flex-row flex-wrap items-start gap-2 {$userSettings.showCompactPosts ? 'md:mt-4' : ''} font-medium max-w-full w-full break-words text-left"
     style="word-break: break-word;"
     class:text-slate-500={isPostView(post) && post.read && $userSettings.markReadPosts}
-    class:dark:text-zinc-400={isPostView(post) &&post.read && $userSettings.markReadPosts}
+    class:dark:text-zinc-200={isPostView(post) && post.read && $userSettings.markReadPosts}
     title="{fixLemmyEncodings(post.post.name)}"
 >
 
