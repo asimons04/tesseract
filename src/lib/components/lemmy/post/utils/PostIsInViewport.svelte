69cb6de6c406d5c8ee1d13a8b4f33ce4ed4d9c69
Admiral Patrick <asimons@ptznetwork.org>
2024-10-17 20:59:48 -0400

    Add configurable delay and threshold

--- src/lib/components/lemmy/post/utils/PostIsInViewport.svelte
+++ src/lib/components/lemmy/post/utils/PostIsInViewport.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     export let postContainer:HTMLDivElement
     export let inViewport:boolean
+    export let delay: number = 2000
+    export let threshold: number = 0
+    
     let timeout:number
     
     // Determine if the post is in the viewport and use that to determine whether to render it as an embed in the feed.
@@ -15,9 +18,9 @@
             // Give a short buffer time to allow resizing window without the element being flagged out of viewport and destroyed.
             timeout = window.setTimeout(() => {
                 inViewport = false
-            }, 2000)
+            }, delay)
         }, 
-        { root: null, threshold: 0,}
+        { root: null, threshold: threshold,}
     )
     $: if (postContainer) observer.observe(postContainer);
 
