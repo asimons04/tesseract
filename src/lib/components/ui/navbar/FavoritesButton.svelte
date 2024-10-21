989419b374607ecdf846693e4d468b4a1825faa5
Admiral Patrick <asimons@ptznetwork.org>
2024-10-21 15:51:57 -0400

    Add subscribed communities and view toggle to Favorites dropdown

--- src/lib/components/ui/navbar/FavoritesButton.svelte
+++ src/lib/components/ui/navbar/FavoritesButton.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
     import { getGroupIndex, sortGroups } from '$lib/favorites'
     import { profile, type CommunityGroup } from '$lib/auth'
+    import { slide } from 'svelte/transition'
 
     
     import Button from '$lib/components/input/Button.svelte'
     import CommunityList from '$lib/components/ui/sidebar/CommunityList.svelte'
-    import EditCommunityGroup from '$lib/components/util/EditCommunityGroup.svelte';
     import Menu from '$lib/components/ui/menu/Menu.svelte'
-    import MenuButton from '../menu/MenuButton.svelte';
+    import TextInput from '$lib/components/input/TextInput.svelte'
 
     import {
         Icon,
-        PencilSquare,
+        InboxArrowDown,
         Star,
+        XCircle,
     } from 'svelte-hero-icons'
     
     export let size:number = 28
+    
+    let selected: 'subscribed' | 'favorites' = 'favorites'
+    let selectedClass = '!text-sky-700 dark:!text-sky-500 font-bold'
+    let onlyShowModerating = false
+    let communityFilterTerm = ''
+    let communityFiltervalue:string = ''
     
     let favoritesGroup: CommunityGroup 
     $: favoritesGroup = ($profile?.groups && $profile.groups.length > 0)
@@ -24,46 +31,138 @@
             name: "Favorites",
             communities: [],
         } as CommunityGroup
+    
+    let debounceTimer: ReturnType<typeof setTimeout>;
+    function debounce(value:string,  timeout=300) {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(
+            () => {
+                communityFilterTerm = value.toLowerCase();
+                clearTimeout(debounceTimer);
+            }, timeout
+        )
+        
+    }
 
 </script>
 
 
 {#if $profile && $profile.user}
-<Menu alignment="bottom-center" containerClass="!max-w-fit overflow-y-auto">
+<Menu alignment="bottom-center" containerClass="!max-w-fit overflow-y-hidden">
     <Button
         color="tertiary"
         slot="button"
         aria-label="Favorites"
         title="Favorites"
         let:toggleOpen
+        let:open
         on:click={toggleOpen}
         class="max-md:w-9 max-md:h-8 max-md:!p-0"
     >
-        <Icon src={Star} width={size} mini slot="icon" />
+        <Icon src={Star} width={size} mini slot="icon" class="{open ? selectedClass : ''}"/>
     </Button>
     
-    <li class="flex flex-row items-center text-xs font-bold opacity-100 text-left px-4 py-1 min-w-48">
-        Favorites
+    <li class="flex flex-row items-center justify-between text-xs opacity-100 text-left px-4 py-1 w-full">
+        <Button color="tertiary" alignment="left" title='Favorites' icon={Star}
+            class="!text-xs hover:bg-slate-200 {selected == 'favorites' ? selectedClass : ''}" 
+            on:click={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                selected = selected = 'favorites'
+            }}
+            
+        >
+            Favorites
+        </Button>
+
+        <Button color="tertiary" alignment="left" title='Subscribed' icon={InboxArrowDown}
+            class="!text-xs hover:bg-slate-200 {selected == 'subscribed' ? selectedClass: ''}" 
+            on:click={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                selected = selected = 'subscribed'
+            }}
+            
+        >
+            Subscribed
+        </Button>
+        
+        <!---{selected == 'favorites' ? 'Favorites' : 'Subscribed'}--->
+        
+        
+        <!--
         <span class="ml-auto"/>
-        <Icon src={Star} width={16} mini />
+        <Icon src={selected == 'favorites' ? Star : InboxArrowDown} width={16} mini />
+        -->
     </li>
+
+    <!---Show filter term input if viewing subscribed list--->
+    {#if selected == 'subscribed'}
+        <div class="flex flex-row w-full justify-between" transition:slide>
+            
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+            <form class="p-2 flex flex-row items-center gap-2" on:click|preventDefault|stopPropagation on:submit|preventDefault>
+                <TextInput 
+                    type="text" autocomplete="new-password"    
+                    placeholder="Filter Communities"
+                    class="h-8 w-full"
+                    bind:value={communityFiltervalue}
+                    on:click={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                    }}
+                    on:keyup={(e) => { 
+                        debounce(e.detail.srcElement.value);
+                    }}
+                />
+
+                <Button size="square-md" color="tertiary" title="Reset Search Filter" class="ml-auto" icon={XCircle} iconSize={22}
+                    on:click={async (e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        debounce('');
+                        communityFiltervalue = '';
+                    }}
+                >
+                </Button>
+            </form>
+        </div>
+    {/if}
+
     <hr class="dark:opacity-10 w-[90%] my-2 mx-auto" />
 
-    
-    {#if $profile?.groups}
-        <div class="flex flex-col gap-1 h-full overflow-y-auto">
-                       
-            {#if $profile?.groups[getGroupIndex('Favorites')]?.communities?.length > 0}
+    <div class="flex flex-col gap-1">
+
+        <!---Favorites List--->
+        {#if selected == 'favorites'}
+            {#if $profile?.groups &&  $profile?.groups[getGroupIndex('Favorites')]?.communities?.length > 0}
                 <CommunityList 
-                    expanded={true}  hidden={false} group='Favorites'
+                    expanded={true}  hidden={false}
                     items={$profile?.groups[getGroupIndex('Favorites')]?.communities}
-                    menu={false}
+                    class="max-h-[79vh] overflow-y-auto"
                 />
             {:else}
                 <li class="text-xs opacity-80 text-left mx-4 my-1 py-1 w-48">Favorites group is empty.</li>
             {/if}
-        </div>
-    {/if}
+                
+        {/if}
+
+        <!---Subscribed List--->
+        {#if selected == 'subscribed'}
+            <CommunityList expanded hidden={false}
+                items={
+                    onlyShowModerating
+                    ? $profile.user.moderates.map((i) => i.community)
+                    : $profile.user.follows.map((i) => i.community)
+                }
+                filter={communityFilterTerm}
+                class="max-h-[79vh] overflow-y-auto"
+            />
+
+        {/if}
+
+    </div>
 
 </Menu>
 {/if}
