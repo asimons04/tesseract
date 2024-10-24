efa0319a46579934e7376fef4b6b4e4d0ea9d447
Admiral Patrick <asimons@ptznetwork.org>
2024-10-24 19:51:12 -0400

    Refactor Bandcamp component to make images smaller

--- src/lib/components/lemmy/post/PostBandcamp.svelte
+++ src/lib/components/lemmy/post/PostBandcamp.svelte
@@ -6,8 +6,11 @@
     import { userSettings  } from '$lib/settings.js'
 
     import Link from '$lib/components/input/Link.svelte'
+    import PostEmbedDescription from './PostEmbedDescription.svelte';
     import PostImage from '$lib/components/lemmy/post/PostImage.svelte'
     import PostIsInViewport from './utils/PostIsInViewport.svelte'
+    import ClickToPlayOverlay from './utils/ClickToPlayOverlay.svelte';
+    import ZoomableImage from '$lib/components/ui/ZoomableImage.svelte';
 
     export let post: PostView
     export let displayType: PostDisplayType
@@ -57,21 +60,33 @@
 <PostIsInViewport bind:postContainer bind:inViewport />
 
 {#if showAsEmbed}
-    <Link href={post.post.url} newtab={$userSettings.openInNewTab.links} title={post.post.url} domainOnly={!$userSettings.uiState.showFullURL} highlight nowrap/>
+    <PostEmbedDescription title={post.post.embed_title} on:clickThumbnail
+        description={$userSettings.uiState.hideCompactThumbnails && displayType=='feed' ? undefined : post.post.embed_description} 
+        url={post.post.url}
+        card={
+            (
+                (post.post.embed_description && !$userSettings.uiState.hideCompactThumbnails) || 
+                (post.post.embed_description && displayType=='post') ||
+                (post.post.thumbnail_url && !$userSettings.uiState.hideCompactThumbnails)
+            ) ? true : false
+        } 
+    >     
+        <Link href={post.post.url} newtab={$userSettings.openInNewTab.links} title={post.post.url} domainOnly={!$userSettings.uiState.showFullURL} highlight nowrap/>
+    </PostEmbedDescription>
 
     <div class="overflow-hidden  relative bg-slate-200 dark:bg-zinc-800 rounded-md max-w-full p-1">
         
         <!--- Album Art from Thumbnail --->
         {#if post.post.thumbnail_url}
-        <div class="max-w-full">
-            <div class="ml-auto mr-auto h-auto} max-w-[627px]">
-                <picture class="rounded-md overflow-hidden  max-h-[min(50vh,500px)] max-w-full"> <!--max-h-[min(50vh,500px)]--->
+        <div class="max-w-full max-h-[min(40vh,500px)]">
+            
+            <div class="ml-auto mr-auto max-w-[627px]">
+                <picture class="rounded-md overflow-hidden  max-h-[min(40vh,500px)] max-w-full"> <!--max-h-[min(50vh,500px)]--->
                     <!-- svelte-ignore a11y-missing-attribute -->
-                    <img
-                        src="{imageProxyURL(post.post.thumbnail_url)}"
-                        alt="{post.post.name}"
-                        loading="lazy"
-                        class="object-cover rounded-md h-auto z-30"
+                    <ZoomableImage 
+                        url={post.post.thumbnail_url}
+                        altText={post.post.name}
+                        class="max-h-[min(40vh,500px)] mx-auto object-cover rounded-md z-30"
                     />
                 </picture>
             </div>
@@ -80,7 +95,7 @@
         <!--- End Album Art from Thumbnail--->
         
         <!---Iframe with player--->
-        <div class="overflow-hidden  relative mt-[-120px]  max-w-full max-h-[128px]">
+        <div class="overflow-hidden max-w-full max-h-[128px]">
             <div class="ml-auto mr-auto w-full}">
                 <div class="flexiframe-container  max-w-screen max-h-[128px] mx-auto">
                     <iframe 
@@ -102,16 +117,27 @@
 
 {:else if post.post.thumbnail_url}
     <!---Create image post if user has media embeds enabled for posts--->    
-    <Link
-        href={post.post.url}
-        title={post.post.name}
-        newtab={$userSettings.openInNewTab.links}
-        highlight nowrap
-        domainOnly={!$userSettings.uiState.showFullURL}
-    />
-    <PostImage bind:post={post}  displayType={displayType} clickToPlay={true} zoomable={false} class="min-h-[300px]" on:click={(e)=> clickToPlayClicked=true}/>
-    
+    <PostEmbedDescription title={post.post.embed_title} on:clickThumbnail
+        description={$userSettings.uiState.hideCompactThumbnails && displayType=='feed' ? undefined : post.post.embed_description} 
+        url={post.post.url}
+        card={
+            (
+                (post.post.embed_description && !$userSettings.uiState.hideCompactThumbnails) || 
+                (post.post.embed_description && displayType=='post') ||
+                (post.post.thumbnail_url && !$userSettings.uiState.hideCompactThumbnails)
+            ) ? true : false
+        } 
+    >
+        <Link
+            href={post.post.url}
+            title={post.post.name}
+            newtab={$userSettings.openInNewTab.links}
+            highlight nowrap
+            domainOnly={!$userSettings.uiState.showFullURL}
+        />
+    </PostEmbedDescription>
 
+    <PostImage bind:post={post}  displayType={displayType} clickToPlay={true} zoomable={false} class="min-h-[300px]" on:click={(e)=> clickToPlayClicked=true}/>
 
 {:else if !post.post.thumbnail_url}
     <Link
