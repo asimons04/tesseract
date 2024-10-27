67ed2f62cb495b55079fbfad8d9ae58aff36df7e
Admiral Patrick <asimons@ptznetwork.org>
2024-10-27 17:24:44 -0400

    Add share/copy link to archive link selector

--- src/lib/components/lemmy/post/utils/ArchiveLinkSelector.svelte
+++ src/lib/components/lemmy/post/utils/ArchiveLinkSelector.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { lookup } from '$lib/MBFC/client'
-    import { removeURLParams } from "../helpers"
+    import { isYouTube, removeURLParams } from "../helpers"
+    import { toast } from '$lib/components/ui/toasts/toasts'
     import { userSettings } from '$lib/settings'
 
     import Button from "$lib/components/input/Button.svelte";
@@ -12,7 +13,8 @@
         ChevronDown, 
         ChevronUp, 
         Icon, 
-        Link as LinkIcon 
+        Link as LinkIcon,
+        Share,
     } from 'svelte-hero-icons'
 
     export let url:string | undefined
@@ -41,16 +43,22 @@
             </span>
         </Button>
 
-        <div class="flex flex-row items-center text-xs font-bold opacity-100 text-left mx-4 my-1 py-1 min-w-[175px]">
-            Alternate Sources
-            <span class="ml-auto"/>
-            <Icon slot="icon" src={LinkIcon} width={16} mini />
-        </div>
-        <hr class="dark:opacity-10 w-[90%] my-2 mx-auto" />
+       
         
         
+        {#if ['link', 'thumbLink', 'youtube'].includes(postType)}
+            <div class="flex flex-row items-center text-xs font-bold opacity-100 text-left mx-4 my-1 py-1 min-w-[175px]">
+                Alternate Sources
+                <span class="ml-auto"/>
+                <Icon slot="icon" src={LinkIcon} width={16} mini />
+            </div>
+            <hr class="dark:opacity-10 w-[90%] my-2 mx-auto" />
+        {/if}
+
         <!---Archive Link Providers for 'link' Post Types--->
         {#if ['link', 'thumbLink'].includes(postType)}
+            
+
             <MenuButton color="info" link href="https://archive.ph/{removeURLParams(url)}" newtab={$userSettings.openInNewTab.links} title="Archive Today">
                 Archive Today
             </MenuButton>
@@ -83,27 +91,42 @@
             <MenuButton color="info" link href="https://spinscore.io/?url={removeURLParams(url)}" newtab={$userSettings.openInNewTab.links} title="SpinScore.io">
                 SpinScore.io
             </MenuButton>
-
-
-
-            
         {/if}
-        
 
         <!---Piped/Invidious Providers for 'youtube' Post Types--->
         {#if postType == 'youtube'}
             <MenuButton color="info" title="Invidious" link href={updateYTHostname(url, $userSettings.embeddedMedia.customInvidious)} newtab={$userSettings.openInNewTab.links}>
-                Invidious
+                Invidious/Piped
             </MenuButton>
-
-            <MenuButton color="info" title="Invidious" link href={updateYTHostname(url, $userSettings.embeddedMedia.customPiped)} newtab={$userSettings.openInNewTab.links}>
-                Piped
-            </MenuButton>
-
+            
+            {#if !isYouTube(url)}
             <MenuButton color="info" title="Invidious" link href={updateYTHostname(url, 'youtube.com')} newtab={$userSettings.openInNewTab.links}>
                 YouTube
             </MenuButton>
+            {/if}
         {/if}
+
+
+        {#if ['link', 'thumbLink', 'youtube'].includes(postType)}
+            <hr class="dark:opacity-10 w-[90%] my-2 mx-auto" />
+        {/if}
+        
+        <MenuButton title="Share" color="success"
+            on:click={() => {
+                navigator.share?.({
+                    url: url
+                }) ?? navigator.clipboard.writeText(url)
+                toast({
+                    type: 'success',
+                    content: `Copied URL to clipboard!`,
+                    title: 'Copied'
+                })
+                
+            }}
+        >
+            <Icon src={Share} width={16} mini />
+            Copy Link
+        </MenuButton>
 
     </Menu>
 {/if}
