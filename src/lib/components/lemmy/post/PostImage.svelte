e11ab5c1396f5cdbe2a2caae41026df8317d17d0
Admiral Patrick <asimons@ptznetwork.org>
2024-11-02 19:56:13 -0400

    Add conditional max height bounding now that click to play behavior changed

--- src/lib/components/lemmy/post/PostImage.svelte
+++ src/lib/components/lemmy/post/PostImage.svelte
@@ -47,14 +47,14 @@
         <ClickToPlayOverlay bind:show={clickToPlay} displayType={displayType} on:click={(e)=> dispatcher('click', e)}/>
         
         <ZoomableImage url={thumbnail_url} bind:nsfw={post.post.nsfw} altText={post.post.name} zoomable={zoomable}
-            class="ml-auto mr-auto object-cover rounded-md min-h-[min(40vh,800px)] max-h-[min(40vh,800px)] z-20 {$$props.class}"
+            class="ml-auto mr-auto object-cover rounded-md min-h-[min(40vh,800px)] max-h-[min(50vh,800px)] z-20 {$$props.class}"
             on:click={(e)=> dispatcher('click', e)}
         />
     
     <!---Rendering in post page--->
     {:else}
         <ZoomableImage url={url} altText={post.post.name} {zoomable}
-            class="ml-auto mr-auto object-contain rounded-md min-h-[min(40vh,800px)] z-30"
+            class="ml-auto mr-auto object-contain rounded-md min-h-[min(40vh,800px)] {displayType=='feed' ? 'max-h-[min(50vh,800px)]' : ''} z-30"
         />
 
     {/if}
