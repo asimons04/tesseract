1aa096a2d7bde8e38b501f5687a0f4628f8d4cfc
Admiral Patrick <asimons@ptznetwork.org>
2024-11-05 08:41:53 -0500

    Increase site/community/user sidebar width from 25->27 rem

--- src/lib/components/ui/StickyCard.svelte
+++ src/lib/components/ui/StickyCard.svelte
@@ -1,12 +1,14 @@
 <aside
     class="sticky top-[8rem] break-words flex flex-col gap-1 z-10 xl:pr-2 -mt-1
     w-full h-full
-    xl:w-[25rem] xl:overflow-auto
+    xl:w-[27rem] xl:overflow-auto
     max-h-[calc(100svh-9rem)]
     text-sm border-slate-200 dark:border-zinc-800 {$$props.class}"
 >
     <slot />
 </aside>
+
+<!--xl:w-[25rem] xl:overflow-auto-->
 <!---
     md:w-[80%] md:mx-auto
     lg:w-[70%] lg:mx-auto 
