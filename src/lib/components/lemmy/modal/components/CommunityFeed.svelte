0955add2d47323313b3c14d9bc07511f57298129
Admiral Patrick <asimons@ptznetwork.org>
2024-11-09 17:10:32 -0500

    Add both manual pagination and infinite scroll

--- src/lib/components/lemmy/modal/components/CommunityFeed.svelte
+++ src/lib/components/lemmy/modal/components/CommunityFeed.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
     import type { GetPostsResponse, PostView } from "lemmy-js-client"
+    import type { InfiniteScrollStateVars } from "$lib/components/ui/infinitescroll/helpers"
 
+    import { addMBFCResults, filterKeywords } from "../../post/helpers";
     import { getClient } from "$lib/lemmy"
-    import {  isCommentView } from "$lib/lemmy/item";
     import { onMount } from "svelte";
     
-
+    import InfiniteScrollDiv from "$lib/components/ui/infinitescroll/InfiniteScrollDiv.svelte";
     import Post from "../../post/Post.svelte";
     import Placeholder from "$lib/components/ui/Placeholder.svelte";
     import Pageination from "$lib/components/ui/Pageination.svelte";
     import Spinner from "$lib/components/ui/loader/Spinner.svelte";
     
-    import { PencilSquare } from "svelte-hero-icons";
-    import { addMBFCResults, filterKeywords } from "../../post/helpers";
+    import { Bars3BottomRight, PencilSquare } from "svelte-hero-icons";
+    import { userSettings } from "$lib/settings";
+    
     
     export let community_id: number | undefined = undefined
     export let community_name: string | undefined = undefined
@@ -27,6 +29,7 @@
     } as GetPostsResponse
     
     let scrollContainer: HTMLDivElement
+    let scrollState:InfiniteScrollStateVars
 
     onMount(() => load())
 
@@ -37,21 +40,38 @@
         if (!community_id && !community_name) return
 
         try {
-            loading = true;
+            $userSettings.uiState.infiniteScroll
+                ? scrollState.loading = true
+                : loading = true
 
-            posts = await getClient().getPosts({
-                limit: 10,
+            let batch = await getClient().getPosts({
+                limit: $userSettings.uiState.postsPerPage,
                 page_cursor: page_cursor,
                 sort: 'New',
                 community_id: community_id,
                 community_name: community_name
             })
             
-            posts.posts = addMBFCResults(posts.posts)
-            posts.posts = filterKeywords(posts.posts)
-            page_cursor = posts.next_page
+            batch.posts = addMBFCResults(batch.posts)
+            batch.posts = filterKeywords(batch.posts)
+            
+            page_cursor = batch.next_page
                         
-            loading = false;
+            $userSettings.uiState.infiniteScroll
+                ? scrollState.loading = false
+                : loading = false
+            
+            if ($userSettings.uiState.infiniteScroll) {
+                if (batch.posts.length < 1) scrollState.exhausted = true
+                posts = {
+                    next_page: batch.next_page,
+                    posts: [...posts.posts, ...batch.posts]
+                }
+            }
+            else {
+                posts = batch
+            }
+
             posts = posts
         }
         catch (err){
@@ -61,34 +81,46 @@
 
 </script>
 
-<div bind:this={scrollContainer} class="flex flex-col w-full mt-2 gap-4 overflow-x-hidden max-h-[60vh] overflow-y-scroll" >
-    {#if loading}
+<div bind:this={scrollContainer} class="flex flex-col w-full mt-2 gap-4 overflow-x-hidden max-h-[75vh] overflow-y-scroll" >
+    <!---Only use this loading spinner if infinite scroll is disabled--->
+    {#if loading && !$userSettings.uiState.infiniteScroll}
         <span class="flex flex-row w-full items-center">        
             <span class="ml-auto"/>
             <Spinner width={64}/>
             <span class="mr-auto"/>
         </span>
-    {:else}
+    {/if}
 
-        {#if posts?.posts?.length > 0 }
-            
-            {#each posts.posts as item, idx (item.post.id)}
-                <Post bind:post={item} actions={false} inCommunity={true}/>
-            {/each}
-            
-        {:else}
-            <div class="mt-2 w-full flex flex-col gap-5 mx-auto">
-                <Placeholder icon={PencilSquare} title="No Posts" description="No posts were returned for this community." />
-            </div>
-        {/if}
+    {#if posts?.posts?.length > 0 }
+        
+        {#each posts.posts as item, idx (item.post.id)}
+            <Post bind:post={item} actions={false} inCommunity={true}/>
+        {/each}
+        
+    <!---Only use this placeholder if infinite scroll is disabled--->
+    {:else if (!loading && !$userSettings.uiState.infiniteScroll)}
+        <div class="mt-2 w-full flex flex-col gap-5 mx-auto">
+            <Placeholder icon={PencilSquare} title="No Posts" description="No posts were returned for this community." />
+        </div>
+    {/if}
 
+    
+    
+    {#if $userSettings.uiState.infiniteScroll}
+        <div class="flex flex-col items-center pt-2 w-full">
+            <InfiniteScrollDiv bind:state={scrollState} bind:element={scrollContainer} threshold={500}
+                on:loadMore={ () => {
+                    load()
+                }}
+            />
+        </div>
     {/if}
 </div>
 
-<div class="mt-4" />
-
-<Pageination bind:page class="px-4 mb-4" on:change={(e) => {
-    page = e.detail
-    load()
-    scrollContainer.scrollTo(0,0)
-}}/>
+{#if !$userSettings.uiState.infiniteScroll}
+    <Pageination bind:page class="px-4" on:change={(e) => {
+        page = e.detail
+        load()
+        scrollContainer.scrollTo(0,0)
+    }}/>
+{/if}
