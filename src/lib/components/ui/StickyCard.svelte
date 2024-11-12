d45e6304d8736c989046253cc433c7f2eb0523ec
Admiral Patrick <asimons@ptznetwork.org>
2024-11-12 18:07:37 -0500

    Hide site/community/user sidebar in smaller vw's

--- src/lib/components/ui/StickyCard.svelte
+++ src/lib/components/ui/StickyCard.svelte
@@ -1,6 +1,6 @@
 <aside
-    class="sticky top-[8rem] break-words flex flex-col gap-1 z-10 xl:pr-2 -mt-1
-    w-full h-full
+    class="sticky top-[8rem] break-words hidden xl:flex flex-col gap-1 z-10 xl:pr-2 -mt-1
+    h-full w-full
     xl:w-[27rem] xl:overflow-auto
     max-h-[calc(100svh-9rem)]
     text-sm border-slate-200 dark:border-zinc-800 {$$props.class}"
