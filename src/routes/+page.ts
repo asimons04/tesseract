29e88f3233ada4e148a51b689796e693259b4492
Admiral Patrick <asimons@ptznetwork.org>
2024-11-12 18:31:41 -0500

    Only fetch/return site data if not already populated in  store

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,5 @@
 import type { GetPostsResponse, GetSiteResponse, ListingType, PostView, SortType } from 'lemmy-js-client'
 
-import { addMBFCResults, findCrossposts, filterKeywords, sortPosts } from '$lib/components/lemmy/post/helpers'
 import { getClient, site } from '$lib/lemmy.js'
 import { get } from 'svelte/store'
 import { error } from '@sveltejs/kit'
@@ -15,49 +14,21 @@ interface LoadParams {
 
 
 export async function load({ url, passedSite }: LoadParams) {
-    const page_cursor = url.searchParams.get('page_cursor')
-    const sort: SortType = (url.searchParams.get('sort') as SortType) || get(userSettings).defaultSort.sort
-    const listingType: ListingType = (url.searchParams.get('type') as ListingType) || get(userSettings).defaultSort.feed
-    
     try {
-        // Fetch posts
-        let [ posts, siteData ] = await Promise.all([
-            getClient().getPosts({
-                limit: get(userSettings)?.uiState.postsPerPage || 10,
-                sort: sort,
-                type_: listingType,
-                page_cursor: page_cursor,
-            }),
-            passedSite ?? getClient().getSite()
-        ])
-        
+        let siteData = get(site) ?? (await getClient().getSite())
         site.set(siteData)
-
-        // Filter the posts for keywords
-        posts.posts = filterKeywords(posts.posts);
-
-        // Roll up any duplicate posts/crossposts
-        posts.posts = findCrossposts(posts.posts);
-
-        // Apply MBFC data object to post
-        posts.posts = addMBFCResults(posts.posts);
-        
-        // Only sort the first fetch. If a site object is passed, it can be used to indicate this is a re-fetch (which are sorted after being retrieved)
-        //if (!passedSite) posts.posts = sortPosts(posts.posts, sort)
-
-        // Return the data to the frontend
         return {
-            sort: sort,
-            listingType: listingType,
-            posts: posts,
-            site: siteData,
-            type: listingType
+            site: siteData
         }
-        
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err)
         throw error(500, {
             message: 'Failed to fetch homepage.',
         })
     }
+
 }
+
+
+
