022d781e9f1d5f52243f874ed0b7f808327488ac
Admiral Patrick <asimons@ptznetwork.org>
2024-11-12 18:25:36 -0500

    Tweak medium viewport size width from 85 to 90%

--- src/lib/components/ui/containers/FeedContainer.svelte
+++ src/lib/components/ui/containers/FeedContainer.svelte
@@ -2,6 +2,6 @@
     import { userSettings} from '$lib/settings'
 </script>
 
-<div class="w-full flex flex-col gap-5 h-full ml-auto mr-auto mt-2 {$userSettings.uiState.feedMargins ? 'sm:w-full md:w-[85%] lg:w-[90%] xl:w-[75%]' : ''} {$$props.class}">
+<div class="w-full flex flex-col gap-5 h-full ml-auto mr-auto mt-2 {$userSettings.uiState.feedMargins ? 'sm:w-full md:w-[90%] lg:w-[90%] xl:w-[75%]' : ''} {$$props.class}">
     <slot/>
 </div>
