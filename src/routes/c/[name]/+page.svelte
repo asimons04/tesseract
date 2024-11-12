13439a942b919691d64bf72bb76b90e7b98a424b
Admiral Patrick <asimons@ptznetwork.org>
2024-11-12 18:32:39 -0500

    Replace bespoke feed renderer with integrated component

--- src/routes/c/[name]/+page.svelte
+++ src/routes/c/[name]/+page.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-    import type { FeedController } from '$lib/components/lemmy/modal/components/helpers.js'
+    import type { ListingType, SortType } from 'lemmy-js-client';
+    import type { FeedController } from '$lib/components/lemmy/feed/helpers.js'
 
     import { page } from '$app/stores';
-        
+    import { userSettings } from '$lib/settings'
+
     import CommunityCard from '$lib/components/lemmy/community/CommunityCard.svelte'
+    import CommunityCardSmall from '$lib/components/lemmy/community/CommunityCardSmall.svelte';
     import MainContentArea from '$lib/components/ui/containers/MainContentArea.svelte';
+    import PostFeed from '$lib/components/lemmy/feed/PostFeed.svelte';
     import SiteSearch from '$lib/components/ui/subnavbar/SiteSearch.svelte';
     import SubNavbar from '$lib/components/ui/subnavbar/SubNavbar.svelte'
-    import CommunityFeed from '$lib/components/lemmy/modal/components/CommunityFeed.svelte';
     import FeedContainer from '$lib/components/ui/containers/FeedContainer.svelte';
         
     export let data
-      
 
     // Container for the bound variables from the feed component
     let feedController: FeedController = {} as FeedController
-
+    let selectedSort: SortType = ($page.url.searchParams.get('sort') as SortType) ?? $userSettings.defaultSort.sort ?? 'New'
 </script>
 
 
@@ -32,17 +34,14 @@
 
 
 <SubNavbar back quickSettings toggleMargins toggleCommunitySidebar compactSwitch
-    sortMenu sortPreventDefault selectedSortOption={feedController?.sort } 
+    sortMenu sortPreventDefault bind:selectedSortOption={selectedSort} 
      on:navChangeSort={(e) => {
         if (e?.detail && feedController.sort != e.detail) {
             feedController.sort = e.detail
-            feedController.refresh(true)
+            feedController.refresh()
         }
     }}
-
     scrollButtons scrollPreventDefault on:navScrollBottom={() => feedController.scrollBottom() } on:navScrollTop={() => feedController.scrollTop() }
-
-
     refreshButton refreshPreventDefault on:navRefresh={()=> feedController.refresh(true) } 
 >
     <SiteSearch placeholder="Search {data.community.community_view.community.name}" community_id={data.community.community_view.community.id} slot="center"/>
@@ -50,9 +49,18 @@
 
 <MainContentArea>
 
-    <div class="flex w-full" style="height: calc(100vh - 9rem);">
+    <div class="flex w-full" style="height: calc(100vh - 8rem);">
         <FeedContainer>
-            <CommunityFeed actions community_name={$page.params.name}  bind:controller={feedController} />
+            <PostFeed actions type="All"
+                bind:community_name={$page.params.name}  
+                bind:controller={feedController} 
+                bind:sort={selectedSort}
+            >
+                <!---Add the Site Banner to the top of the feed below 'xl' width--->
+                <div class="flex xl:hidden flex-col w-full" slot="banner">    
+                    <CommunityCardSmall community_view={data.community.community_view}/>
+                </div>
+            </PostFeed>
         </FeedContainer>
     </div>
     
