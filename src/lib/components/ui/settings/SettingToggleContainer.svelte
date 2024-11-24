aaf9de6a3670fd0f1e110b4def9d59007990b4c2
Admiral Patrick <asimons@ptznetwork.org>
2024-11-24 11:31:08 -0500

    Standardize divider colors

--- src/lib/components/ui/settings/SettingToggleContainer.svelte
+++ src/lib/components/ui/settings/SettingToggleContainer.svelte
@@ -1,3 +1,3 @@
-<div class="flex flex-col divide-y border-slate-400/75 dark:border-zinc-400/75 gap-4 w-full">
+<div class="flex flex-col divide-y divide-slate-200 dark:divide-zinc-800 gap-4 w-full">
     <slot/>
 </div>
