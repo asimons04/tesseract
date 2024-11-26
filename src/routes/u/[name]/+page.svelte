8dc9b45ea93fb3c3fd964a30ab8ce120d64466ce
Admiral Patrick <asimons@ptznetwork.org>
2024-11-26 18:46:18 -0500

    Use user submission feed component

--- src/routes/u/[name]/+page.svelte
+++ src/routes/u/[name]/+page.svelte
@@ -1,183 +1,77 @@
 <script lang="ts">
-    import type { Snapshot } from '@sveltejs/kit';    
+    import type { UserSubmissionFeedController } from '$lib/components/lemmy/feed/helpers'
 
-    import { beforeNavigate } from '$app/navigation';
-    import { isCommentView } from '$lib/lemmy/item.js'
-    import { goto } from '$app/navigation'
-    import { load } from './+page'
     import { page } from '$app/stores'
-    import { PageSnapshot } from '$lib/storage.js';
-    import { scrollToLastSeenPost } from '$lib/components/lemmy/post/helpers.js';
-    import { searchParam } from '$lib/util.js'
     import { userSettings } from '$lib/settings.js'
 
-    import Avatar from '$lib/components/ui/Avatar.svelte';
-    import CommentItem from '$lib/components/lemmy/comment/CommentItem.svelte'
     import FeedContainer from '$lib/components/ui/containers/FeedContainer.svelte'
-    import InfiniteScroll from '$lib/components/ui/InfiniteScroll.svelte'
-    import InfiniteScrollRefreshOldestPosts from '$lib/components/ui/InfiniteScrollRefreshOldestPosts.svelte';
     import MainContentArea from '$lib/components/ui/containers/MainContentArea.svelte';
-    import Placeholder from '$lib/components/ui/Placeholder.svelte'
-    import Post from '$lib/components/lemmy/post/Post.svelte'
     import SiteSearch from '$lib/components/ui/subnavbar/SiteSearch.svelte';
     import SubNavbar from '$lib/components/ui/subnavbar/SubNavbar.svelte'
-    import UserCard from '$lib/components/lemmy/user/UserCard.svelte'
-
-    import {
-        PencilSquare,
-    } from 'svelte-hero-icons'
-    
+    import UserSubmissionFeed from '$lib/components/lemmy/feed/UserSubmissionFeed.svelte'
+    import UserCardSmall from '$lib/components/lemmy/user/UserCardSmall.svelte';
+    import SiteCard from '$lib/components/lemmy/SiteCard.svelte';
+    import CollapseButton from '$lib/components/ui/CollapseButton.svelte';
+    import CommunityLink from '$lib/components/lemmy/community/CommunityLink.svelte';
+    import Markdown from '$lib/components/markdown/Markdown.svelte';
+    import { ShieldCheck, UserCircle } from 'svelte-hero-icons';
     
     export let data
 
-    // Page state that will persist in snapshots
-    let pageState = {
-        scrollY: 0,
-    }
-    
-    // Infinite scroll object to hold config parms
-    let infiniteScroll = {
-        loading: false,
-        exhausted: false,
-        maxPosts: $userSettings.uiState.maxScrollPosts,
-        truncated: false,
-        enabled: $userSettings.uiState.infiniteScroll,
-    }
-
-    // Needed to re-enable scroll fetching when switching between an exhausted sort option (top hour) to one with more post (top day)
-    beforeNavigate(() => {
-        infiniteScroll.exhausted = false
-    })
-
-    // Store and reload the page data between navigations (Override functions to use LocalStorage instead of Session Storage)
-    // Also jumps to last seen post upon restore.
-    export const snapshot: Snapshot<void> = {
-        capture: () => {
-            pageState.scrollY = window.scrollY
-            
-            while (data.items.length > infiniteScroll.maxPosts) {
-                data.items.shift()
-                infiniteScroll.truncated = true
-            }
-            if (infiniteScroll.enabled) PageSnapshot.capture({data: data, state: pageState, infiniteScroll: infiniteScroll})
-        },
-        restore: async () => {
-            try { 
-                if (infiniteScroll.enabled) {
-                    let snapshot = PageSnapshot.restore() 
-                    if (snapshot.data)              data = snapshot.data
-                    if (snapshot.state)             pageState = snapshot.state
-                    if (snapshot.infiniteScroll)    infiniteScroll = snapshot.infiniteScroll
-                }
-
-                await scrollToLastSeenPost(data.items.length + 200)
-            }
-            catch (err) { 
-                console.error("Error restoring snapshot: ", err)
-                PageSnapshot.clear() 
-                window.scrollTo(0,0)
-            }
-        }
-    }
-
-    async function refresh() {
-        PageSnapshot.clear()
-        infiniteScroll.exhausted = false
-        infiniteScroll.truncated = false
-        window.scrollTo(0,0)
-    }
-
-    function loadPosts() {
-        const loadParams = {
-            params: {
-                name: $page.params.name
-            },
-            url: new URL(window.location.href)
-        }
-        
-        loadParams.url.searchParams.set('sort', data.sort)
-        loadParams.url.searchParams.set('type', data.type)
-        loadParams.url.searchParams.set('page', (++data.page).toString())
-        loadParams.url.searchParams.set('limit', data.limit.toString())
-
-        load(loadParams).then((nextBatch) => {
-            if (nextBatch.items.length < 1) infiniteScroll.exhausted = true
-            else infiniteScroll.exhausted = false
-
-            nextBatch.items.forEach((item) => {
-                data.items.push(item)
-            })
-            
-            data = data
-            infiniteScroll.loading  = false
-        })
-    }
+    let feedController: UserSubmissionFeedController = {} as UserSubmissionFeedController
 
 </script>
 
 <svelte:head>
-  <title>Profile | {data.person_view.person.display_name ?? data.person_view.person.name}</title>
+  <title>Profile</title>
 </svelte:head>
 
+<SubNavbar  back quickSettings toggleMargins toggleCommunitySidebar 
+    refreshButton refreshPreventDefault
+    on:navRefresh={()=> feedController.refresh(true)}
+    
+    scrollButtons scrollPreventDefault 
+    on:navScrollBottom={() => feedController.scrollBottom() } 
+    on:navScrollTop={() => feedController.scrollTop() }
 
-
-<!---Only show on /u/{username} routes since the /profile/user route will use the navbar from its layout page--->
-{#if $page.url.pathname.startsWith('/u/')}
-<SubNavbar  back quickSettings toggleMargins toggleCommunitySidebar scrollButtons 
-    refreshButton   on:navRefresh={()=> refresh()}
-    sortMenu        sortOptions={['New', 'TopAll', 'Old']} sortOptionNames={['New', 'Top', 'Old']} bind:selectedSortOption={data.sort}
-    listingType     listingTypeOptions={['all', 'posts', 'comments']} listingTypeOptionNames={['All', 'Posts', 'Comments']} bind:selectedListingType={data.type}
 >
-    <SiteSearch placeholder="Search {$userSettings.displayNames ? (data.person_view.person.display_name ?? data.person_view.person.name) : data.person_view.person.name }" 
-        person_id={data.person_view.person.id} slot="center"
-    />
+
 </SubNavbar>
-{/if}
+
 
 <MainContentArea>
-    {#if data.items.length > 0}
-        
-        <!---Shows a button to refresh for oldest ost once infinite scroll FIFO overflows--->
-        <InfiniteScrollRefreshOldestPosts bind:show={infiniteScroll.truncated} 
-            on:click={() => {
-                goto(window.location.href, {invalidateAll: true})
-                refresh()
-            }}
-        />
-
+    <div class="flex w-full" style="height: calc(100vh - 8rem);">
         <FeedContainer>
-            {#each data.items as item, idx (isCommentView(item) ? item.comment.id : item.post.id)}
-                {#if item && isCommentView(item) && (data.type == 'all' || data.type == 'comments')}
-                    <CommentItem bind:comment={item} />
+
+            <UserSubmissionFeed bind:controller={feedController} bind:person_name={$page.params.name}  actions >
                 
-                {:else if item && !isCommentView(item) && (data.type == 'all' || data.type == 'posts')}
-                    <Post bind:post={item} inProfile/>
-                {/if}
-            {/each}
+                <div class="flex flex-col mx-auto w-full gap-2 max-w-[820px]" slot="banner" let:user>
+                    {#if user}
+                        
+                        <UserCardSmall person_view={user.person_view} />
+                        
+                        <!---Person Bio--->
+                        {#if user.person_view.person.bio}
+                            <CollapseButton icon={UserCircle} title="About Me" expanded={false}>
+                                <Markdown source={user.person_view.person.bio} />
+                            </CollapseButton>
+                        {/if}
+
+                        <!---List of Communities Moderated--->
+                        {#if user.moderates?.length > 0}
+                            <CollapseButton icon={ShieldCheck} title="Moderates">
+                                {#each user.moderates as community, idx (community.community.id)}
+                                    <CommunityLink community={community.community} avatar />
+                                {/each}
+                            </CollapseButton>
+                        {/if}
+
+                    {/if}
+                </div>
+
+            </UserSubmissionFeed>
         </FeedContainer>
+    </div>
 
-        <InfiniteScroll bind:loading={infiniteScroll.loading} bind:exhausted={infiniteScroll.exhausted} threshold={75} bind:enabled={infiniteScroll.enabled}
-            disableBack={ data.page < 2 ? true : false }
-            on:loadMore={ () => {
-                if (!infiniteScroll.exhausted && !infiniteScroll.loading) {
-                    infiniteScroll.loading = true
-                    loadPosts()
-                }
-            }}
-
-            on:next={ () => {
-                searchParam($page.url, 'page', (++data.page).toString())
-            }}
-            on:prev={ () => {
-                if (data.page > 1) searchParam($page.url, 'page', (--data.page).toString())
-            }}
-        />
-        
-    {:else}
-        <Placeholder icon={PencilSquare} title="No submissions" description="This user has no submissions that match this filter."/>
-    {/if}
-
-    <!---User Info Panel: Only show on /u/ pages.  Profile (/profile/user) will use its own layout--->
-    <UserCard bind:person={data.person_view} moderates={data.moderates} display={$page.url.pathname.startsWith('/u/')} slot="right-panel"/>
-
+    <SiteCard site={data.site.site_view} version={data.site.version} taglines={data.site.taglines} admins={data.site.admins} slot="right-panel"/>
 </MainContentArea>
--- src/routes/u/[name]/+page.ts
+++ src/routes/u/[name]/+page.ts
@@ -1,9 +1,11 @@
-import type { SortType, GetPersonDetailsResponse } from 'lemmy-js-client'
+import type { SortType, GetPersonDetailsResponse, GetSiteResponse } from 'lemmy-js-client'
 
 import { get } from 'svelte/store'
+import { instance as currentInstance} from '$lib/instance'
 import { profile } from '$lib/auth.js'
-import { getClient } from '$lib/lemmy.js'
+import { getClient, site } from '$lib/lemmy.js'
 import { getItemPublished } from '$lib/lemmy/item.js'
+
 import { userSettings } from '$lib/settings'
 
 
@@ -13,42 +15,24 @@ interface LoadParams {
 }
 
 export async function load({ params, url }: LoadParams) {
+    let instance =  get(currentInstance)
+    let siteData: GetSiteResponse
     
-    const page = Number(url.searchParams.get('page')) || 1
-    const type: 'comments' | 'posts' | 'all' = (url.searchParams.get('type') as 'comments' | 'posts' | 'all') || 'all'
-    const sort: SortType = (url.searchParams.get('sort') as SortType) || 'New'
-    const limit = Number(url.searchParams.get('limit')) || get(userSettings)?.uiState.postsPerPage || 20
+    if (params.name && params.name.split('@')[1]) instance = params.name.split('@')[1]
 
-    const user = await getClient().getPersonDetails({
-        limit: limit,
-        page: page,
-        username: params.name ?? get(profile)?.username,
-        sort: sort,
-    })
-
-    
-    const items = [...user.posts, ...user.comments]
-    
-    if (sort == 'TopAll') {
-        items.sort( (a, b) => (b.counts.upvotes - b.counts.downvotes) - (a.counts.upvotes - a.counts.downvotes) )
+    try {
+        if (instance == get(currentInstance)){
+            siteData = get(site) ?? await getClient().getSite()
+        }
+        else {
+            siteData = await getClient(instance).getSite()
+        }
     }
-    
-    if (sort == 'New') {
-        items.sort( (a, b) => Date.parse(getItemPublished(b)) - Date.parse(getItemPublished(a)) )
+    catch {
+        siteData = get(site) ?? await getClient().getSite()
     }
 
-    if (sort == 'Old') {
-        items.sort( (a, b) => Date.parse(getItemPublished(a)) - Date.parse(getItemPublished(b)) )
-    }
-    
-    console.log("/u/ loader running")
     return {
-        limit: limit,
-        type: type,
-        page: page,
-        sort: sort,
-        person_view: user.person_view,
-        moderates: user.moderates,
-        items:items,
+        site: siteData
     }
 }
