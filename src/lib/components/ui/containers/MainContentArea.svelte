18cc692fdb56333b204bf62f449665e4bc636bc1
Admiral Patrick <asimons@ptznetwork.org>
2024-11-26 18:21:36 -0500

    Reduce main container gap

--- src/lib/components/ui/containers/MainContentArea.svelte
+++ src/lib/components/ui/containers/MainContentArea.svelte
@@ -3,9 +3,9 @@
 </script>
 
 
-<div class="flex flex-col-reverse  xl:flex-row gap-4 max-w-full w-full h-full py-0">
+<div class="flex flex-col-reverse  xl:flex-row gap-1 max-w-full w-full h-full py-0">
     
-    <div class="flex flex-col gap-4 max-w-full w-full h-full min-w-0">
+    <div class="flex flex-col gap-0 max-w-full w-full h-full min-w-0">
         <slot/>
     </div>
     
