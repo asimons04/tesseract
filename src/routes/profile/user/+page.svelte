392adbbe469dff49aafef09a7eec6aaa690a682f
Admiral Patrick <asimons@ptznetwork.org>
2024-11-26 18:22:49 -0500

    Use UserSubmissionFeed component

--- src/routes/profile/user/+page.svelte
+++ src/routes/profile/user/+page.svelte
@@ -1,45 +1,99 @@
 <script lang="ts">
-    import type { Snapshot } from '@sveltejs/kit';    
     
-    import { PageSnapshot } from '$lib/storage.js';
-    import { scrollToLastSeenPost } from '$lib/components/lemmy/post/helpers.js';
+    import { profile } from '$lib/auth'
     import { userSettings } from '$lib/settings'
-    import UserPage from '../../u/[name]/+page.svelte'
+    
+    import CollapseButton from '$lib/components/ui/CollapseButton.svelte'
+    import CommunityLink from '$lib/components/lemmy/community/CommunityLink.svelte';
+    import FeedContainer from '$lib/components/ui/containers/FeedContainer.svelte';
+    import MainContentArea from '$lib/components/ui/containers/MainContentArea.svelte';
+    import Markdown from '$lib/components/markdown/Markdown.svelte'
 
-    export let data
+    import UserSubmissionFeed from '$lib/components/lemmy/feed/UserSubmissionFeed.svelte'
+    import UserCardSmall from '$lib/components/lemmy/user/UserCardSmall.svelte'
+    
+    
+    
+    import { HandRaised, ShieldCheck, UserCircle } from 'svelte-hero-icons';
+    import ProfileMenuBar from '../ProfileMenuBar.svelte';
+    import SubNavbar from '$lib/components/ui/subnavbar/SubNavbar.svelte';
+    import UserCard from '$lib/components/lemmy/user/UserCard.svelte';
+    import Placeholder from '$lib/components/ui/Placeholder.svelte';
+    import type { UserSubmissionFeedController } from '$lib/components/lemmy/feed/helpers';
+    
 
-    // Page state that will persist in snapshots
-    let pageState = {
-        scrollY: 0,
-    }
-
-    // Store and reload the page data between navigations (Override functions to use LocalStorage instead of Session Storage)
-    // Also jumps to last seen post upon restore.
-    export const snapshot: Snapshot<void> = {
-        capture: () => {
-            pageState.scrollY = window.scrollY
-            if ($userSettings.uiState.infiniteScroll) PageSnapshot.capture({data: data, state: pageState})
-        },
-        restore: async () => {
-            try { 
-                if ($userSettings.uiState.infiniteScroll)  {
-                    let snapshot = PageSnapshot.restore() 
-                    if (snapshot.data)  data = snapshot.data
-                    if (snapshot.state) pageState = snapshot.state
-                }
-
-                await scrollToLastSeenPost(data.items.length + 200)
-            }
-            catch { 
-                PageSnapshot.clear() 
-                window.scrollTo(0,0)
-            }
-        }
-    }
-
-
+    //export let data
+    let feedController: UserSubmissionFeedController
 </script>
 
+<svelte:head>
+    <title>Profile {$profile?.user ? `| ${$profile.user.local_user_view.person.display_name ?? $profile.user.local_user_view.person.name}` : ''}</title>
+</svelte:head>
 
-<UserPage bind:data={data}/>
+<SubNavbar home back quickSettings toggleCommunitySidebar toggleMargins 
+    refreshButton refreshPreventDefault
+    on:navRefresh={()=> feedController.refresh(true)}    
+
+    scrollButtons scrollPreventDefault 
+    on:navScrollBottom={() => feedController.scrollBottom() } 
+    on:navScrollTop={() => feedController.scrollTop() }
+
+/>
+
+{#if $profile?.user}
+    <MainContentArea>
+        
+        <ProfileMenuBar />
+
+        <div class="flex w-full" style="height: calc(100vh - 11.6rem);">
+            <FeedContainer>
+
+                <UserSubmissionFeed bind:controller={feedController} bind:person_id={$profile.user.local_user_view.person.id}  actions >
+                    
+                    <!---Add the User's Profile Card, About Me, and List of Communities they Moderate Above the Feed--->                    
+                    <div class="flex 2xl:hidden flex-col mx-auto w-full gap-2 max-w-[820px]" slot="banner" let:user>
+                        {#if user}
+                            
+                            <UserCardSmall person_view={user.person_view} />
+                            
+                            <!---Person Bio--->
+                            {#if user.person_view.person.bio}
+                                <CollapseButton icon={UserCircle} title="About Me" expanded={false}>
+                                    <Markdown source={user.person_view.person.bio} />
+                                </CollapseButton>
+                            {/if}
+
+                            <!---List of Communities Moderated--->
+                            {#if user.moderates?.length > 0}
+                                <CollapseButton icon={ShieldCheck} title="Moderates">
+                                    {#each user.moderates as community, idx (community.community.id)}
+                                        <CommunityLink community={community.community} avatar />
+                                    {/each}
+                                </CollapseButton>
+                            {/if}
+
+                        {/if}
+                    </div>
+
+                </UserSubmissionFeed>
+            </FeedContainer>
+        </div>
+
+        <UserCard slot="right-panel" moderates={$profile.user.moderates} person={
+                {
+                    person: $profile.user.local_user_view.person,
+                    is_admin: $profile.user.local_user_view.local_user.admin,
+                    counts: $profile.user.local_user_view.counts
+                }
+            }
+        />
+
+    </MainContentArea>
+{:else}
+    <div class="flex w-full h-full">    
+        <Placeholder icon={HandRaised} title="Not Logged In" description="You must be logged in to access this section" class="mx-auto"/>
+    </div>
+{/if}
+
+
 
