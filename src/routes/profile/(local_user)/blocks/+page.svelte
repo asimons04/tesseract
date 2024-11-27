c266c5479044e201dd0d812ff9924dd73b20ef3c
Admiral Patrick <asimons@ptznetwork.org>
2024-11-26 20:04:04 -0500

    Update to not use shared layout

--- src/routes/profile/(local_user)/blocks/+page.svelte
+++ src/routes/profile/(local_user)/blocks/+page.svelte
@@ -7,13 +7,16 @@
     import { refreshProfile } from '$lib/lemmy/user.js'
     import { slide } from 'svelte/transition'
 
+
     import Button from '$lib/components/input/Button.svelte'
     import CollapseButton from '$lib/components/ui/CollapseButton.svelte'
     import CommunityLink from '$lib/components/lemmy/community/CommunityLink.svelte'
     import EditableList from '$lib/components/ui/list/EditableList.svelte'
     import MainContentArea from '$lib/components/ui/containers/MainContentArea.svelte';
     import Placeholder from '$lib/components/ui/Placeholder.svelte'
+    import ProfileMenuBar from '$routes/profile/ProfileMenuBar.svelte';
     import SiteLink from '$lib/components/lemmy/SiteLink.svelte'
+    import SubNavbar from '$lib/components/ui/subnavbar/SubNavbar.svelte';
     import UserLink from '$lib/components/lemmy/user/UserLink.svelte'
 
     import { 
@@ -23,6 +26,9 @@
         User,
         UserGroup,
     } from 'svelte-hero-icons'
+    import SiteCard from '$lib/components/lemmy/SiteCard.svelte';
+    
+    
 
     let blocking = false
     
@@ -69,16 +75,15 @@
     <title>Profile | Blocks</title>
 </svelte:head>
 
-<h1 class="flex flex-row justify-between">
-    <span class="font-bold text-2xl">Blocks</span>
-</h1>
-
+<SubNavbar back quickSettings scrollButtons toggleCommunitySidebar/>
 
 <MainContentArea>
-{#if $profile?.user}
+    <ProfileMenuBar />
+    {#if $profile?.user}
     
     <!---User Blocks--->
     <CollapseButton title="Users" icon={User} heading={true} innerClass="max-h-[50vh] overflow-y-auto">
+        
         {#if $profile.user.person_blocks.length > 0}
             <EditableList let:action on:action={(i) => unblockUser(i.detail)}>
                 {#each $profile.user.person_blocks as block (block.target.id)}
@@ -136,8 +141,13 @@
             <Placeholder description="You have not blocked any instances yet." title="No instance blocks" icon={Check} />
         {/if}
     </CollapseButton>
-    
 {/if}
+
+    <div class="h-full" slot="right-panel">
+        {#if $site}
+            <SiteCard site={$site.site_view} taglines={$site.taglines} version={$site.version} admins={$site.admins} />
+        {/if}
+    </div>
 </MainContentArea>
 
 
