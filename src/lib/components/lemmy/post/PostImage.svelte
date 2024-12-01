556017eb04af4d29a9f61ffe4fdd669d412c3a3b
Admiral Patrick <asimons@ptznetwork.org>
2024-12-01 08:18:07 -0500

    Refactor reactive setup function

--- src/lib/components/lemmy/post/PostImage.svelte
+++ src/lib/components/lemmy/post/PostImage.svelte
@@ -21,22 +21,24 @@
 
 
     // Finesse the url and thumbnail URL to accommodate GIFs (and not thumbnail webms ugh) or when the thumbnanil is a static image but the embed URL is a GIF (Imgur)
-    $:  {
-            url = post.post.url as string
+    $:  post.post.id, setup()
+    
+    function setup() {
+        url = post.post.url as string
 
-            // Get Imgur gifs to render without having to click through to the site.
-            if (!url?.endsWith('.gif')  && post?.post?.embed_video_url?.endsWith('.gif')) {
-                url = post.post.embed_video_url
-            }
-
-            // Hack to get GIFs to play in the feed.  Lemmy converts them to weird webm at best.
-            if (displayType == 'feed' && url?.endsWith('.gif')) {
-                thumbnail_url = url;
-            }
-            else {
-                thumbnail_url = (post.post.thumbnail_url as string ?? post.post.url as string)
-            }
+        // Get Imgur gifs to render without having to click through to the site.
+        if (!url?.endsWith('.gif')  && post?.post?.embed_video_url?.endsWith('.gif')) {
+            url = post.post.embed_video_url
         }
+
+        // Hack to get GIFs to play in the feed.  Lemmy converts them to weird webm at best.
+        if (displayType == 'feed' && url?.endsWith('.gif')) {
+            thumbnail_url = url;
+        }
+        else {
+            thumbnail_url = (post.post.thumbnail_url as string ?? post.post.url as string)
+        }
+    }
    
 </script>
 
@@ -44,7 +46,7 @@
     {#if clickToPlay}
         <ClickToPlayOverlay bind:show={clickToPlay} displayType={displayType} on:click={(e)=> dispatcher('click', e)}/>
         
-        <ZoomableImage url={thumbnail_url} bind:nsfw={post.post.nsfw} altText={post.post.name} zoomable={zoomable}
+        <ZoomableImage bind:url={thumbnail_url} bind:nsfw={post.post.nsfw} altText={post.post.name} zoomable={zoomable}
             class="ml-auto mr-auto object-cover rounded-md min-h-[min(40vh,800px)] max-h-[min(50vh,800px)] z-20 {$$props.class}"
             on:click={(e)=> dispatcher('click', e)}
         />
