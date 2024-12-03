83fdf318ff35efbb1bf1b12ceb92cf76011be2bb
Admiral Patrick <asimons@ptznetwork.org>
2024-12-02 19:55:19 -0500

    Remove unused imports, enable session storage caching for site lookups

--- src/routes/u/[name]/+page.ts
+++ src/routes/u/[name]/+page.ts
@@ -1,13 +1,16 @@
-import type { SortType, GetPersonDetailsResponse, GetSiteResponse } from 'lemmy-js-client'
+import type { GetSiteResponse } from 'lemmy-js-client'
 
 import { get } from 'svelte/store'
 import { instance as currentInstance} from '$lib/instance'
-import { profile } from '$lib/auth.js'
 import { getClient, site } from '$lib/lemmy.js'
-import { getItemPublished } from '$lib/lemmy/item.js'
+import { StorageController } from '$lib/storage-controller'
 
-import { userSettings } from '$lib/settings'
 
+const storage = new StorageController({
+    type: 'session',
+    ttl: -1,
+    useCompression: true   
+})
 
 interface LoadParams {
     params: any,
@@ -15,19 +18,31 @@ interface LoadParams {
 }
 
 export async function load({ params, url }: LoadParams) {
+    // Fetch the site info for the provided user. Default to current instance.
     let instance =  get(currentInstance)
     let siteData: GetSiteResponse
     
+    // If the username is non-local (e.g. user@instance), grab the domain part. Treat usernames without instances as local
     if (params.name && params.name.split('@')[1]) instance = params.name.split('@')[1]
 
     try {
-        if (instance == get(currentInstance)){
-            siteData = get(site) ?? await getClient().getSite()
+        // Check the session storage cache to see if this instance has been cached
+        siteData = await storage.get(`getSite:${instance}`)
+        
+        // If site data for that instance isn't already cached, fetch it and cache it.
+        if (!siteData) {
+            siteData = await getClient().getSite()
+            
+            // Don't store the user's profile data in case this is the local instance
+            if (siteData?.my_user) delete siteData.my_user
+            
+            // Cache the site data response for that instance for later
+            storage.put(`getSite:${instance}`, siteData)
         }
-        else {
-            siteData = await getClient(instance).getSite()
-        }
+        
     }
+    
+    // If any issue, just use the site data for the home instance
     catch {
         siteData = get(site) ?? await getClient().getSite()
     }
