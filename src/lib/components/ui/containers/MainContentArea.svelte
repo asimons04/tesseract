eea21f6a3062f3c4781b8f7ab369724965dde282
Admiral Patrick <asimons@ptznetwork.org>
2024-12-03 13:28:45 -0500

    Pass class props

--- src/lib/components/ui/containers/MainContentArea.svelte
+++ src/lib/components/ui/containers/MainContentArea.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col-reverse  xl:flex-row gap-1 max-w-full w-full h-full py-0">
     
-    <div class="flex flex-col gap-0 max-w-full w-full h-full min-w-0">
+    <div class="flex flex-col gap-0 max-w-full w-full h-full min-w-0 {$$props.class}">
         <slot/>
     </div>
     
