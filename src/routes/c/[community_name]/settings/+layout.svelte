ed6cc74b0eceb08baad3f2764bcfc132653e407b
Admiral Patrick <asimons@ptznetwork.org>
2024-12-03 13:29:19 -0500

    Put section nav bar into card

--- src/routes/c/[community_name]/settings/+layout.svelte
+++ src/routes/c/[community_name]/settings/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-    import type { PageData } from './$types.js'
-
     import { amMod, isAdmin, isTopMod } from '$lib/components/lemmy/moderation/moderation.js';
     import { fullCommunityName } from '$lib/util.js'
     import { profile } from '$lib/auth.js';
     import { page } from '$app/stores'
     
     import Button from '$lib/components/input/Button.svelte'
+    import Card from '$lib/components/ui/Card.svelte';
     import CommunityCard from '$lib/components/lemmy/community/CommunityCard.svelte'
     import MainContentArea from '$lib/components/ui/containers/MainContentArea.svelte'
     import Placeholder from '$lib/components/ui/Placeholder.svelte';
@@ -19,8 +18,7 @@
         UserGroup
     } from 'svelte-hero-icons'
     
-    
-    export let data: PageData
+    export let data
 
     $: communityUrl = `/c/${fullCommunityName(
         data.community.community_view.community.name,
@@ -35,27 +33,29 @@
     <MainContentArea>
             
         <!---Profile Sub-Page Buttons--->
-        <div class="sticky top-[6.9rem] flex flex-row gap-1 -ml-2 px-2 py-1 w-[calc(100%+1rem)] bg-slate-50/80 dark:bg-zinc-950/80 backdrop-blur-3xl z-10">
-            <Button color="tertiary" alignment="left" title="Settings" class="hover:bg-slate-200" href="{communityUrl}/settings">
-                <span class="flex flex-col items-center {$page.url.pathname==`${communityUrl}/settings` || $page.url.pathname==`${communityUrl}/settings/` ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
-                    <Icon src={Cog6Tooth} mini size="18" title="Settings" />
-                    <span class="text-xs">Settings</span>
-                </span>            
-            </Button>
-            
-            <Button color="tertiary" alignment="left" title="Team" class="hover:bg-slate-200" href="{communityUrl}/settings/team">
-                <span class="flex flex-col items-center {$page.url.pathname.startsWith(`${communityUrl}/settings/team`) ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
-                    <Icon src={UserGroup} mini size="18" title="Team" />
-                    <span class="text-xs">Team</span>
-                </span>            
-            </Button>
+        <div class="sticky top-[6.9rem] flex flex-row gap-1 px-2 py-1 w-full bg-slate-50/80 dark:bg-zinc-950/80 backdrop-blur-3xl z-10">
+            <Card class="flex flex-row w-full">
+                <Button color="tertiary" alignment="left" title="Settings" class="hover:bg-slate-200" href="{communityUrl}/settings">
+                    <span class="flex flex-col items-center {$page.url.pathname==`${communityUrl}/settings` || $page.url.pathname==`${communityUrl}/settings/` ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                        <Icon src={Cog6Tooth} mini size="18" title="Settings" />
+                        <span class="text-xs">Settings</span>
+                    </span>            
+                </Button>
+                
+                <Button color="tertiary" alignment="left" title="Team" class="hover:bg-slate-200" href="{communityUrl}/settings/team">
+                    <span class="flex flex-col items-center {$page.url.pathname.startsWith(`${communityUrl}/settings/team`) ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                        <Icon src={UserGroup} mini size="18" title="Team" />
+                        <span class="text-xs">Team</span>
+                    </span>            
+                </Button>
 
-            <Button color="tertiary" alignment="left" title="Moderation" class="hover:bg-slate-200" href="{communityUrl}/settings/moderation">
-                <span class="flex flex-col items-center {$page.url.pathname.startsWith(`${communityUrl}/settings/moderation`) ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
-                    <Icon src={HandRaised} mini size="18" title="Moderation" />
-                    <span class="text-xs">Moderation</span>
-                </span>            
-            </Button>
+                <Button color="tertiary" alignment="left" title="Moderation" class="hover:bg-slate-200" href="{communityUrl}/settings/moderation">
+                    <span class="flex flex-col items-center {$page.url.pathname.startsWith(`${communityUrl}/settings/moderation`) ? 'text-sky-700 dark:text-sky-500 font-bold' : '' }">
+                        <Icon src={HandRaised} mini size="18" title="Moderation" />
+                        <span class="text-xs">Moderation</span>
+                    </span>            
+                </Button>
+            </Card>
         </div>
             
         <!---Content Area for Child Pages--->
