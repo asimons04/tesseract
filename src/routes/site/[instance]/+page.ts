07690ac203687076d2fc61bc3ba09ce03de03350
Admiral Patrick <asimons@ptznetwork.org>
2024-12-08 09:00:14 -0500

    Add caching to getSite lookup on /site route

--- src/routes/site/[instance]/+page.ts
+++ src/routes/site/[instance]/+page.ts
@@ -1,14 +1,32 @@
+import { StorageController } from '$lib/storage-controller'
+
 import { getClient } from '$lib/lemmy.js'
 import { error } from '@sveltejs/kit'
+import type { GetSiteResponse } from 'lemmy-js-client'
+
+const storage = new StorageController({
+    type: 'session',
+    ttl: 60,
+    useCompression: true   
+})
 
 export async function load(req: any) {
     try {
-        return {
-            site: await getClient(req.params.instance).getSite(),
+        const storageKey = `getSite:${req.params.instance}`
+        let getSiteResponse: GetSiteResponse | undefined = await storage.get(storageKey)
+
+        if (!getSiteResponse) {
+            getSiteResponse = await getClient(req.params.instance).getSite()
+            storage.put(storageKey, getSiteResponse)
         }
+
+        return {
+            site: getSiteResponse
+        }
+
     } catch (err) {
         throw error(500, {
-            message: 'Failed to fetch legal page.',
+            message: 'Failed to fetch remote site info.',
         })
     }
 }
