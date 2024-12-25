2c7e0a61305d868e6a75e0df033034e06a6b33f1
Admiral Patrick <asimons@ptznetwork.org>
2024-12-25 13:04:06 -0500

    Remove menu-based buttons and add discrete buttons for edit/delete operations

--- src/lib/components/ui/sidebar/CommunityGroup.svelte
+++ src/lib/components/ui/sidebar/CommunityGroup.svelte
@@ -8,13 +8,10 @@
 
     import Button from '$lib/components/input/Button.svelte'
     import CommunityList from '$lib/components/ui/sidebar/CommunityList.svelte'
-    import Menu from '$lib/components/ui/menu/Menu.svelte'
-    import MenuButton from '$lib/components/ui/menu/MenuButton.svelte'
     import SidebarButton from '$lib/components/ui/sidebar/SidebarButton.svelte'
 
     import {
         Icon,
-        Bars3,
         Folder,
         FolderOpen,
         PencilSquare,
@@ -44,67 +41,46 @@
 
 
 {#if group && expanded && (showEmptyGroups || group.communities?.length > 0)}
-    <div class="flex flex-col gap-1">    
+    <div class="flex flex-col px-2 gap-1">    
         
-        <div class="border border-transparent bg-transparent hover:bg-slate-200 hover:dark:bg-zinc-800 dark:text-zinc-200  border-none disabled:border-none
-            flex flex-row w-full px-2 gap-1 items-center rounded-md {open ? '!bg-slate-200 dark:!bg-zinc-800' : ''}"
-        >
-            <SidebarButton class="w-full {open ? '' : 'opacity-80'}" title="{open ? 'Collapse' : 'Expand'} {group.name}" on:click={ (e) => {
+        <div class="flex flex-row w-full gap-1 items-center rounded-md {open ? '!bg-slate-200 dark:!bg-zinc-800' : ''}">
+            <SidebarButton class="w-[calc(100%-96px)] {open ? '' : 'opacity-80'}" 
+                title="{open ? 'Collapse' : 'Expand'} {group.name}" 
+                icon={open ? FolderOpen : Folder}
+                iconSize={16}
+                on:click={ (e) => {
                     e.preventDefault()
                     e.stopPropagation()
                     open = !open
                 }}
             >
-                <Icon src={open ? FolderOpen : Folder} mini size="16"/>
-                <span class="font-bold">{group.name}</span>
+                <span class="w-[calc(100%-18px)] {open ? 'font-bold' : ''} truncate">{group.name}</span>
             </SidebarButton>
 
             <span class="ml-auto"/>
 
-            {#if expanded}
-                <!--<Button size="sm" color="ghost" class="border-none" icon={ArrowTopRightOnSquare} href="/feeds/{group.name}" title="View as Feed"/>-->
+            <span class="{expanded ? 'flex' : 'hidden'} flex-row gap-2 items-center">
+                <!---Edit Group --->
+                <Button title="Edit Group" 
+                    icon={PencilSquare} iconSize={16}
+                    on:click={(e)=> {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        editCommunityGroup(group)
+                    }}
+                />
 
-                <Menu alignment="bottom-right" itemsClass="h-8 md:h-8" containerClass="!max-h-[90vh] max-w-[18rem]">
-                    
-                    <Button color="tertiary" slot="button" let:toggleOpen title="Group Options" 
-                        icon={Bars3} iconSize={16}
-                        on:click={(e) => {
-                            e.preventDefault()
-                            e.stopPropagation()
-                            toggleOpen()
-                        }}
-                    />
-                    
-                    <!---Group Name Header--->
-                    <span class="px-4 py-1 my-1 text-xs text-slate-600 dark:text-zinc-400">
-                        {group.name}
-                    </span>
+                <!---Delete Group --->
+                <Button title="Delete Group" 
+                    icon={Trash} iconSize={16}
+                    on:click={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        deleteGroup()
+                    }}
+                />
+            </span>
             
-
-                    <!---Edit Group --->
-                    <MenuButton title="Edit Group" on:click={(e)=> {
-                            e.preventDefault()
-                            e.stopPropagation()
-                            editCommunityGroup(group)
-                        }}
-                    >
-                        <Icon src={PencilSquare} mini size="16" />
-                        Edit Group
-                    </MenuButton>
-
-                    <!---Delete Post --->
-                    <MenuButton title="Delete Group" on:click={(e) => {
-                            e.preventDefault()
-                            e.stopPropagation()
-                            deleteGroup()
-                        }}
-                    >
-                        <Icon src={Trash} mini size="16"/>
-                        Delete Group
-                    </MenuButton>
-                    
-                </Menu>
-            {/if}
         </div>
         
         {#if open}
