14e53e8ba56642ed2cf784692ddbdba252c4c208
Admiral Patrick <asimons@ptznetwork.org>
2024-12-25 13:00:42 -0500

    Add params to pass icon and icon size to inner Button component

--- src/lib/components/ui/sidebar/SidebarButton.svelte
+++ src/lib/components/ui/sidebar/SidebarButton.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import Button from '$lib/components/input/Button.svelte'
+    import type { IconSource } from 'svelte-hero-icons'
 
     export let expanded: boolean = true
     export let title: string = ''
     export let disabled: boolean = false
+    export let icon: IconSource | undefined = undefined
+    export let iconSize: number | undefined = undefined
 </script>
 
-<Button {...$$restProps} {disabled} color="tertiary" alignment="left" title={title} on:click class="!text-xs font-bold hover:bg-slate-200 {expanded ? '' : '!p-1.5'} {$$props.class}">
+<Button {...$$restProps} {icon} {iconSize} {disabled} color="tertiary" alignment="left" title={title} on:click class="!text-xs font-bold hover:bg-slate-200 {expanded ? '' : '!p-1.5'} {$$props.class}">
     <slot />
 </Button>
