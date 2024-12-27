41a061526f6345aefdff16c75fca1bb01d35a880
Admiral Patrick <asimons@ptznetwork.org>
2024-12-27 15:19:17 -0500

    Move rounded class to colorClass values

--- src/lib/components/ui/Card.svelte
+++ src/lib/components/ui/Card.svelte
@@ -14,22 +14,22 @@
                     case -1:
                         return ''
                     case 0:
-                        return 'border border-slate-300 dark:border-zinc-900 bg-slate-200 dark:bg-zinc-950'
+                        return 'border border-slate-300 dark:border-zinc-900 bg-slate-200 dark:bg-zinc-950 rounded-2xl'
                     case 1:
-                        return 'border border-slate-300 dark:border-zinc-800 bg-slate-100 dark:bg-zinc-900'
+                        return 'border border-slate-300 dark:border-zinc-800 bg-slate-100 dark:bg-zinc-900 rounded-2xl'
 
                     case 2:
-                        return 'border border-slate-300 dark:border-zinc-900 bg-slate-200 dark:bg-zinc-800'
+                        return 'border border-slate-300 dark:border-zinc-900 bg-slate-200 dark:bg-zinc-800 rounded-2xl'
                 }
             
             case 'warning':
-                return 'border border-yellow-300 dark:bg-yellow-950/30 dark:border-yellow-900 bg-yellow-50'
+                return 'border border-yellow-300 dark:bg-yellow-950/30 dark:border-yellow-900 bg-yellow-50 rounded-2xl'
 
             case 'success':
-                return 'border border-green-300 dark:bg-green-950/30 dark:border-green-900 bg-green-50'
+                return 'border border-green-300 dark:bg-green-950/30 dark:border-green-900 bg-green-50 rounded-2xl'
 
             case 'error':
-                return 'border border-red-300 dark:bg-red-950/30 dark:border-red-900 bg-red-50'
+                return 'border border-red-300 dark:bg-red-950/30 dark:border-red-900 bg-red-50 rounded-2xl'
         }
     }
 
@@ -51,7 +51,7 @@
         </div>
     </div>
 {:else}
-    <div class="break-words {colorClass} rounded-2xl {clazz}" {...$$restProps}>
+    <div class="break-words {colorClass} {clazz}" {...$$restProps}>
         <slot />
     </div>
 {/if}
