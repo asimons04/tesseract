82228ec381ca73928578a72ef1d83a8bfaf1caa2
Admiral Patrick <asimons@ptznetwork.org>
2024-12-28 11:39:18 -0500

    Add custom MD renderer for tables; wrap in horizontally-scrolling div

---
+++ src/lib/components/markdown/renderers/MarkdownTable.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import type { Tokens } from 'marked'
+    import {
+        MarkdownTokens,
+        type MarkdownOptions,
+        type Renderers,
+    } from '@magidoc/plugin-svelte-marked'
+
+    export let token: Tokens.Table
+    export let options: MarkdownOptions
+    export let renderers: Renderers
+</script>
+
+<div class="max-w-[90%] overflow-x-scroll">
+    <table>
+        <thead>
+            <tr>
+                {#each token.header as item}
+                <th scope="col">
+                    <MarkdownTokens tokens={item.tokens} {options} {renderers} />
+                </th>
+                {/each}
+            </tr>
+        </thead>
+        
+        <tbody>
+        {#each token.rows as row}
+            <tr>
+            {#each row as col}
+                <td>
+                    <MarkdownTokens tokens={col.tokens} {options} {renderers} />
+                </td>
+            {/each}
+            </tr>
+        {/each}
+        </tbody>
+    </table>
+</div>
