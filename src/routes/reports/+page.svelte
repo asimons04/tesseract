19aded2e01b64e5b927929c04dedebacfa19a283
Admiral Patrick <asimons@ptznetwork.org>
2025-01-02 07:56:07 -0500

    Add /reports redirect to /moderation so report email links work correctly

---
+++ src/routes/reports/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    // The API's emails send a link to /reports, so set up a redirect to /moderation here
+    
+    import { goto } from '$app/navigation'
+    goto(`/moderation`)
+</script>
+
