aaaffc1192d7900e27553c09adff4db5923f4e38
Admiral Patrick <asimons@ptznetwork.org>
2025-01-07 19:27:36 -0500

    Standardize divider colors

--- src/lib/components/ui/settings/SettingGroup.svelte
+++ src/lib/components/ui/settings/SettingGroup.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+    import { dividerColors } from '$lib/ui/colors'
     import Card from '$lib/components/ui/Card.svelte'
 </script>
 
-<Card class="flex flex-col divide-y gap-3 p-4 text-sm my-2">
+<Card class="flex flex-col divide-y {dividerColors} gap-3 p-4 text-sm my-2">
     <slot />
 </Card>
