21cae19751305a4e7bff69d0b3d6beda3a8512af
Admiral Patrick <asimons@ptznetwork.org>
2025-01-13 15:56:15 -0500

    Reduce custom emoji from 48->24px, set inline-flex on image for vertical alignment

--- src/lib/components/markdown/renderers/MarkdownImage.svelte
+++ src/lib/components/markdown/renderers/MarkdownImage.svelte
@@ -71,7 +71,7 @@
             <!---Image--->
             {:else}
                 {#if isEmoji}
-                    <ZoomableImage url={token.href} altText={token.text} class="w-[48px] h-[48px] mx-1 mt-1"/>
+                    <ZoomableImage url={token.href} altText={token.text} class="inline-flex w-[24px] h-[24px] mx-1 mt-1"/>
                 {:else}
                     <ZoomableImage url={token.href} altText={token.text} class="mx-auto rounded-xl"/>
                 {/if}
