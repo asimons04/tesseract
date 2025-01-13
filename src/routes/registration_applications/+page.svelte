57ec25cdf90b2dfc1b8c5dc74aae7ccdcb90f420
Admiral Patrick <asimons@ptznetwork.org>
2025-01-13 12:20:23 -0500

    Add redirect /registration_applications -> /admin/applications

---
+++ src/routes/registration_applications/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    // The API's emails send a link to /registration_applications, so set up a redirect to /admin/applications here
+    
+    import { goto } from '$app/navigation'
+    goto(`/admin/applications`)
+</script>
+
