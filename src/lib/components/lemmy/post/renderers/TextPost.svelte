571e71acdaebfee5f3c9b9fb777100df2281b064
Admiral Patrick <asimons@ptznetwork.org>
2025-01-17 14:52:31 -0500

    Create new renderer for 'text' type posts

---
+++ src/lib/components/lemmy/post/renderers/TextPost.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type { PostDisplayType } from '../helpers.js'
+    import type { PostView } from 'lemmy-js-client'
+
+    import Crossposts from '../components/Crossposts.svelte'
+    import PostActions from '../PostActions.svelte'
+    import PostBody from '../components/PostBody.svelte'
+    import PostMeta from '../components/PostMeta.svelte'
+
+    // Standard for all post types
+    export let post:PostView
+    export let actions: boolean = true
+    export let inCommunity = false
+    export let inProfile = false
+    export let displayType: PostDisplayType = 'feed'
+    export let collapseBadges = false
+    export let postType = 'text'
+    export let inViewport = true
+    export let compact: boolean = true
+</script>
+
+<PostMeta {post} showTitle={true} {collapseBadges} {actions} {inCommunity} {inProfile} {compact} on:toggleCompact={() => compact = !compact} />
+<PostBody {post} {displayType}  />
+<Crossposts {post} size="xs" class="mb-1 !pl-0"/>
+<PostActions {post} {displayType} on:reply class="mt-2" />
