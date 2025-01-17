253aa4ad58d601ccf1b9c9fe52e19d4f0e834882
Admiral Patrick <asimons@ptznetwork.org>
2025-01-17 14:54:11 -0500

    Continue phase out of renderers that have been re-implemented

--- src/lib/components/lemmy/post/PostMediaRenderers.svelte
+++ src/lib/components/lemmy/post/PostMediaRenderers.svelte
@@ -5,20 +5,10 @@
     import PostAudio from '$lib/components/lemmy/post/PostAudio.svelte'
     import PostBandcamp from '$lib/components/lemmy/post/PostBandcamp.svelte'
     import PostDailymotion from '$lib/components/lemmy/post/PostDailymotion.svelte'
-    //import PostLink from '$lib/components/lemmy/post/PostLink.svelte'
-    //import PostLoops from './PostLoops.svelte'
-    //import PostImage from '$lib/components/lemmy/post/PostImage.svelte'
     import PostOdysee from '$lib/components/lemmy/post/PostOdysee.svelte'
-    import PostPeerTube from '$lib/components/lemmy/post/PostPeerTube.svelte'
     import PostSongLink from '$lib/components/lemmy/post/PostSongLink.svelte'
-    import PostSpotify from '$lib/components/lemmy/post/PostSpotify.svelte'
     import PostSoundCloud from '$lib/components/lemmy/post/PostSoundCloud.svelte'
-    //import PostVideo from '$lib/components/lemmy/post/PostVideo.svelte'
-    import PostVimeo from '$lib/components/lemmy/post/PostVimeo.svelte'
-    //import PostYouTube from '$lib/components/lemmy/post/PostYouTube.svelte'
-    
-
-    
+   
     export let post: PostView
     export let postType: PostType
     export let displayType: PostDisplayType
@@ -42,20 +32,12 @@
 {/if}
 
 
-<!--- Spotify Embed --->
-{#if postType == "spotify"}
-    <PostSpotify bind:post bind:inViewport  {displayType} />
-{/if}
 
 <!--- Soundcloud Embed --->
 {#if postType == "soundcloud"}
     <PostSoundCloud bind:post bind:inViewport {displayType} />
 {/if}
 
-<!--- Vimeo Embed --->
-{#if postType == "vimeo"}
-    <PostVimeo bind:post bind:inViewport {displayType} />
-{/if}
 
 <!--- Odysee Embed --->
 {#if postType == "odysee"}
@@ -67,10 +49,6 @@
     <PostSongLink bind:post bind:inViewport {displayType} />
 {/if}
 
-<!---Peertube Embed--->
-{#if postType == 'peertube'}
-    <PostPeerTube bind:post bind:inViewport {displayType} />
-{/if}
 
 <!---Dailymotion Embed--->
 {#if postType == 'dailymotion'}
