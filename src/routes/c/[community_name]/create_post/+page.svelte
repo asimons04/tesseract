db4014499f371e84285e64db80f238747b4bf5a9
Admiral Patrick <asimons@ptznetwork.org>
2025-01-17 10:43:12 -0500

    Make typescript happy

--- src/routes/c/[community_name]/create_post/+page.svelte
+++ src/routes/c/[community_name]/create_post/+page.svelte
@@ -1,32 +1,38 @@
 <script lang="ts">
+    import type { EditPostEvent } from '$lib/ui/events'
+
     import { goto } from '$app/navigation';
-    import { shortenCommunityName } from  '$lib/components/lemmy/community/helpers'
-    
+
     import CommunityCard from '$lib/components/lemmy/community/CommunityCard.svelte';
     import PostForm from '$lib/components/lemmy/post/PostForm.svelte'
     import SubNavbar from '$lib/components/ui/subnavbar/SubNavbar.svelte';
     import MainContentArea from '$lib/components/ui/containers/MainContentArea.svelte';
-    import FeedContainer from '$lib/components/ui/containers/FeedContainer.svelte';
     
     export let data
+    
 </script>
 
 <svelte:head>
-    <title>Create post in {data.community.community_view.community.title}</title>
+    <title>Create post in {data?.community?.community_view.community.title ?? 'Community'}</title>
 </svelte:head>
+
+
 
 <SubNavbar home back toggleMargins toggleCommunitySidebar />
 
-<MainContentArea>
-    
-    <h1 class="text-2xl font-bold">
-        Create Post in {data.community.community_view.community.title ?? data.community.community_view.community.name}
-    </h1>    
-    <PostForm bind:community={data.community.community_view.community} hideCommunityInput={true}
-        on:submit={(e) => goto(`/post/${e.detail.post.id}`)}
-    >
-    </PostForm>
-    
+{#if data?.community}
+    <MainContentArea>
 
-    <CommunityCard community_view={data.community.community_view} moderators={data.community.moderators} slot="right-panel" />
-</MainContentArea>
+        <h1 class="text-2xl font-bold">
+            Create Post in {data.community.community_view.community.title ?? data.community.community_view.community.name}
+        </h1>    
+
+        <PostForm community={data.community.community_view.community} hideCommunityInput={true} on:submit={(e) => { 
+                if (e?.detail?.post) goto(`/post/${e.detail.post.id}`)
+            }}
+        />
+
+        <CommunityCard community_view={data.community.community_view} moderators={data.community.moderators} slot="right-panel" />
+
+    </MainContentArea>
+{/if}
