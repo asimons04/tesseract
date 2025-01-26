d1afe71a36f762f60cb000baf624c3ee19c93fbf
Admiral Patrick <asimons@ptznetwork.org>
2025-01-25 20:01:47 -0500

    Use generated iframe URL first

--- src/lib/components/lemmy/post/utils/IFrame.svelte
+++ src/lib/components/lemmy/post/utils/IFrame.svelte
@@ -31,7 +31,7 @@
                 allow="accelerometer; fullscreen; encrypted-media; gyroscope; picture-in-picture" 
                 loading="lazy"
                 allowfullscreen 
-                title="Embedded Content: {title}"
+                title=""
                 target="_top"
             >
             </iframe>
