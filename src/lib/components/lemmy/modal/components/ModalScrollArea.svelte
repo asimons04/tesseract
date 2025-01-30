6a979a611f556cb19f8d595b17c35fbc7676e6f0
Admiral Patrick <asimons@ptznetwork.org>
2025-01-29 21:17:20 -0500

    Allow binding to card div

--- src/lib/components/lemmy/modal/components/ModalScrollArea.svelte
+++ src/lib/components/lemmy/modal/components/ModalScrollArea.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import Card from "$lib/components/ui/Card.svelte"
     export let card: boolean = true
+    export let div: HTMLDivElement | undefined = undefined
 </script>
 
-<Card elevation={card ? 1 : -1} 
+<Card bind:div elevation={card ? 1 : -1} 
     class="flex flex-col gap-2 {card ? 'p-2' : ''} w-full max-h-full overflow-y-auto {$$props.class}"
 >
     <slot />
