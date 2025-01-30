ea064d0de69009cc893148553b7121873efff66f
Admiral Patrick <asimons@ptznetwork.org>
2025-01-29 22:16:38 -0500

    Add post status icons, add buttons to jump to item in modal or tab

--- src/routes/profile/(local_user)/inbox/InboxItem.svelte
+++ src/routes/profile/(local_user)/inbox/InboxItem.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import type { ExpandAllInboxItemEvent } from '$lib/ui/events'
+    import { dispatchWindowEvent, type ExpandAllInboxItemEvent } from '$lib/ui/events'
     import type { InboxFeedType } from './+page'
     import type {
         CommentReplyView,
@@ -13,22 +13,26 @@
     import { goto } from '$app/navigation'
     import { getInboxItemPublished, isRead } from '$lib/lemmy/inbox'
     import { isCommentReply, isPersonMention, isPostReply } from '$lib/lemmy/item'
+    import { instance } from '$lib/instance'
     import { page } from '$app/stores'
+    import { postViewerModal } from '$lib/components/lemmy/moderation/moderation';
     import { profile } from '$lib/auth.js'
     import { toast } from '$lib/components/ui/toasts/toasts.js'
     import { userSettings } from '$lib/settings'
     
-    import Button from '$lib/components/input/Button.svelte'
-    import Card from '$lib/components/ui/Card.svelte'
-    import CollapseButton from '$lib/components/ui/CollapseButton.svelte';
-    import Comment from '$lib/components/lemmy/comment/Comment.svelte'
-    import CommunityLink from '$lib/components/lemmy/community/CommunityLink.svelte';
-    import MarkdownEditor from '$lib/components/markdown/MarkdownEditor.svelte';
-    import PrivateMessageItem from '$lib/components/lemmy/private_message/PrivateMessageItem.svelte'
-    import RelativeDate from '$lib/components/util/RelativeDate.svelte';
-    import ReportModal from '$lib/components/lemmy/moderation/ReportModal.svelte'
+    import Button               from '$lib/components/input/Button.svelte'
+    import Card                 from '$lib/components/ui/Card.svelte'
+    import CollapseButton       from '$lib/components/ui/CollapseButton.svelte';
+    import Comment              from '$lib/components/lemmy/comment/Comment.svelte'
+    import CommunityLink        from '$lib/components/lemmy/community/CommunityLink.svelte';
+    import MarkdownEditor       from '$lib/components/markdown/MarkdownEditor.svelte';
+    import PrivateMessageItem   from '$lib/components/lemmy/private_message/PrivateMessageItem.svelte'
+    import RelativeDate         from '$lib/components/util/RelativeDate.svelte';
+    import ReportModal          from '$lib/components/lemmy/moderation/ReportModal.svelte'
 
     import { 
+        type IconSource, 
+        ArrowTopRightOnSquare,
         AtSymbol, 
         ChatBubbleLeftEllipsis, 
         ChatBubbleLeftRight, 
@@ -36,13 +40,14 @@
         Envelope, 
         EnvelopeOpen, 
         Flag, 
-        Window as WindowIcon,
         Icon, 
-        type IconSource 
+        LockClosed, 
+        Megaphone, 
+        NoSymbol, 
+        Trash, 
+        Window as WindowIcon,
     } from 'svelte-hero-icons'
-    
-    
-    
+    import Badge from '$lib/components/ui/Badge.svelte';
     
     export let item: CommentReplyView | PersonMentionView | PrivateMessageView
     export let type: InboxFeedType = 'all'
@@ -207,15 +212,60 @@
                         
                         <!--Post/Comment Reply and Mentions--->
                         {#if isPostReply(item) || isCommentReply(item) || isPersonMention(item)}
-                            <span class="text-xs">
-                                <CommunityLink avatar bind:community={item.community} />
-                            </span>
+                            <div class="flex flex-row gap-1 justify-between items-center">
+                                <span class="text-xs">
+                                    <CommunityLink avatar bind:community={item.community} />
+                                </span>
+
+                                <div class="flex flex-row ml-auto gap-2 items-end">
+                                    {#if item.post.locked}
+                                        <Badge label="Locked" color="yellow" icon={LockClosed} click={false} />
+                                    {/if}
+                                    
+                                    {#if item.post.removed}
+                                        <Badge label="Removed" color="red" icon={NoSymbol} click={false} />
+                                    {/if}
+                                    
+                                    {#if item.post.deleted}
+                                        <Badge label="Deleted" color="red" icon={Trash} click={false} />
+                                    {/if}
+                                    
+                                    {#if (item.post.featured_local || item.post.featured_community)}
+                                        <Badge label="Featured" color="green" icon={Megaphone} click={false} />
+                                    {/if}
+                                    
+                                </div>
+                            </div>
                             
-                            <a href="/comment/{item.comment.id}" class="font-bold text-base">
-                                {item.post.name}
-                            </a>
+                            <!---Post Title and Jump to Comment Buttons--->
+                            <div class="flex flex-row justify-between gap-1 items-center">
+                                <!---Post Title--->
+                                <a href="/comment/{item.comment.id}" class="font-bold text-base">
+                                    {item.post.name}
+                                </a>
+                                
+                                <!---"Jump to" Buttons--->
+                                <div class="flex flex-row gap-1 items-center">
+                                    <Button icon={WindowIcon} iconSize={16} color="tertiary-border" size="sm" title="Open Comment Thread in Modal" on:click={() => {
+                                        if (isPostReply(item) || isCommentReply(item) || isPersonMention(item)) {
+                                            postViewerModal($instance, undefined, item.comment.id)
+                                        }
+                                    }}/>
+                    
+                                    <Button
+                                        color="tertiary-border"
+                                        href="/post/{$instance}/{item.post.id}?thread={item.comment.path}"
+                                        size="sm"
+                                        class="self-start"
+                                        title="Jump to Comment"
+                                        on:click={() => dispatchWindowEvent('clickIntoPost') }
+                                    >
+                                        <Icon src={ArrowTopRightOnSquare} width={16}/>
+                                    </Button>
+                                </div>
+                            </div>
                             
-                            <Comment elevation={-1} postId={item.post.id} node={{ children: [], comment_view: item, depth: 1 }} replying={false} class="!p-0" />
+                            <Comment elevation={-1} postId={item.post.id} onHomeInstance={true} node={{ children: [], comment_view: item, depth: 1 }} replying={false} class="!p-0" />
                         {/if}
 
                         <!---Direct Messages--->
