8bed072d548de310d97a3d5c96895931bde57bea
Admiral Patrick <asimons@ptznetwork.org>
2025-02-05 18:28:12 -0500

    Make xpost links work like post title links for newtab/modal/normal; change responsive layout of item

--- src/lib/components/lemmy/post/components/CrosspostItem.svelte
+++ src/lib/components/lemmy/post/components/CrosspostItem.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import type { EditPostEvent } from '$lib/ui/events'
+    import { dispatchWindowEvent, type EditPostEvent } from '$lib/ui/events'
     import type { PostView } from 'lemmy-js-client';
 
-    import { amMod, isAdmin, postModerationModal } from '$lib/components/lemmy/moderation/moderation'
+    import { amMod, isAdmin, postModerationModal, postViewerModal } from '$lib/components/lemmy/moderation/moderation'
     import { fade } from 'svelte/transition'
     import { getInstance } from '$lib/lemmy.js'
     import { profile } from '$lib/auth'
@@ -22,11 +22,11 @@
         Pencil,
         ShieldCheck
     } from 'svelte-hero-icons'
+    import { goto } from '$app/navigation';
 
     export let crosspost:PostView;                      // PostView object to render
     export let textSize:string = "text-xs"              // Taildwind text size class to apply
     export let iconSize:number = 18                     // Size of the icons to use for avatars
-    export let newTab:boolean = false                   // Open the crosspost in a new tab
     export let instance:string = getInstance()          // Allows passing an instance if the post ID is remote (defaults to current)
     export let noClick:boolean = false                  // Disables pointer events if list is for display only
     export let voteButtons: boolean = true              // Whether to show the vote buttons
@@ -43,26 +43,71 @@
             }
         },
     }
+    
+    let postInstance: string
+    let postID: number
+    let postURL: string
+    
+    function generatePostInstanceAndID() {
+        postInstance = onHomeInstance
+            ? instance
+            : new URL(crosspost.post.ap_id).hostname
+
+        postID = onHomeInstance 
+            ? crosspost.post.id
+            : Number(new URL(crosspost.post.ap_id).pathname.replace('/post/',''))
+        
+        postURL = `/post/${postInstance}/${postID}`
+    }
+
+    $:  crosspost.post.id, onHomeInstance, generatePostInstanceAndID()
 
 </script>
 
 <svelte:window on:editPost={handlers.EditPostEvent} />
 
 {#if !crosspost.post.removed}
-    <a class="flex flex-col lg:flex-row gap-2 items-start w-full rounded-lg
+    <a class="flex flex-col lg:flex-row gap-2 items-center w-full rounded-lg
             hover:dark:bg-zinc-800 hover:bg-slate-200
             py-1 px-1 sm:px-2
             {getTextSize()}
             {noClick ? 'pointer-events-none' : ''}
         " 
         id="{crosspost.post.id.toString()}"
-        href="/post/{instance}/{crosspost.post.id}" title="{crosspost.post.name}"
-        target={newTab ? '_blank' : undefined}
+        href="{postURL}" 
+        title="{crosspost.post.name}"
+        target="_blank"
         transition:fade
+        on:click={ (
+            //@ts-ignore
+            e ) => {
+                // If not already in modal and the setting to open posts in modals is enabled, open the modal viewer.
+                if ($userSettings.openInNewTab.postsInModal) {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    postViewerModal(postInstance, postID)
+                    dispatchWindowEvent('requestSnapshot')
+                    return
+                }
+                // Use goto instead of href to avoid occasionally reloading the whole app on page transition
+                if (!$userSettings.openInNewTab.posts) { 
+                    e.preventDefault()
+                    e.stopPropagation()
+                    goto(postURL)
+                    dispatchWindowEvent('clickIntoPost', {post_id: postID})
+                    return
+                }
+            }
+        }
     >
-        <CommunityLink community={crosspost.community} avatar avatarSize={iconSize} showInstance={true} noClick/>
 
-        <div class="flex flex-row gap-4 lg:ml-auto items-center w-full">
+        <!---Published Date--->
+        <span class="flex flex-row-reverse mr-auto w-full justify-between gap-2 items-center lg:flex-row lg:mr-0 lg:justify-start ">
+            <RelativeDate date={crosspost.post.published} class="opacity-80"/>
+            <CommunityLink community={crosspost.community} avatar avatarSize={iconSize} showInstance={true} noClick/>
+        </span>
+
+        <div class="flex flex-row gap-2 mr-auto lg:ml-auto lg:mr-0 items-center">
             
             <!---Vote Buttons for the XPost Item--->
             {#if voteButtons}
@@ -70,16 +115,7 @@
                     <PostVote bind:post={crosspost} small {onHomeInstance}/>
                 </button>
             {/if}
-
-            <!---Moderation--->
-            {#if !noClick && onHomeInstance && (amMod($profile?.user, crosspost.community) || isAdmin($profile?.user))}
-                <Button color="tertiary-border" size="sm" title="Moderation" icon={ShieldCheck} {iconSize} on:click={(e) => {
-                        e.preventDefault()
-                        e.stopPropagation()
-                        postModerationModal(crosspost) 
-                    }}
-                />
-            {/if}
+           
             
             {#if $userSettings.uiState.showScores && !voteButtons}
                 <span class="flex flex-row gap-1 font-normal items-center">
@@ -91,23 +127,19 @@
             <!---Comment Counts--->
             <CommentCountButton bind:post={crosspost} {onHomeInstance} displayType="feed" />
 
+            <!---Moderation--->
+            {#if !noClick && onHomeInstance && (amMod($profile?.user, crosspost.community) || isAdmin($profile?.user))}
+                <Button color="tertiary-border" size="sm" title="Moderation" icon={ShieldCheck} {iconSize} on:click={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        postModerationModal(crosspost) 
+                    }}
+                />
+            {/if}
+
              
             
-            <!---Published and Edited Date--->
-            <span class="flex flex-row gap-1 ml-auto items-center text-slate-600 dark:text-zinc-400">
-                
-                <RelativeDate date={crosspost.post.published} />
-                
-                <!---Edited Date (hidden on mobile)--->
-                {#if crosspost.post.updated}
-                    <span class="hidden md:flex flex-row items-center gap-1 ml-1">•
-                        <Icon src={Pencil} solid size="12" title="Edited" />
-                        <RelativeDate date={crosspost.post.updated}/>
-                    </span>
-                {/if}
-
-               
-            </span>
+            
 
         </div>
     </a>
