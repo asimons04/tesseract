a211815e03d0b7d193b089e35bdf094a048c1748
Admiral Patrick <asimons@ptznetwork.org>
2025-02-05 19:42:46 -0500

    Revert beta codename to production

--- vite.config.ts
+++ vite.config.ts
@@ -37,8 +37,7 @@ export default defineConfig({
 
   define: {
     __VERSION__: JSON.stringify(process.env.npm_package_version),
-    __CODENAME__: JSON.stringify("NX-01")
-    //__CODENAME__: JSON.stringify("Intrepid")
+    __CODENAME__: JSON.stringify("Intrepid")
     
   },
 })
