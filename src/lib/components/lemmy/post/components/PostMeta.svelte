97215f6ffc66eb5fed45c6c2d9166e1089107d66
Admiral Patrick <asimons@ptznetwork.org>
2025-05-01 19:15:39 -0400

    Backport post meta fix

--- src/lib/components/lemmy/post/components/PostMeta.svelte
+++ src/lib/components/lemmy/post/components/PostMeta.svelte
@@ -65,7 +65,7 @@
     <div class="flex flex-row gap-1 w-full">
 
         <!---Community Name, User Name, Avatar, etc--->
-        <div class="flex flex-col gap-1 w-[calc(100%-150px)]">
+        <div class="flex flex-col gap-1 w-[calc(100%-150px)] xl:w-[calc(100%-300px)]">
 
             <span class="flex flex-row gap-2 text-sm items-start">
                 
@@ -123,7 +123,7 @@
         </div>
 
         <!--Post Action Buttons--->
-        <div class="flex flex-col xl:flex-row-reverse ml-auto gap-1 w-[150px] xl:w-fit">
+        <div class="flex flex-col xl:flex-row-reverse ml-auto gap-1 w-[150px] xl:w-[300px]">
             
             <!---Post Action Buttons Top Row--->
             <div class="flex flex-row items-start gap-2 ml-auto">
