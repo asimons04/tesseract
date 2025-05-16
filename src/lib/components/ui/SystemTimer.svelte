cbace53786ab20b24376b24cd6777f80ac5d53e2
Admiral Patrick <asimons@ptznetwork.org>
2025-05-16 09:53:07 -0400

    Add debug logging

--- src/lib/components/ui/SystemTimer.svelte
+++ src/lib/components/ui/SystemTimer.svelte
@@ -3,6 +3,7 @@
     import { onDestroy, onMount } from "svelte"
     import { StorageCache } from '$lib/storage-controller'
     import { userSettings } from "$lib/settings";
+    import { get } from "svelte/store";
 
     let systemTimer: number
     let lastTick = 0
@@ -24,8 +25,9 @@
         SystemTimerEvent(e:SystemTimerEvent) {
             
             // Every minute, run housekeeping on the controller's storage
-            if ( (e.detail.timestamp - lastTick) > 60) {
+            if ( (e.detail.timestamp - lastTick) >= 60) {
                 lastTick = e.detail.timestamp
+                if (get(userSettings).debugInfo) console.log("SystemTimer.svelte: Running storage housekeeping...")
                 new StorageCache({type: 'local'}).housekeep()
                 new StorageCache({type: 'session'}).housekeep()
             }
@@ -33,4 +35,4 @@
     }
 </script>
 
-<svelte:window on:systemTimer = {handlers.SystemTimerEvent} />
+<svelte:window on:systemTimer={handlers.SystemTimerEvent} />
