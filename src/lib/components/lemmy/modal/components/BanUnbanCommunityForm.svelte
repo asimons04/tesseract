68548a9222fc3604516cfa188d8833a3245038bd
Admiral Patrick <asimons@ptznetwork.org>
2025-05-22 14:49:56 -0400

    Allow switching between direct entry and search when selecting a user to ban

--- src/lib/components/lemmy/modal/components/BanUnbanCommunityForm.svelte
+++ src/lib/components/lemmy/modal/components/BanUnbanCommunityForm.svelte
@@ -18,12 +18,13 @@
     import TextArea from '$lib/components/input/TextArea.svelte';
     import TextInput from '$lib/components/input/TextInput.svelte'
 
-    import { CalendarDays, HandRaised, Icon, Trash } from 'svelte-hero-icons';
+    import { CalendarDays, HandRaised, Icon, MagnifyingGlass, Trash, XCircle } from 'svelte-hero-icons';
     import SettingToggleContainer from '$lib/components/ui/settings/SettingToggleContainer.svelte';
     import SettingToggle from '$lib/components/ui/settings/SettingToggle.svelte';
     import SettingDateInput from '$lib/components/ui/settings/SettingDateInput.svelte';
     import { dispatchWindowEvent } from '$lib/ui/events';
     import UserLink from '../../user/UserLink.svelte';
+    import PersonAutocomplete from '../../PersonAutocomplete.svelte';
     
     
     export let community: Community | undefined = undefined
@@ -40,6 +41,7 @@
     
     let banning = false
     let ban_all_communities = false
+    let searchInstead = false
 
     const dispatcher = createEventDispatcher()
 
@@ -63,7 +65,7 @@
         
         if (!$profile?.jwt || !$profile?.user) return
         
-        if (!formData.username) {
+        if (!user) {
             toast({
                 content: `Please supply a user to ${formData.ban ? 'ban' : 'unban'}.`,
                 type: 'warning',
@@ -83,45 +85,24 @@
             
             return
         }
-        
-        
-        
-        // Convert a Lemmyverse link to something that can be resolved
-        if (formData.username.startsWith('https://lemmyverse.link')) {
-            formData.username = formData.username.replace('https://lemmyverse.link/u/', '@')
-        }
 
         banning = true
 
         try {
-            const res = await getClient().resolveObject({
-                q: formData.username,
-            })
-
-            if (!res.person) {
-                toast({
-                    content: 'Could not find that user.',
-                    type: 'warning',
-                    title: 'User Not Found'
-                })
-
-                return
-            }
-                
             if (ban_all_communities || !community) {
                 
                 $profile.user.moderates.forEach(async (c) => {
                     await getClient().banFromCommunity({
                         ban: formData.ban,
                         expires: date || undefined,
-                        person_id: res.person!.person.id,
+                        person_id: user!.id,
                         community_id: c.community.id,
                         reason: formData.reason,
                         remove_data: formData.remove_data
                     })
 
                     dispatchWindowEvent('banCommunity', {
-                        person_id: res.person!.person.id,
+                        person_id: user!.id,
                         banned: formData.ban,
                         community_id: c.community.id,
                         remove_content: formData.remove_data
@@ -134,14 +115,14 @@
                 await getClient().banFromCommunity({
                     ban: formData.ban,
                     expires: date || undefined,
-                    person_id: res.person.person.id,
+                    person_id: user!.id,
                     community_id: community.id,
                     reason: formData.reason,
                     remove_data: formData.remove_data
                 })
 
                 dispatchWindowEvent('banCommunity', {
-                    person_id: res.person!.person.id,
+                    person_id: user!.id,
                     banned: formData.ban,
                     community_id: community.id,
                     remove_content: formData.remove_data
@@ -152,8 +133,8 @@
 
             toast({
                 content: (ban_all_communities || !community)
-                    ? `${formData.ban ? 'Banned' : 'Unbanned'} ${res.person.person.name}@${new URL(res.person.person.actor_id).hostname} from all moderated communities..`
-                    : `${formData.ban ? 'Banned' : 'Unbanned'} ${res.person.person.name}@${new URL(res.person.person.actor_id).hostname} from ${community.name}.`,
+                    ? `${formData.ban ? 'Banned' : 'Unbanned'} ${user.name}@${new URL(user.actor_id).hostname} from all moderated communities..`
+                    : `${formData.ban ? 'Banned' : 'Unbanned'} ${user.name}@${new URL(user.actor_id).hostname} from ${community.name}.`,
                 type: 'success',
                 title: 'Success'
             })
@@ -176,21 +157,83 @@
 
         banning = false
     }
+
+    async function resolveUser() {
+        // Convert a Lemmyverse link to something that can be resolved
+        if (formData.username.startsWith('https://lemmyverse.link')) {
+            formData.username = formData.username.replace('https://lemmyverse.link/u/', '@')
+        }
+    
+
+        try {
+            const res = await getClient().resolveObject({
+                q: formData.username,
+            })
+            if (res.person) user = res.person.person
+        }
+        catch {
+            toast({
+                content: 'Could not find that user.',
+                type: 'warning',
+                title: 'User Not Found'
+            })
+            return
+        }
+    }
 </script>
 
-<div class="flex flex-col w-full gap-4">
+<div class="flex flex-col w-full gap-1">
     <span class="text-base font-bold">
         {formData.ban ? 'Banning' : 'Unbanning'} from {!community || ban_all_communities ? 'All My Communities' : `${community.name}@${new URL(community.actor_id).hostname}`}
     </span>
     
-    <div class="flex flex-row gap-2 pt-4 w-full items-end">
+    <div class="flex flex-col lg:flex-row gap-2 pt-4 w-full items-start">
+        <!---If User is Selected, Show User Link and 'Unset' Button--->
         {#if user}
-            <UserLink user={user} avatar avatarSize={20} noClick class="my-auto" />
+        <div class="flex flex-col gap-1 w-full">
+            <span class="text-sm font-bold">User</span>    
+            <div class="flex flex-row w-full pt-2 gap-2 items-end justify-between">
+                <UserLink user={user} avatar avatarSize={20} noClick class="my-auto" />
+                <Button color="tertiary-border" class="h-8 mt-auto" size="sm" icon={XCircle} iconSize={16} on:click={() => {
+                    user = undefined
+                    formData.username = ''
+                    }}
+                />
+            </div>
+        </div>
         {:else}
-            <TextInput bind:value={formData.username} class="w-full" label="User" required placeholder="@user@example.com, https://example.com/u/user, or https://lemmyverse.link/u/user@example.com" />
+            <div class="flex flex-col gap-1 w-full">    
+                <!---Search for a User--->
+                <!--containerStyle="max-height: 290px !important;"    -->
+                {#if searchInstead}
+                    <PersonAutocomplete 
+                        containerClass="!w-full"
+                        maxResultsHeight="!max-h-[290px]"
+                        placeholder="Search for User"
+                        label="Search for User"
+                        required={true}
+                        on:select={(e) => {
+                            formData.username = e.detail.actor_id
+                            user = e.detail
+                        }}
+                    />
+                <!---Enter a User Manually--->
+                {:else}
+                    <form class="flex flex-row gap-2 w-full justify-between" on:submit|preventDefault={resolveUser}>
+                        <TextInput bind:value={formData.username} class="w-full" label="Enter User Manually" required placeholder="@user@example.com, https://example.com/u/user, or https://lemmyverse.link/u/user@example.com" />
+                        <Button submit color="tertiary-border" class="h-8 mt-auto" size="sm" icon={MagnifyingGlass} iconSize={16} />
+                    </form>
+                {/if}
+                
+                <button class="text-left text-xs text-sky-800 dark:text-sky-500 hover:underline" on:click={() => {
+                    searchInstead = !searchInstead
+                }}>
+                    {searchInstead ? 'Enter user directly' : 'Search instead'}
+                </button>
+            </div>
         {/if}
         
-        <MultiSelect  options={[false, true]} optionNames={['Unban', 'Ban']} bind:selected={formData.ban} />
+        <MultiSelect headless items={0} label="Action" options={[false, true]} optionNames={['Unban', 'Ban']} bind:selected={formData.ban} />
     </div>
 
     <TextArea bind:value={formData.reason} rows={3} placeholder="Why are you {formData.ban ? 'banning' : 'unbanning'} this user?" class="w-full" label="Reason"/>
@@ -211,7 +254,7 @@
     </SettingToggleContainer>
 
     
-    <Button loading={banning} disabled={banning} color={formData.ban ? 'danger' : 'primary'} size="lg" class="w-full flex-shrink-0" on:click={() => {
+    <Button loading={banning} disabled={banning || !user} color={formData.ban ? 'danger' : 'primary'} size="lg" class="w-full flex-shrink-0" on:click={() => {
             banning = true
             banUnbanUser().then(() => banning = false)
         }}
